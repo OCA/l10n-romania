d82c806b01efec4650787f6f99636e00ccf6fd65
Fekete Mihai <mihai.fekete@forestandbiomass.ro>
2016-01-14 00:02:08 -0800

    Update code, add onchange methods, images.

--- l10n_ro_vat_on_payment/models/account_invoice.py
+++ l10n_ro_vat_on_payment/models/account_invoice.py
@@ -22,9 +22,10 @@ class AccountInvoice(models.Model):
     def onchange_partner_id(self, type, partner_id, date_invoice=False,
                             payment_term=False, partner_bank_id=False,
                             company_id=False):
-        # Check if invoice is with VAT on Payment.
-        # Romanian law specify that the VAT on payment is applied only
-        # for internal invoices (National or not specified fiscal position)
+        """ Check if invoice is with VAT on Payment.
+            Romanian law specify that the VAT on payment is applied only
+            for internal invoices (National or not specified fiscal position)
+        """
         result = super(AccountInvoice, self).onchange_partner_id(
             type, partner_id, date_invoice, payment_term, partner_bank_id,
             company_id)
@@ -37,29 +38,21 @@ class AccountInvoice(models.Model):
                 ctx)._check_vat_on_payment()
         else:
             partner = self.env['res.partner'].browse(partner_id)
-            if self.fiscal_position:
-                if 'National' in self.fiscal_position.name:
-                    vatp = partner.with_context(ctx)._check_vat_on_payment()
-            else:
-                vatp = partner.with_context(ctx)._check_vat_on_payment()
+            vatp = partner.with_context(ctx)._check_vat_on_payment()
         result['value']['vat_on_payment'] = vatp
         return result
 
-    @api.multi
-    def onchange_fiscal_receipt(self, fiscal_receipt=False):
-        # Uncheck VAT on payment if you check fiscal receipt
-        result = {'value': {}}
+    @api.onchange("fiscal_receipt", "fiscal_position")
+    def onchange_fiscal_receipt(self):
+        """ Check for partner vat on payment if the invoice is not
+            a fiscal receipt, and the national fiscal position is set."""
         vatp = False
-        if not fiscal_receipt:
+        if not self.fiscal_receipt:
             ctx = dict(self._context)
             if self.date_invoice:
                 ctx.update({'check_date': self.date_invoice})
-            if self.fiscal_position:
-                if 'National' in self.fiscal_position.name:
-                    vatp = self.partner_id.with_context(
-                        ctx)._check_vat_on_payment()
-            else:
+            if (not self.fiscal_position or
+                    'National' in self.fiscal_position.name):
                 vatp = self.partner_id.with_context(
                     ctx)._check_vat_on_payment()
-        result['value']['vat_on_payment'] = vatp
-        return result
+        self.vat_on_payment = vatp
