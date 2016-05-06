e337d32bc62019e2798c7b42ecddc1b6059916b1
Fekete Mihai <mihai.fekete@forestandbiomass.ro>
2016-05-06 08:08:21 +0300

    Squash commits, add unique partners module. (#8)

---
+++ l10n_ro_partner_unique/__openerp__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+# ©  2015 Forest and Biomass Services Romania
+# See README.rst file on addons root folder for license details
+
+{
+    'name': 'Romania - Partners Unique',
+    'summary': 'Creates a rule for vat and nrc unique for partners.',
+    'version': '8.0.1.0.0',
+    'category': 'Localization',
+    'author': 'Forest and Biomass Services Romania, '
+              'Odoo Community Association (OCA)',
+    'website': 'https://www.forbiom.eu',
+    'license': 'AGPL-3',
+    'application': False,
+    'installable': True,
+    'depends': ['base', 'l10n_ro'],
+    'images': [],
+    'auto_install': False,
+}
