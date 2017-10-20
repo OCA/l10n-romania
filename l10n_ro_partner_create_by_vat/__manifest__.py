f33f26997fa34bcd74f532df7c5859deb866083f
Fekete Mihai <mihai.fekete@forbiom.eu>
2017-10-20 02:38:59 -0700

    Update Romanian partner create by vat to v11, updated dependencies from partner_create_by_vat to base_vat_autocomplete, change Mfinante fetch to ANAF.
    Update get ANAF method, replace vat_number with cod.
    
    Remove images.
    
    Remove openapi source, datas from https://legacy.openapi.ro are old, add tests on ANAF.
    
    Update flake in tests.
    
    Increase tests.
    
    Travis errors.
    
    Increase coverage.
    
    Update ANAF data, remove notfound check untill i found a vat to check with tests.
    
    Small details.

---
+++ l10n_ro_partner_create_by_vat/__manifest__.py
@@ -0,0 +1,16 @@
+# Copyright  2015 Forest and Biomass Romania
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+{
+    'name': 'Romania - Partner Create by VAT',
+    'summary': 'Romania - Partner Create by VAT',
+    'version': '11.0.1.0.0',
+    'category': 'Localization',
+    'author': 'Forest and Biomass Romania, '
+              'Odoo Community Association (OCA)',
+    'website': 'https://www.forbiom.eu',
+    'license': 'AGPL-3',
+    'installable': True,
+    'depends': ['l10n_ro', 'base_vat_autocomplete'],
+    'data': ['views/res_partner_view.xml'],
+}
