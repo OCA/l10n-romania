db55b8f83ee100d5e11195df972405b7e6307be5
Fekete Mihai <mihai.fekete@forbiom.eu>
2017-12-12 04:18:53 -0800

    Update license, readme, imports.

--- l10n_ro_siruta/hooks.py
+++ l10n_ro_siruta/hooks.py
@@ -1,4 +1,4 @@
-# Copyright 2017 Forest and Biomass Romania
+# Copyright (C) 2017 Forest and Biomass Romania
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 from odoo import api, SUPERUSER_ID
