ca73179d71b802d7005349c3a56f7034ca708c2c
Fekete Mihai <mihai.fekete@forbiom.eu>
2018-01-28 08:29:12 +0200

    Migrate l10n_ro_partner_unique to 11.0.

--- l10n_ro_partner_unique/__init__.py
+++ l10n_ro_partner_unique/__init__.py
@@ -1,5 +1,4 @@
-# -*- coding: utf-8 -*-
-# ©  2015 Forest and Biomass Services Romania
-# See README.rst file on addons root folder for license details
+# Copyright  2015 Forest and Biomass Romania
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 from . import models
