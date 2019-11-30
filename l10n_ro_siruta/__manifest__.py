778563f55b7e817ddd735117ff3f31b9f9056710
Mihai Fekete <feketemihai@gmail.com>
2019-11-30 01:43:49 -0800

    Update modules manifest.

--- l10n_ro_siruta/__manifest__.py
+++ l10n_ro_siruta/__manifest__.py
@@ -1,16 +1,9 @@
-# Copyright (C) 2015 Forest and Biomass Romania
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 {
     'name': 'Romania - Siruta',
-    'summary': 'Romania - Siruta',
-    'version': '11.0.1.0.0',
     'category': 'Localization',
-    'author': 'Forest and Biomass Romania, '
-              'Odoo Community Association (OCA)',
-    'website': 'https://www.forbiom.eu',
-    'license': 'AGPL-3',
-    'installable': True,
+    'summary': 'Romania - Siruta',
     'depends': ['base', 'base_address_city', 'contacts', 'l10n_ro'],
     'data': ['views/partner_view.xml',
              'views/siruta_view.xml',
@@ -18,5 +11,14 @@
              ],
     'demo': ['demo/demo_siruta.xml'],
     'images': ['static/description/customer.png',
-               'static/description/address.png']
+               'static/description/address.png'],
+    'license': 'AGPL-3',
+    'version': '11.0.1.0.0',
+    'author': 'OdooERP Romania,'
+              'Forest and Biomass Romania,'
+              'Odoo Community Association (OCA)',
+    'website': 'https://github.com/OCA/l10n-romania',
+    'installable': True,
+    'development_status': 'Mature',
+    'maintainers': ['feketemihai'],
 }
