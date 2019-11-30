9d876b073401049cfe626882ea8d6a8f72d2d556
Mihai Fekete <feketemihai@gmail.com>
2019-11-30 01:43:49 -0800

    Update modules manifest.

--- l10n_ro_address_extended/__manifest__.py
+++ l10n_ro_address_extended/__manifest__.py
@@ -3,17 +3,20 @@
 
 {
     'name': 'Romania - Extended Addresses',
-    'summary': 'Romania - Extended Addresses',
-    'version': '11.0.1.0.0',
     'category': 'Localization',
-    'author': 'Forest and Biomass Romania, '
-              'Odoo Community Association (OCA)',
-    'website': 'https://www.forbiom.eu',
-    'license': 'AGPL-3',
-    'installable': True,
+    'summary': 'Romania - Extended Addresses',
     'depends': ['base_address_extended'],
     'data': [
         'views/res_company_view.xml',
         'views/res_partner_view.xml',
     ],
+    'license': 'AGPL-3',
+    'version': '11.0.1.0.0',
+    'author': 'OdooERP Romania,'
+              'Forest and Biomass Romania,'
+              'Odoo Community Association (OCA)',
+    'website': 'https://github.com/OCA/l10n-romania',
+    'installable': True,
+    'development_status': 'Mature',
+    'maintainers': ['feketemihai'],
 }
