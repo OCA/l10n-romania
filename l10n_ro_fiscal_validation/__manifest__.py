5371c5a0e9db29c3a9beb99d97dcc62fffbb6008
Mihai Fekete <feketemihai@gmail.com>
2019-11-30 01:43:49 -0800

    Update modules manifest.

--- l10n_ro_fiscal_validation/__manifest__.py
+++ l10n_ro_fiscal_validation/__manifest__.py
@@ -1,17 +1,19 @@
-# Copyright  2018 Forest and Biomass Romania
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 {
     'name': 'Romania - Fiscal Validation',
-    'summary': 'Romania - Fiscal Validation',
-    'version': '11.0.1.0.0',
     'category': 'Localization',
-    'author': 'Forest and Biomass Romania, '
-              'Odoo Community Association (OCA)',
-    'website': 'https://www.forbiom.eu',
-    'license': 'AGPL-3',
-    'installable': True,
+    'summary': 'Romania - Fiscal Validation',
     'depends': ['l10n_ro_partner_create_by_vat',
                 'l10n_ro_vat_on_payment'],
     'data': ['data/res_partner_vat_cron.xml'],
+    'license': 'AGPL-3',
+    'version': '11.0.1.0.0',
+    'author': 'OdooERP Romania,'
+              'Forest and Biomass Romania,'
+              'Odoo Community Association (OCA)',
+    'website': 'https://github.com/OCA/l10n-romania',
+    'installable': True,
+    'development_status': 'Mature',
+    'maintainers': ['feketemihai'],
 }
