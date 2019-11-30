157a40105d9728a2f62def85a3f8c66ee783e869
Mihai Fekete <feketemihai@gmail.com>
2019-11-30 01:43:49 -0800

    Update modules manifest.

--- currency_rate_update_RO_BNR/__manifest__.py
+++ currency_rate_update_RO_BNR/__manifest__.py
@@ -1,17 +1,19 @@
-# © 2017 Comunitea
-# © 2018 Forest and Biomass Romania
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
     'name': 'Currency Rate Update - BNR',
+    'category': 'Financial Management/Configuration',
     'summary': 'Currency Rate Update National Bank of Romania service',
-    'version': '11.0.1.0.0',
-    "author": "Camptocamp,Odoo Community Association (OCA)",
-    "website": "https://github.com/OCA/l10n-romania",
-    "license": "AGPL-3",
-    "category": "Financial Management/Configuration",
-    "depends": [
-        "base",
-        "currency_rate_update"
+    'depends': [
+        'base',
+        'currency_rate_update'
     ],
-    'installable': True
+    'license': 'AGPL-3',
+    'version': '11.0.1.0.0',
+    'author': 'OdooERP Romania,'
+              'Forest and Biomass Romania,'
+              'Odoo Community Association (OCA)',
+    'website': 'https://github.com/OCA/l10n-romania',
+    'installable': True,
+    'development_status': 'Mature',
+    'maintainers': ['feketemihai'],
 }
