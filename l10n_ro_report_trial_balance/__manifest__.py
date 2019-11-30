55b06160cbe83566891f59d3bcb44b5b40c74cfb
Mihai Fekete <feketemihai@gmail.com>
2019-11-30 01:43:49 -0800

    Update modules manifest.

--- l10n_ro_report_trial_balance/__manifest__.py
+++ l10n_ro_report_trial_balance/__manifest__.py
@@ -1,16 +1,9 @@
-# Copyright  2018 Forest and Biomass Romania
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 {
     'name': 'Romania - Account Trial Balance Report',
-    'summary': 'Romania - Account Trial Balance Report',
-    'version': '11.0.1.0.0',
     'category': 'Localization',
-    'author': 'Forest and Biomass Romania, '
-              'Odoo Community Association (OCA)',
-    'website': 'https://www.forbiom.eu',
-    'license': 'AGPL-3',
-    'installable': True,
+    'summary': 'Romania - Account Trial Balance Report',
     'depends': ['account', 'account_financial_report', 'l10n_ro'],
     'data': [
         "views/layouts.xml",
@@ -20,4 +13,13 @@
         "views/trial_balance_view.xml",
         "wizards/trial_balance_wizard_view.xml",
     ],
+    'license': 'AGPL-3',
+    'version': '11.0.1.0.0',
+    'author': 'OdooERP Romania,'
+              'Forest and Biomass Romania,'
+              'Odoo Community Association (OCA)',
+    'website': 'https://github.com/OCA/l10n-romania',
+    'installable': True,
+    'development_status': 'Mature',
+    'maintainers': ['feketemihai'],
 }
