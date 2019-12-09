cf8b5b71aa36493ac6fbaa366b728c3370c74d6b
Mihai Fekete <feketemihai@gmail.com>
2019-12-08 23:06:47 -0800

    Add module l10n_ro_stock.
    Add module l10n_ro_stock.
    
    Update pylint.

---
+++ l10n_ro_stock/__manifest__.py
@@ -0,0 +1,19 @@
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+{
+    "name": "Romania - Stock",
+    'category': 'Localization',
+    "depends": ['stock'],
+    'data': ['views/stock_warehouse_view.xml',
+             'views/stock_location_view.xml'],
+    'license': 'AGPL-3',
+    'version': '11.0.1.0.0',
+    'author': 'OdooERP Romania,'
+              'Dorin Hongu,'
+              'Forest and Biomass Romania,'
+              'Odoo Community Association (OCA)',
+    'website': 'https://github.com/OCA/l10n-romania',
+    'installable': True,
+    'development_status': 'Mature',
+    'maintainers': ['feketemihai']
+}
