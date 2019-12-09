a61c47946d05a6759a7d71c07cbd133c71d038f7
Mihai Fekete <feketemihai@gmail.com>
2019-12-08 23:06:47 -0800

    Add module l10n_ro_stock.
    Add module l10n_ro_stock.
    
    Update pylint.

---
+++ l10n_ro_stock/models/stock_location.py
@@ -0,0 +1,19 @@
+# Copyright (C) 2016 Forest and Biomass Romania
+# Copyright (C) 2018 Dorin Hongu <dhongu(@)gmail(.)com
+# Copyright (C) 2019 OdooERP Romania
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from odoo import fields, models, _
+
+
+class StockLocation(models.Model):
+    _inherit = "stock.location"
+
+    usage = fields.Selection(
+        selection_add=[('usage_giving', 'Usage Giving'),
+                       ('consume', 'Consume')])
+    merchandise_type = fields.Selection(
+        [("store", _("Store")),
+         ("warehouse", _("Warehouse"))],
+        string="Merchandise type",
+        default="warehouse")
