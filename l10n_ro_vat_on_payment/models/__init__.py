9a923a3ebf4c69310409aaa670a0c1c16de08cb8
Mihai Fekete <feketemihai@odooerpromania.ro>
2020-02-03 09:00:55 +0200

    [MIG] l10n_ro_vat_on_payment: Migration to 13.0

--- l10n_ro_vat_on_payment/models/__init__.py
+++ l10n_ro_vat_on_payment/models/__init__.py
@@ -1,5 +1,3 @@
 from . import res_partner_anaf
 from . import res_partner
-from . import account_tax
-from . import account_partial_reconcile
-from . import account_invoice
+from . import account_move
