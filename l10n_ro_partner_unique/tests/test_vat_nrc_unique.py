bbd2f7856f3dc073d4746a2287bdd6cd407a5685
Mihai Fekete <feketemihai@odooerpromania.ro>
2020-02-04 12:19:03 +0200

    [MIG] l10n_ro_partner_unique: Migration to 13.0

--- l10n_ro_partner_unique/tests/test_vat_nrc_unique.py
+++ l10n_ro_partner_unique/tests/test_vat_nrc_unique.py
@@ -1,4 +1,5 @@
 # Copyright (C) 2017 Forest and Biomass Romania
+# Copyright (C) 2020 OdooERP Romania
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 from odoo.exceptions import ValidationError
@@ -9,27 +10,25 @@ class TestVatUnique(SavepointCase):
     @classmethod
     def setUpClass(cls):
         super(TestVatUnique, cls).setUpClass()
-        cls.partner = cls.env['res.partner'].create({
-            'name': 'Test partner',
-            'vat': 'RO30834857',
-            'nrc': 'J35/2622/2012'
-        })
+        cls.partner = cls.env["res.partner"].create(
+            {"name": "Test partner", "vat": "RO30834857", "nrc": "J35/2622/2012"}
+        )
 
     def test_duplicated_vat_creation(self):
         """ Test creation of partner."""
         with self.assertRaises(ValidationError):
-            self.env['res.partner'].create({
-                'name': 'Second partner',
-                'vat': 'RO30834857',
-                'nrc': 'J35/2622/2012'
-            })
+            self.env["res.partner"].create(
+                {"name": "Second partner", "vat": "RO30834857", "nrc": "J35/2622/2012"}
+            )
 
     def test_contact_vat_creation(self):
         """ Test creation of partner contacs."""
-        self.env['res.partner'].create({
-            'name': 'Test partner 1 - child',
-            'parent_id': self.partner.id,
-            'is_company': False,
-            'vat': 'RO30834857',
-            'nrc': 'J35/2622/2012'
-        })
+        self.env["res.partner"].create(
+            {
+                "name": "Test partner 1 - child",
+                "parent_id": self.partner.id,
+                "is_company": False,
+                "vat": "RO30834857",
+                "nrc": "J35/2622/2012",
+            }
+        )
