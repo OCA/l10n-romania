14b5a026554aaf2dc405d94f14bb8e133411bbd8
Mihai Fekete <feketemihai@odooerpromania.ro>
2020-02-05 16:08:00 +0200

    [MIG] l10n_ro_account_period_close: Migration to 13.0

--- l10n_ro_account_period_close/wizards/wizard_account_period_closing.py
+++ l10n_ro_account_period_close/wizards/wizard_account_period_closing.py
@@ -19,34 +19,26 @@ class WizardAccountPeriodClosing(models.TransientModel):
         return today + relativedelta(day=1, days=-1)
 
     closing_id = fields.Many2one(
-        "account.period.closing",
-        "Closing Model",
-        required=True,
-        ondelete="cascade"
+        "account.period.closing", "Closing Model", required=True, ondelete="cascade"
     )
     company_id = fields.Many2one(
-        comodel_name="res.company",
-        related="closing_id.company_id")
+        comodel_name="res.company", related="closing_id.company_id"
+    )
     journal_id = fields.Many2one(
-        comodel_name="account.journal",
-        related="closing_id.journal_id")
-    date_range_id = fields.Many2one(
-        comodel_name="date.range",
-        string="Date range")
-    date_from = fields.Date("Start Date", required=True,
-                            default=_get_default_date_from)
-    date_to = fields.Date("End Date", required=True,
-                          default=_get_default_date_to)
+        comodel_name="account.journal", related="closing_id.journal_id"
+    )
+    date_range_id = fields.Many2one(comodel_name="date.range", string="Date range")
+    date_from = fields.Date("Start Date", required=True, default=_get_default_date_from)
+    date_to = fields.Date("End Date", required=True, default=_get_default_date_to)
 
-    @api.onchange('date_range_id')
+    @api.onchange("date_range_id")
     def onchange_date_range_id(self):
         """Handle date range change."""
         if self.date_range_id:
             self.date_from = self.date_range_id.date_start
             self.date_to = self.date_range_id.date_end
 
-    @api.multi
     def do_close(self):
-        self.ensure_one()
-        self.closing_id.close(self.date_from, self.date_to)
-        return {'type': 'ir.actions.act_window_close'}
+        for wizard in self:
+            wizard.closing_id.close(wizard.date_from, wizard.date_to)
+        return {"type": "ir.actions.act_window_close"}
