59c2fd45a84c3ea51c947114d57b2b577bda7aff
Mihai Fekete <feketemihai@odooerpromania.ro>
2020-02-05 17:07:58 +0200

    [MIG] l10n_ro_address_extended: Migration to 13.0

--- l10n_ro_address_extended/models/res_company.py
+++ l10n_ro_address_extended/models/res_company.py
@@ -1,22 +1,22 @@
-# Copyright  2018 Forest and Biomass Romania
+# Copyright (C) 2018 Forest and Biomass Romania
+# Copyright (C) 2020 OdooERP Romania
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 from odoo import fields, models
 
 
 class Company(models.Model):
-    _inherit = 'res.company'
+    _inherit = "res.company"
 
-    street_staircase = fields.Char('Staircase Number',
-                                   compute='_compute_address',
-                                   inverse='_inverse_street_staircase')
+    street_staircase = fields.Char(
+        "Staircase Number",
+        compute="_compute_address",
+        inverse="_inverse_street_staircase",
+    )
 
     def _get_company_address_fields(self, partner):
-        address_fields = super(
-            Company, self)._get_company_address_fields(partner)
-        address_fields.update({
-            'street_staircase': partner.street_staircase,
-        })
+        address_fields = super(Company, self)._get_company_address_fields(partner)
+        address_fields.update({"street_staircase": partner.street_staircase})
         return address_fields
 
     def _inverse_street_staircase(self):
