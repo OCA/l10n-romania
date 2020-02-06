42ca6e88daae232c6307cc9259172f2998530a8c
Mihai Fekete <feketemihai@odooerpromania.ro>
2020-02-06 18:41:52 +0200

    Improve code in l10n_ro_vat_on_payment and l10n_ro_stock.

--- l10n_ro_account_period_close/models/account.py
+++ l10n_ro_account_period_close/models/account.py
@@ -1,4 +1,5 @@
-# Copyright  2018 Forest and Biomass Romania
+# Copyright (C) 2018 Forest and Biomass Romania
+# Copyright (C) 2019 OdooERP Romania
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 from odoo import fields, models
