9145b5fbf84ade67372b112fa5e082991a94c802
Mihai Fekete <feketemihai@odooerpromania.ro>
2020-05-12 11:30:09 +0300

    [13.0] [UPD] Update owner with new company name.

--- l10n_ro_partner_unique/__manifest__.py
+++ l10n_ro_partner_unique/__manifest__.py
@@ -7,7 +7,7 @@
     "depends": ["l10n_ro"],
     "license": "AGPL-3",
     "version": "13.0.1.0.0",
-    "author": "OdooERP Romania,"
+    "author": "NextERP Romania,"
     "Forest and Biomass Romania,"
     "Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/l10n-romania",
