8ba6f10ec70173dc634d8dc08f87d1767e379d66
Mihai Fekete <feketemihai@odooerpromania.ro>
2020-05-12 11:30:09 +0300

    [13.0] [UPD] Update owner with new company name.

--- l10n_ro_fiscal_validation/tests/test_partner_update_vat_subjected.py
+++ l10n_ro_fiscal_validation/tests/test_partner_update_vat_subjected.py
@@ -1,5 +1,5 @@
 # Copyright (C) 2017 Forest and Biomass Romania
-# Copyright (C) 2020 OdooERP Romania
+# Copyright (C) 2020 NextERP Romania
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 import io
