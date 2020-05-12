fa36d65b806e6bb0515a6017935548e7109448c5
Mihai Fekete <feketemihai@odooerpromania.ro>
2020-05-12 11:30:09 +0300

    [13.0] [UPD] Update owner with new company name.

--- l10n_ro_address_extended/tests/test_street_fields.py
+++ l10n_ro_address_extended/tests/test_street_fields.py
@@ -1,5 +1,5 @@
 # Copyright (C) 2018 Forest and Biomass Romania
-# Copyright (C) 2020 OdooERP Romania
+# Copyright (C) 2020 NextERP Romania
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 from odoo.tests.common import TransactionCase
