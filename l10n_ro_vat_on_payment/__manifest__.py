9c99c799fce4eaf1e4f3b3efc76fcefdea503bdd
Mihai Fekete <feketemihai@odooerpromania.ro>
2020-05-12 11:30:09 +0300

    [13.0] [UPD] Update owner with new company name.

--- l10n_ro_vat_on_payment/__manifest__.py
+++ l10n_ro_vat_on_payment/__manifest__.py
@@ -12,7 +12,7 @@
     "depends": ["account", "l10n_ro"],
     "license": "AGPL-3",
     "version": "13.0.1.0.0",
-    "author": "OdooERP Romania,"
+    "author": "NextERP Romania,"
     "Forest and Biomass Romania,"
     "Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/l10n-romania",
