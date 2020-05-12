6d8cbb518a82577cf71199d57988fb40941be12b
Mihai Fekete <feketemihai@odooerpromania.ro>
2020-05-12 11:30:09 +0300

    [13.0] [UPD] Update owner with new company name.

--- l10n_ro_vat_on_payment/models/account_move.py
+++ l10n_ro_vat_on_payment/models/account_move.py
@@ -1,5 +1,5 @@
 # Copyright (C) 2015 Forest and Biomass Romania
-# Copyright (C) 2020 OdooERP Romania
+# Copyright (C) 2020 NextERP Romania
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 from odoo import api, models
