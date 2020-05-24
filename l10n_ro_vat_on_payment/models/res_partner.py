a18973e102edb38c657da8e5c3aab9fb452cb7d8
Mihai Fekete <feketemihai@odooerpromania.ro>
2020-05-24 08:27:18 +0300

    [UPD] Improve VAT on payment.

--- l10n_ro_vat_on_payment/models/res_partner.py
+++ l10n_ro_vat_on_payment/models/res_partner.py
@@ -18,6 +18,8 @@ class ResPartner(models.Model):
         for partner in self:
             if partner.vat:
                 partner.vat_number = self._split_vat(partner.vat)[1]
+            else:
+                partner.vat_number = ""
 
     @api.depends("vat_number")
     def _compute_anaf_history(self):
@@ -53,7 +55,9 @@ class ResPartner(models.Model):
                 return datetime.strptime(str(strdate), "%Y%m%d").strftime(DATE_FORMAT)
 
         vat_numbers = [
-            p.vat_number for p in self if p.vat and p.vat.lower().startswith("ro")
+            p.vat_number
+            for p in self
+            if p.vat and p.vat.lower().startswith("ro") and p.vat[2:].isnumeric()
         ]
         if vat_numbers == []:
             return
