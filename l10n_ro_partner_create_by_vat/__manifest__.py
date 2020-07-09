1136afa1a452ff2f0b9efc2d91e5d95e3d2c9a07
Mihai Fekete <feketemihai@odooerpromania.ro>
2020-07-09 18:41:15 +0300

    Fix partner create by vat.

--- l10n_ro_partner_create_by_vat/__manifest__.py
+++ l10n_ro_partner_create_by_vat/__manifest__.py
@@ -7,7 +7,7 @@
     "depends": ["base_vat"],
     "data": ["views/res_partner_view.xml"],
     "license": "AGPL-3",
-    "version": "13.0.1.0.0",
+    "version": "13.0.1.0.1",
     "author": "NextERP Romania,"
     "Forest and Biomass Romania,"
     "Odoo Community Association (OCA)",
