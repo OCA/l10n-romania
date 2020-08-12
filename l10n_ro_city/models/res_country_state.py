e7e48c7a354a7ce7c35d3702fb7906a4a3088a6c
Dorin Hongu <dhongu@gmail.com>
2020-08-12 17:21:59 +0300

    eliminare api din res_country_state.py

--- l10n_ro_city/models/res_country_state.py
+++ l10n_ro_city/models/res_country_state.py
@@ -1,7 +1,7 @@
 #
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-from odoo import api, fields, models
+from odoo import fields, models
 
 
 class CountryCity(models.Model):
