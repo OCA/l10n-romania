427701a94ec9f9849be9eaec71d2c8f9b4bccf1a
Fekete Mihai <feketemihai@nexterp.ro>
2020-09-30 18:30:46 +0300

    [13.0] [MIG] l10n_ro_siruta (#88)
    * Add SIRUTA module.
    
    * Fix camelcase, readme.
    
    * Add images, translation.
    
    * Add images, translation.
    
    * Remove import of csv files, will be done from the config module with a load action, this will reduce time at update of the module.
    
    * Fix typo, update address field from partner, add hook when installing the module.
    
    * Make the views more explicit, remove end of lines spaces.
    
    * Format view, replace tabs with spaces.
    
    * Crop images.
    
    * OCA Transbot updated translations from Transifex
    
    * Update Romania - Siruta to v11.
    
    Update readonly attributes in partner.
    
    Update siruta onchange on partner, add demo, create tests.
    
    Update copyright, license, add hook at install.
    
    Update code, add logger, remove unused imports.
    
    Update hook.
    
    Add l10n_ro as a dependant module.
    
    Remove name_search functions, in the views there already the ability to search after model fields.
    
    Increase tests on onchange, demo data.
    
    * Update license, readme, imports.
    
    * remove obsolete .pot files [ci skip]
    
    * [UPD] Update l10n_ro_siruta.pot
    
    * Update modules manifest.
    
    * [11.0] [UPD] Update owner with new company name. (#67)
    
    * [MIG] l10n_ro_siruta: Migration to 13.0
    
    Co-authored-by: Fekete Mihai <mihai.fekete@forbiom.eu>
    Co-authored-by: OCA Transbot <transbot@odoo-community.org>
    Co-authored-by: Stéphane Bidoul (ACSONE) <stephane.bidoul@acsone.eu>
    Co-authored-by: oca-travis <oca+oca-travis@odoo-community.org>

---
+++ l10n_ro_siruta/hooks.py
@@ -0,0 +1,22 @@
+# Copyright (C) 2017 Forest and Biomass Romania
+# Copyright (C) 2020 Terrabit
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+import os
+
+from odoo import SUPERUSER_ID, api, tools
+
+
+def post_init_hook(cr, registry):
+
+    path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "data")
+
+    files = ["res.country.zone.csv", "res.country.state.csv", "res.country.commune.csv"]
+    for file in files:
+        with tools.file_open(path + "/" + file, mode="rb") as fp:
+            tools.convert_csv_import(
+                cr, "base", file, fp.read(), {}, mode="init", noupdate=True,
+            )
+    with api.Environment.manage():
+        env = api.Environment(cr, SUPERUSER_ID, {})
+        env["res.partner"]._install_l10n_ro_siruta()
