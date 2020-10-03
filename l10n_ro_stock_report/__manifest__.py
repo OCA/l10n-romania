b02700b17bbb4afd189036d02edcfcbfd189d97c
Mihai Fekete <feketemihai@odooerpromania.ro>
2020-10-03 16:08:08 +0300

    Add stock report module

---
+++ l10n_ro_stock_report/__manifest__.py
@@ -0,0 +1,13 @@
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+{
+    "name": "Romania - Stock Report",
+    "license": "AGPL-3",
+    "version": "13.0.1.0.0",
+    "author": "Terrabit,NextERP Romania,Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/l10n-romania",
+    "category": "Warehouse",
+    "depends": ["l10n_ro_stock_account", "date_range"],
+    "data": ["report/stock_report_view.xml"],
+    "installable": True,
+    "maintainers": ["dhongu", "feketemihai"],
+}
