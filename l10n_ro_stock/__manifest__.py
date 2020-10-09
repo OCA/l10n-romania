80c5c6016b36e95e2e9bc67c9408dc8864bf7b8e
Mihai Fekete <feketemihai@odooerpromania.ro>
2020-10-09 14:37:16 +0300

    [MIG] l10n_ro_stock: Migration to 14.0

--- l10n_ro_stock/__manifest__.py
+++ l10n_ro_stock/__manifest__.py
@@ -6,7 +6,7 @@
     "depends": ["stock"],
     "data": ["views/stock_warehouse_view.xml", "views/stock_location_view.xml"],
     "license": "AGPL-3",
-    "version": "15.0.1.0.0",
+    "version": "14.0.1.0.0",
     "author": "NextERP Romania,"
     "Dorin Hongu,"
     "Forest and Biomass Romania,"
