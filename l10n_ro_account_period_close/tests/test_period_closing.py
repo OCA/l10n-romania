03181f5b7382bc996b6f11de81e823f66f67a53e
Mihai Fekete <feketemihai@odooerpromania.ro>
2020-10-09 16:56:41 +0300

    [MIG] l10n_ro_account_period_close: Migration to 14.0

--- l10n_ro_account_period_close/tests/test_period_closing.py
+++ l10n_ro_account_period_close/tests/test_period_closing.py
@@ -5,64 +5,124 @@ import time
 
 from dateutil.relativedelta import relativedelta
 
-from odoo import fields, tools
-from odoo.modules.module import get_resource_path
-from odoo.tests.common import TransactionCase
+from odoo import fields
+from odoo.tests import tagged
+
+from odoo.addons.account.tests.common import AccountTestInvoicingCommon
 
 
-class TestCurrencyReevaluation(TransactionCase):
-    def setUp(self):
-        super(TestCurrencyReevaluation, self).setUp()
-        ref = self.env.ref
-        self.per_close_model = self.env["account.period.closing"]
-        self.wiz_close_model = self.env["account.period.closing.wizard"]
-        # load account_minimal_test.xml file for chart of account in configuration
-        tools.convert_file(
-            self.cr,
-            "l10n_ro_account_period_close",
-            get_resource_path("account", "test", "account_minimal_test.xml"),
-            {},
-            "init",
-            False,
-            "test",
-            self.registry._assertion_report,
-        )
-        company = self.env.user.company_id
+@tagged("post_install", "-at_install")
+class TestCurrencyReevaluation(AccountTestInvoicingCommon):
+    @classmethod
+    def setUpClass(cls, chart_template_ref=None):
+        super().setUpClass(chart_template_ref=chart_template_ref)
+        cls.per_close_model = cls.env["account.period.closing"]
+        cls.wiz_close_model = cls.env["account.period.closing.wizard"]
+        cls.company = company = cls.env.company
 
-        search_account = self.env["account.account"].search
-
-        type_revenue = self.env.ref("account.data_account_type_revenue")
-        default_account_revenue = search_account(
-            [("company_id", "=", company.id), ("user_type_id", "=", type_revenue.id)],
-            limit=1,
-        )
-        type_expenses = self.env.ref("account.data_account_type_expenses")
-        default_account_expense = search_account(
-            [("company_id", "=", company.id), ("user_type_id", "=", type_expenses.id)],
-            limit=1,
-        )
-
-        default_account_receivable = search_account(
-            [("company_id", "=", company.id), ("user_type_id.type", "=", "receivable")],
-            limit=1,
-        )
-
-        default_account_payable = search_account(
-            [("company_id", "=", company.id), ("user_type_id.type", "=", "payable")],
-            limit=1,
-        )
-
+        default_account_revenue = cls.company_data["default_account_revenue"]
+        default_account_expense = cls.company_data["default_account_expense"]
+        default_account_receivable = cls.company_data["default_account_receivable"]
+        default_account_payable = cls.company_data["default_account_payable"]
         default_account_tax_sale = company.account_sale_tax_id.mapped(
             "invoice_repartition_line_ids.account_id"
         )
-
         default_account_tax_purchase = company.account_purchase_tax_id.mapped(
             "invoice_repartition_line_ids.account_id"
         )
 
-        self.test_move = self.env["account.move"].create(
+        cls.misc_journal = cls.company_data["default_journal_misc"]
+        cls.debit_acc = cls.env["account.account"].create(
             {
-                "type": "entry",
+                "name": "DEBIT ACC",
+                "code": "DEBITACC",
+                "user_type_id": cls.env.ref(
+                    "account.data_account_type_current_liabilities"
+                ).id,
+                "reconcile": True,
+                "company_id": cls.company.id,
+            }
+        )
+        cls.credit_acc = cls.env["account.account"].create(
+            {
+                "name": "CREDIT ACC",
+                "code": "CREDITACC",
+                "user_type_id": cls.env.ref(
+                    "account.data_account_type_current_liabilities"
+                ).id,
+                "reconcile": True,
+                "company_id": cls.company.id,
+            }
+        )
+        cls.vat_close_debit = cls.env["account.account"].create(
+            {
+                "name": "VAT CLOSE DEBIT",
+                "code": "VATCLOSEDEBIT",
+                "user_type_id": cls.env.ref(
+                    "account.data_account_type_current_assets"
+                ).id,
+                "company_id": cls.company.id,
+            }
+        )
+        cls.vat_close_credit = cls.env["account.account"].create(
+            {
+                "name": "VAT CLOSE CREDIT",
+                "code": "VATCLOSECREDIT",
+                "user_type_id": cls.env.ref(
+                    "account.data_account_type_current_assets"
+                ).id,
+                "company_id": cls.company.id,
+            }
+        )
+        cls.tax_base_amount_account = cls.env["account.account"].create(
+            {
+                "name": "TAX_BASE",
+                "code": "TBASE",
+                "user_type_id": cls.env.ref(
+                    "account.data_account_type_current_assets"
+                ).id,
+                "company_id": cls.company.id,
+            }
+        )
+        cls.exp_closing = cls.per_close_model.create(
+            {
+                "name": "Closing Expenses",
+                "type": "expense",
+                "journal_id": cls.misc_journal.id,
+                "debit_account_id": cls.debit_acc.id,
+                "credit_account_id": cls.credit_acc.id,
+            }
+        )
+        cls.inc_closing = cls.per_close_model.create(
+            {
+                "name": "Closing Incomes",
+                "type": "income",
+                "journal_id": cls.misc_journal.id,
+                "close_result": True,
+                "debit_account_id": cls.debit_acc.id,
+                "credit_account_id": cls.credit_acc.id,
+            }
+        )
+        cls.vat_closing = cls.per_close_model.create(
+            {
+                "name": "Closing VAT",
+                "type": "selected",
+                "account_ids": [
+                    (
+                        6,
+                        0,
+                        [default_account_tax_sale.id, default_account_tax_purchase.id],
+                    )
+                ],
+                "journal_id": cls.misc_journal.id,
+                "debit_account_id": cls.vat_close_debit.id,
+                "credit_account_id": cls.vat_close_credit.id,
+            }
+        )
+
+        cls.test_move = cls.env["account.move"].create(
+            {
+                "move_type": "entry",
                 "date": fields.Date.from_string(time.strftime("%Y-%m") + "-01"),
                 "line_ids": [
                     (
@@ -99,11 +159,11 @@ class TestCurrencyReevaluation(TransactionCase):
                 ],
             }
         )
-        self.test_move.action_post()
+        cls.test_move.action_post()
 
-        self.test_move = self.env["account.move"].create(
+        cls.test_move = cls.env["account.move"].create(
             {
-                "type": "entry",
+                "move_type": "entry",
                 "date": fields.Date.from_string(time.strftime("%Y-%m") + "-01"),
                 "line_ids": [
                     (
@@ -140,44 +200,7 @@ class TestCurrencyReevaluation(TransactionCase):
                 ],
             }
         )
-        self.test_move.action_post()
-
-        self.misc_journal = ref("l10n_ro_account_period_close.miscellaneous_journal")
-        self.debit_acc = ref("l10n_ro_account_period_close.current_liabilities")
-        self.credit_acc = ref("l10n_ro_account_period_close.current_liabilities")
-        self.vat_paid = ref("l10n_ro_account_period_close.ova")
-        self.vat_received = ref("l10n_ro_account_period_close.iva")
-        self.vat_close_debit = ref("l10n_ro_account_period_close.cas")
-        self.vat_close_credit = ref("l10n_ro_account_period_close.current_liabilities")
-        self.exp_closing = self.per_close_model.create(
-            {
-                "name": "Closing Expenses",
-                "type": "expense",
-                "journal_id": self.misc_journal.id,
-                "debit_account_id": self.debit_acc.id,
-                "credit_account_id": self.credit_acc.id,
-            }
-        )
-        self.inc_closing = self.per_close_model.create(
-            {
-                "name": "Closing Incomes",
-                "type": "income",
-                "journal_id": self.misc_journal.id,
-                "close_result": True,
-                "debit_account_id": self.debit_acc.id,
-                "credit_account_id": self.credit_acc.id,
-            }
-        )
-        self.vat_closing = self.per_close_model.create(
-            {
-                "name": "Closing VAT",
-                "type": "selected",
-                "account_ids": [(6, 0, [self.vat_paid.id, self.vat_received.id])],
-                "journal_id": self.misc_journal.id,
-                "debit_account_id": self.vat_close_debit.id,
-                "credit_account_id": self.vat_close_credit.id,
-            }
-        )
+        cls.test_move.action_post()
 
     def test_period_closing(self):
         self.exp_closing._onchange_type()
@@ -224,7 +247,6 @@ class TestCurrencyReevaluation(TransactionCase):
         date_from = today + relativedelta(day=1, months=-1)
         date_to = today + relativedelta(day=1, days=-1)
         wizard = self.wiz_close_model.create({"closing_id": self.vat_closing.id})
-        self.assertEqual(wizard.company_id.id, self.env.ref("base.main_company").id)
         self.assertEqual(
             wizard.date_from.strftime("%Y-%m-%d"), fields.Date.to_string(date_from)
         )
