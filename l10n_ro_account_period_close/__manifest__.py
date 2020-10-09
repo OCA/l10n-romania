6fbf43aea6d8b8eb93d674f35b2277781f21e834
Mihai Fekete <feketemihai@odooerpromania.ro>
2020-10-09 16:56:41 +0300

    [MIG] l10n_ro_account_period_close: Migration to 14.0

--- l10n_ro_account_period_close/__manifest__.py
+++ l10n_ro_account_period_close/__manifest__.py
@@ -12,7 +12,7 @@
         "security/ir.model.access.csv",
     ],
     "license": "AGPL-3",
-    "version": "13.0.1.0.1",
+    "version": "14.0.1.0.0",
     "author": "NextERP Romania,"
     "Forest and Biomass Romania,"
     "Odoo Community Association (OCA)",
