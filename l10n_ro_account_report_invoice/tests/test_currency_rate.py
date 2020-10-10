32131b620c028918643d55a5d9337e08a9cacf1c
Mihai Fekete <feketemihai@odooerpromania.ro>
2020-10-10 08:55:16 +0300

    [MIG] l10n_ro_account_report_invoice: Migration to 14.0

--- l10n_ro_account_report_invoice/tests/test_currency_rate.py
+++ l10n_ro_account_report_invoice/tests/test_currency_rate.py
@@ -3,71 +3,27 @@
 
 import datetime
 
-from odoo import tools
-from odoo.modules.module import get_resource_path
-from odoo.tests.common import SavepointCase
+from odoo.tests import tagged
+from odoo.tests.common import Form
+
+from odoo.addons.account.tests.common import AccountTestInvoicingCommon
 
 
-class TestInvoiceCurrencyRate(SavepointCase):
+@tagged("post_install", "-at_install")
+class TestInvoiceCurrencyRate(AccountTestInvoicingCommon):
     @classmethod
-    def setUpClass(cls):
-        super().setUpClass()
-        cls.Company = cls.env["res.company"]
-        cls.CurrencyRate = cls.env["res.currency.rate"]
-        cls.partner_model = cls.env["res.partner"]
-        cls.invoice_model = cls.env["account.move"]
+    def setUpClass(cls, chart_template_ref=None):
+        super().setUpClass(chart_template_ref=chart_template_ref)
+
+        cls.invoice = cls.init_invoice(
+            "in_invoice", products=cls.product_a + cls.product_b
+        )
         cls.usd_currency = cls.env.ref("base.USD")
         cls.ron_currency = cls.env.ref("base.RON")
         cls.usd_currency.write({"rate": 1.0})
         cls.ron_currency.write({"active": True, "rate": 1.0})
-        # load account_minimal_test.xml file for chart of account in configuration
-        tools.convert_file(
-            cls.cr,
-            "l10n_ro_account_report_invoice",
-            get_resource_path("account", "test", "account_minimal_test.xml"),
-            {},
-            "init",
-            False,
-            "test",
-            cls.registry._assertion_report,
-        )
         cls.curr_year = datetime.date.today().year
         cls.prev_year = cls.curr_year - 1
-
-        # By default, tests are run with the current user set
-        # on the first company.
-        cls.partner1 = cls.partner_model.create(
-            {"name": "NextERP Romania", "vat": "RO39187746"}
-        )
-        default_line_account = cls.env["account.account"].search(
-            [
-                ("internal_type", "=", "other"),
-                ("deprecated", "=", False),
-                ("company_id", "=", cls.env.user.company_id.id),
-            ],
-            limit=1,
-        )
-        cls.invoice_line = [
-            (
-                0,
-                False,
-                {
-                    "name": "Test description #1",
-                    "product_id": cls.env.ref("product.product_delivery_01").id,
-                    "account_id": default_line_account.id,
-                    "quantity": 1.0,
-                    "price_unit": 100.0,
-                },
-            )
-        ]
-        cls.invoice = cls.invoice_model.create(
-            {
-                "partner_id": cls.partner1.id,
-                "type": "in_invoice",
-                "invoice_date": datetime.date(cls.curr_year, 1, 1),
-                "invoice_line_ids": cls.invoice_line,
-            }
-        )
 
     def test_invoice_currency_rate(self):
         """Test download file and partner link."""
@@ -75,12 +31,17 @@ class TestInvoiceCurrencyRate(SavepointCase):
         date_to = datetime.date(self.prev_year, 12, 31)
         today = datetime.date.today()
         self.env["res.currency.rate"].create(
-            dict(currency_id=self.usd_currency.id, name=date_to, rate=2)
+            dict(currency_id=self.ron_currency.id, name=date_to, rate=2)
         )
         self.env["res.currency.rate"].create(
-            dict(currency_id=self.usd_currency.id, name=today, rate=4)
+            dict(currency_id=self.ron_currency.id, name=today, rate=4)
         )
-        self.invoice.currency_id = self.usd_currency
+        move_form = Form(self.invoice)
+        move_form.partner_id = self.partner_a
+        move_form.invoice_date = date_to
+        move_form.currency_id = self.ron_currency
+        move_form.save()
         self.assertEqual(self.invoice.currency_rate, 0.5)
-        self.invoice.invoice_date = today
+        move_form.invoice_date = today
+        move_form.save()
         self.assertEqual(self.invoice.currency_rate, 0.25)
