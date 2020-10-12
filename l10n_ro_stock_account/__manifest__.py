b3b22b2f7f71fa53497f0a9525fd24dc467805b4
Mihai Fekete <feketemihai@odooerpromania.ro>
2020-10-12 10:25:57 +0300

    [MIG] l10n_ro_stock_account: Migration to 14.0

--- l10n_ro_stock_account/__manifest__.py
+++ l10n_ro_stock_account/__manifest__.py
@@ -1,7 +1,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
     "name": "Romania - Stock Accounting",
-    "version": "13.0.1.1.0",
+    "version": "14.0.1.0.0",
     "category": "Localization",
     "summary": "Romania - Stock Accounting",
     "author": "NextERP Romania,"
