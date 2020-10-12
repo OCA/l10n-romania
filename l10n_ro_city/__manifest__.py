141c7bbb720ed8899ed5115f37cce83244cc4939
Mihai Fekete <feketemihai@odooerpromania.ro>
2020-10-12 13:31:20 +0300

    [MIG] l10n_ro_city: Migration to 14.0

--- l10n_ro_city/__manifest__.py
+++ l10n_ro_city/__manifest__.py
@@ -5,7 +5,7 @@
     "name": "Romania - City",
     "summary": "Romania - City",
     "license": "AGPL-3",
-    "version": "13.0.1.0.1",
+    "version": "14.0.1.0.0",
     "author": "Terrabit,Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/l10n-romania",
     "category": "Localization",
