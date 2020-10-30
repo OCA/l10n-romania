e28a28842965c32a98b10a27e045791fa7399f66
Mihai Fekete <feketemihai@odooerpromania.ro>
2020-10-30 08:20:40 +0200

    [IMP] l10n_ro_hr: black, isort, prettier

--- l10n_ro_hr/__manifest__.py
+++ l10n_ro_hr/__manifest__.py
@@ -1,28 +1,26 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 {
-    'name': 'Romania - Human Resources',
-    'category': 'Localization',
-    'summary': 'Romania  - Human Resources',
-    'depends': ['hr', 'hr_employee_firstname'],
-    'external_dependencies': {
-        'python': ['stdnum'],
+    "name": "Romania - Human Resources",
+    "category": "Localization",
+    "summary": "Romania  - Human Resources",
+    "depends": ["hr", "hr_employee_firstname"],
+    "external_dependencies": {
+        "python": ["stdnum"],
     },
-    'data': [
-        'views/hr_employee_view.xml',
-        'views/res_company_view.xml',
-        'security/ir.model.access.csv',
+    "data": [
+        "views/hr_employee_view.xml",
+        "views/res_company_view.xml",
+        "security/ir.model.access.csv",
     ],
-    'demo': [
-        'data/hr_demo.xml'
-    ],
-    'license': 'AGPL-3',
-    'version': '11.0.1.0.0',
-    'author': 'NextERP Romania,'
-              'Forest and Biomass Romania,'
-              'Odoo Community Association (OCA)',
-    'website': 'https://github.com/OCA/l10n-romania',
-    'installable': True,
-    'development_status': 'Mature',
-    'maintainers': ['feketemihai'],
+    "demo": ["data/hr_demo.xml"],
+    "license": "AGPL-3",
+    "version": "11.0.1.0.0",
+    "author": "NextERP Romania,"
+    "Forest and Biomass Romania,"
+    "Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/l10n-romania",
+    "installable": True,
+    "development_status": "Mature",
+    "maintainers": ["feketemihai"],
 }
--- l10n_ro_hr/models/hr_employee.py
+++ l10n_ro_hr/models/hr_employee.py
@@ -3,7 +3,7 @@
 
 import logging
 
-from odoo import api, fields, models, _
+from odoo import _, api, fields, models
 from odoo.exceptions import ValidationError
 
 _logger = logging.getLogger(__name__)
