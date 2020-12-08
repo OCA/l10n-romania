7145822a4f774c25300b9586239a015e98ec5255
Dorin Hongu <dhongu@gmail.com>
2020-12-08 05:41:50 +0200

    generare note contabile pentru dropshipping

--- l10n_ro_stock_account/models/account_invoice.py
+++ l10n_ro_stock_account/models/account_invoice.py
@@ -133,14 +133,18 @@ class AccountMoveLine(models.Model):
                 purchase = self.purchase_order_id
                 if purchase and self.product_id.purchase_method == "receive":
                     # Control bills based on received quantities
-                    if any([p.notice for p in purchase.picking_ids]):
+                    if any(
+                        [p.notice or p._is_dropshipped() for p in purchase.picking_ids]
+                    ):
                         self = self.with_context(valued_type="invoice_in_notice")
             if self.move_id.is_sale_document():
                 sales = self.sale_line_ids
                 if sales and self.product_id.invoice_policy == "delivery":
                     # Control bills based on received quantities
                     sale = self.sale_line_ids[0].order_id
-                    if any([p.notice for p in sale.picking_ids]):
+                    if any(
+                        [p.notice and not p._is_dropshipped() for p in sale.picking_ids]
+                    ):
                         self = self.with_context(valued_type="invoice_out_notice")
         res = super(AccountMoveLine, self)._get_computed_account()
         # Take accounts from stock location in case the category allow changinc
