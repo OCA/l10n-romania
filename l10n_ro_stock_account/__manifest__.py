dabc0dcbdc2323dbe3ee31a14c4ac1081c363e73
Dorin Hongu <dhongu@gmail.com>
2021-01-20 16:09:37 +0200

    14.0-fix-stock-account

--- l10n_ro_stock_account/__manifest__.py
+++ l10n_ro_stock_account/__manifest__.py
@@ -1,7 +1,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
     "name": "Romania - Stock Accounting",
-    "version": "14.0.1.0.0",
+    "version": "14.0.1.0.1",
     "category": "Localization",
     "summary": "Romania - Stock Accounting",
     "author": "NextERP Romania,"
