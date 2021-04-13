8b418212d80123312c0cc4bdb10b105e4ab32a49
Nexterp <dev@cbssolutions.ro>
2021-04-13 23:25:16 +0300

    better autocomplete also nrc,phone,city, also nt vat companies

--- l10n_ro_partner_create_by_vat/models/res_partner.py
+++ l10n_ro_partner_create_by_vat/models/res_partner.py
@@ -11,8 +11,8 @@ from odoo import api, fields, models
 _logger = logging.getLogger(__name__)
 
 CEDILLATRANS = bytes.maketrans(
-    u"\u015f\u0163\u015e\u0162".encode("utf8"),
-    u"\u0219\u021b\u0218\u021a".encode("utf8"),
+    "\u015f\u0163\u015e\u0162".encode("utf8"),
+    "\u0219\u021b\u0218\u021a".encode("utf8"),
 )
 
 headers = {
@@ -45,12 +45,32 @@ class ResPartner(models.Model):
 
     @api.model
     def _Anaf_to_Odoo(self, result):
+        AnafFiled_OdooField_Overwrite = [
+            ("nrc", "nrRegCom", "over_all_the_time"),
+            ("zip", "codPostal", "over_all_the_time"),
+            ("comment", "stare_inregistrare", "if_empty+date"),
+            ("phone", "telefon", "if_empty"),
+        ]
         res = {
             "name": result["denumire"].upper(),
             "vat_subjected": result["scpTVA"],
             "company_type": "company",
         }
+        for field in AnafFiled_OdooField_Overwrite:
+            anaf_value = result.get(field[1], "")
+            if field[2] == "over_all_the_time":
+                res[field[0]] = anaf_value
+            elif field[2] == "if_empty+date" and anaf_value:
+                if not getattr(
+                    self, field[0], None
+                ):  # we are only writing if is not already a value
+                    res[field[0]] = f"UTC: {fields.datetime.now()}: " + anaf_value
+            elif field[2] == "if_empty" and anaf_value:
+                if not getattr(self, field[0], None):
+                    res[field[0]] = anaf_value
+
         addr = city = ""
+        state = False
         if result.get("adresa"):
             sector = False
             address = result["adresa"].replace("NR,", "NR.").upper()
@@ -88,29 +108,45 @@ class ResPartner(models.Model):
             if city:
                 res["city"] = city.replace("-", " ").title().strip()
         res["street"] = addr.strip()
-        return res
+        if "city_id" in self._fields and state and city:
+            res["city_id"] = (
+                self.env["res.city"]
+                .search([("state_id", "=", state.id), ("name", "ilike", city)], limit=1)
+                .id
+            )
+        return res, result.get("scpTVA")
 
     @api.onchange("vat", "country_id")
     def ro_vat_change(self):
         for partner in self:
+            ret = {}
             if not partner.vat:
-                return {}
+                return ret
             res = {}
             vat = partner.vat.strip().upper()
-            vat_country, vat_number = partner._split_vat(vat)
+            original_vat_country, vat_number = partner._split_vat(vat)
+            vat_country = original_vat_country.upper()
             if not vat_country and partner.country_id:
                 vat_country = self._map_vat_country_code(
                     partner.country_id.code.upper()
-                ).lower()
+                )
                 if not vat_number:
                     vat_number = partner.vat
-            if vat_country == "ro":
+            if vat_country == "RO":
+                # at_country=='' is for Romania companies that did not applied for vat tax
                 try:
                     result = partner._get_Anaf(vat_number)
                     if result:
-                        res = partner._Anaf_to_Odoo(result)
-                except Exception:
-                    _logger.info("ANAF Webservice not working.")
+                        res, scopTVA = partner._Anaf_to_Odoo(result)
+                except Exception as ex:
+                    warning = f"ANAF Webservice not working. Or exception={ex}."
+                    _logger.info(warning)
+                    ret["warning"] = {"message": warning}
+                if scopTVA != (original_vat_country == "ro"):
+                    ret["warning"] = {
+                        "message": f"On ANAF this cui/cif has scopTVA={scopTVA},\
+                                    but you gave us a code with wrong VAT marking (RO). "
+                    }
                 if res:
                     res["country_id"] = (
                         self.env["res.country"]
@@ -118,4 +154,4 @@ class ResPartner(models.Model):
                         .id
                     )
                     partner.with_context(skip_ro_vat_change=True).update(res)
-        return {}
+        return ret
