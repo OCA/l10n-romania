9433f3e5216431b11784b493916865efbe0dcc58
Dorin Hongu <dhongu@gmail.com>
2021-10-10 16:54:47 +0300

    upgrade module

--- l10n_ro_account_period_close/__manifest__.py
+++ l10n_ro_account_period_close/__manifest__.py
@@ -12,7 +12,7 @@
         "security/ir.model.access.csv",
     ],
     "license": "AGPL-3",
-    "version": "14.0.1.0.0",
+    "version": "15.0.1.0.0",
     "author": "NextERP Romania,"
     "Forest and Biomass Romania,"
     "Odoo Community Association (OCA)",
