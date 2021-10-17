2536ccc23728143a335507fadcb4230a1fd96c1a
Mihai Fekete <feketemihai@nexterp.ro>
2021-10-17 18:47:33 +0300

    [MIG] l10n_ro_partner_unique: Migration to 15.0

--- l10n_ro_partner_unique/tests/test_vat_nrc_unique.py
+++ l10n_ro_partner_unique/tests/test_vat_nrc_unique.py
@@ -3,10 +3,10 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 from odoo.exceptions import ValidationError
-from odoo.tests.common import SavepointCase
+from odoo.tests.common import TransactionCase
 
 
-class TestVatUnique(SavepointCase):
+class TestVatUnique(TransactionCase):
     @classmethod
     def setUpClass(cls):
         super(TestVatUnique, cls).setUpClass()
