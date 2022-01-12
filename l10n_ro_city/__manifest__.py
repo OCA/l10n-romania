5867b2dc61203886b6a9bc5f1583cf091dba7d5f
Dorin Hongu <dhongu@gmail.com>
2022-01-12 22:35:09 +0200

    add ro_city

---
+++ l10n_ro_city/__manifest__.py
@@ -0,0 +1,17 @@
+# Copyright (C) 2020 Terrabit
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html)
+
+{
+    "name": "Romania - City",
+    "summary": "Romania - City",
+    "license": "AGPL-3",
+    "version": "15.0.2.1.0",
+    "author": "Terrabit,Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/l10n-romania",
+    "category": "Localization",
+    "depends": ["base_address_city"],
+    "data": ["data/res_city.xml", "views/res_city_view.xml"],
+    "development_status": "Mature",
+    "installable": True,
+    "maintainers": ["dhongu"],
+}
