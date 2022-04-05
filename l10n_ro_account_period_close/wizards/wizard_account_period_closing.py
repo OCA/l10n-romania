9cea26f712dc8e0d71918c19c6c0f0496f5853c1
Mihai Fekete <feketemihai@nexterp.ro>
2022-04-05 08:36:31 +0300

    Allow to choose different journal in closing.

--- l10n_ro_account_period_close/wizards/wizard_account_period_closing.py
+++ l10n_ro_account_period_close/wizards/wizard_account_period_closing.py
@@ -25,12 +25,16 @@ class WizardAccountPeriodClosing(models.TransientModel):
     company_id = fields.Many2one(
         comodel_name="res.company", related="closing_id.company_id"
     )
-    journal_id = fields.Many2one(
-        comodel_name="account.journal", related="closing_id.journal_id"
-    )
+    journal_id = fields.Many2one(comodel_name="account.journal")
     date_range_id = fields.Many2one(comodel_name="date.range", string="Date range")
     date_from = fields.Date("Start Date", required=True, default=_get_default_date_from)
     date_to = fields.Date("End Date", required=True, default=_get_default_date_to)
+
+    @api.onchange("closing_id")
+    def onchange_closing_id(self):
+        """Handle closing_id change."""
+        if self.closing_id:
+            self.journal_id = self.closing_id.journal_id
 
     @api.onchange("date_range_id")
     def onchange_date_range_id(self):
