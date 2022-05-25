84484ceba941bccdc7bcebf605b16bca204bcc14
Cojocaru Marcel <marcel.cojocaru@gmail.com>
2022-05-25 13:56:01 +0300

    [14.0][FIX] fix cand anaf returneaza numele vechi al firmei

--- l10n_ro_partner_create_by_vat/__manifest__.py
+++ l10n_ro_partner_create_by_vat/__manifest__.py
@@ -14,5 +14,8 @@
     "website": "https://github.com/OCA/l10n-romania",
     "installable": True,
     "development_status": "Mature",
+    "data": [
+        "views/partner_view.xml",
+    ],        
     "maintainers": ["feketemihai"],
 }
