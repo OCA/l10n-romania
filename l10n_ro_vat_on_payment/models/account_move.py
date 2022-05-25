a7857f5049904a298b72b998050cb72a76946db4
Stana Natanaela <musatnatanaela@gmail.com>
2022-05-25 11:34:39 +0300

    pre-commit

--- l10n_ro_vat_on_payment/models/account_move.py
+++ l10n_ro_vat_on_payment/models/account_move.py
@@ -2,8 +2,9 @@
 # Copyright (C) 2020 NextERP Romania
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from odoo import api, models
 from datetime import date
+
+from odoo import api, models
 
 
 class AccountMove(models.Model):
