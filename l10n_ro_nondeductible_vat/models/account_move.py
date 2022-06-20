a5d1e77fb4894ae7b4998773da346ced78c1551a
Cojocaru Marcel <marcel.cojocaru@gmail.com>
2022-06-20 13:52:05 +0300

    fix dependency; check vat on payment for invoice date

--- l10n_ro_nondeductible_vat/models/account_move.py
+++ l10n_ro_nondeductible_vat/models/account_move.py
@@ -2,6 +2,8 @@
 # Copyright (C) 2021 NextERP Romania
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
+from datetime import date
+
 from odoo import api, models
 
 
@@ -73,12 +75,23 @@ class AccountMoveLine(models.Model):
 
     @api.onchange("tax_ids")
     def onchange_tax_ids(self):
-        if not self.display_type:
+        if not self.display_type and "in" in self.move_id.move_type:
             partner = (
                 self.env["res.partner"]._find_accounting_partner(self.partner_id)
                 or self.partner_id
             )
-            if partner.vat_on_payment:
+            ctx = dict(self._context)
+            vatp = False
+
+            if self.move_id.invoice_date:
+                ctx.update({"check_date": self.move_id.invoice_date})
+            else:
+                ctx.update({"check_date": date.today()})
+
+            if partner:
+                vatp = partner.with_context(ctx)._check_vat_on_payment()
+
+            if vatp:
                 taxes = self.tax_ids
 
                 if taxes and self.move_id.fiscal_position_id:
