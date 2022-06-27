ccd357ccb98c00dcc3b16da2088ab8799dba8582
Dorin Hongu <dhongu@gmail.com>
2022-06-27 04:37:15 +0300

    fix precommit

--- l10n_ro_e_invoice/controllers/anaf_oauth.py
+++ l10n_ro_e_invoice/controllers/anaf_oauth.py
@@ -1,9 +1,11 @@
-from odoo import http
-from odoo.http import request
 import secrets
 from datetime import datetime, timedelta
+
 import requests
-import json
+
+from odoo import http
+from odoo.http import request
+
 # Authorization Endpoint https://logincert.anaf.ro/anaf-oauth2/v1/authorize
 # Token Issuance Endpoint https://logincert.anaf.ro/anaf-oauth2/v1/token
 # Token Revocation Endpoint https://logincert.anaf.ro/anaf-oauth2/v1/revoke
@@ -26,11 +28,17 @@ class WebsitePageDepositKpi(http.Controller):
         if not company.exists():
             return "Error, this company does not exist!"
         elif not company.client_id or not company.client_secret:
-            return f"Error, on company {company.name} you does not have a Oauth client_id or client_secret for anaf.ro!"
+            return (
+                f"Error, on company {company.name} you does not have a "
+                f"Oauth client_id or client_secret for anaf.ro!"
+            )
 
         secret = secrets.token_urlsafe(16)
         now = datetime.now()
-        if company.anaf_request_datetime + timedelta(seconds=30)> now :
+        if (
+            company.anaf_request_datetime
+            and company.anaf_request_datetime + timedelta(seconds=30) > now
+        ):
             return "You can make only one request per minute"
         company.write(
             {
@@ -40,67 +48,77 @@ class WebsitePageDepositKpi(http.Controller):
         )
         anafOauth = (
             "https://logincert.anaf.ro/anaf-oauth2/v1/authorize"
-            "?response_type=code" 
+            "?response_type=code"
             f"&client_id={company.client_id}"
             f"&client_secret={company.client_secret}"
             f"&redirect_uri={user.get_base_url()+'/anaf_oauth'}"
-            f"&scope={secret}"  
+            f"&scope={secret}"
             "&grant_type=authorization_code"  # not necessary?
-
         )
-# ************ working**********
-# alex: here should be POST and not get, I try them in comments below, but did not work
+        # ************ working**********
+        # alex: here should be POST and not get, I try them in comments below, but did not work
         anaf_request_from_redirect = request.redirect(anafOauth, code=302, local=False)
-        """
-This is the default for Authorization Code grant. A successful response is 302 Found which triggers a redirect to the redirect_uri. The response parameters are embedded in the query component (the part after ?) of the redirect_uri in the Location header.
-For example:
-HTTP/1.1 302 Found
-Location: https://my-redirect-uri.callback?code=js89p2x1 where the authorization code is js89p21.
-"""
 
+        # This is the default for Authorization Code grant.
+        # A successful response is 302 Found which triggers a redirect to the redirect_uri.
+        # The response parameters are embedded in the query component (the part after ?)
+        # of the redirect_uri in the Location header.
+        # For example:
+        # HTTP/1.1 302 Found
+        # Location: https://my-redirect-uri.callback?code=js89p2x1
+        # where the authorization code is js89p21.
 
         return anaf_request_from_redirect
-        
+
     @http.route(
-        ["/anaf_oauth"],
-        type="http",
-        auth="public",
-        website=True,
-        sitemap=False,
+        ["/anaf_oauth"], type="http", auth="public", website=True, sitemap=False
     )
     def get_anaf_oauth_code(self, **kw):
-        "returns a text with the result of anaf request from redirect" 
+        "returns a text with the result of anaf request from redirect"
         uid = request.uid
         user = request.env["res.users"].browse(uid)
         now = datetime.now()
         Companies = request.env["res.company"].sudo()
         company = Companies.search(
-            [("client_id", "!=", "")
-             #, ('anaf_request_datetime',">",now-timedelta(minutes=1))
-             ], limit =1)
+            [
+                ("client_id", "!=", "")
+                # , ('anaf_request_datetime',">",now-timedelta(minutes=1))
+            ],
+            limit=1,
+        )
         error = ""
         if len(company) > 1:
-            ret = f"UTC{str(now)[:19]} found {company} that have done request to anaf. We can not set to more than one at a time. Rreponse was {kw}\n"
+            ret = (
+                f"UTC{str(now)[:19]} found {company} that have done request to anaf."
+                f" We can not set to more than one at a time. response was {kw}\n"
+            )
             for comp in company:
-                resp = ret 
+                resp = ret
                 comp.write({"other_responses": resp + (company.other_responses or "")})
             return ret
         elif not company:
-            return f"You can not modify anything, the reponse is to late.\n Response was: kw={kw}"
+            return (
+                f"You can not modify anything, the response is to late.\n"
+                f" Response was: kw={kw}"
+            )
         code = kw.get("code")
         if code:
-            ret= f"UTC{str(now)[:19]} All is OK reponse kw={kw}\n" 
+            ret = f"UTC{str(now)[:19]} All is OK response kw={kw}\n"
 
-            headers = { 'content-type': 'application/x-www-form-urlencoded' }
-            data={ "grant_type": 'authorization_code',
-                  "client_id": f"{company.client_id}",
-                  "client_secret": f"{company.client_secret}",
-                  "code": f'{code}',
-                  "redirect_uri": f"{user.get_base_url()+'/anaf_oauth'}" }
-        
-            response = requests.post("https://logincert.anaf.ro/anaf-oauth2/v1/token",
-                         data=data,
-                         headers=headers)
+            headers = {"content-type": "application/x-www-form-urlencoded"}
+            data = {
+                "grant_type": "authorization_code",
+                "client_id": f"{company.client_id}",
+                "client_secret": f"{company.client_secret}",
+                "code": f"{code}",
+                "redirect_uri": f"{user.get_base_url()+'/anaf_oauth'}",
+            }
+
+            response = requests.post(
+                "https://logincert.anaf.ro/anaf-oauth2/v1/token",
+                data=data,
+                headers=headers,
+            )
             response_json = response.json()
             ret += f" {response_json=}\n\n"
             company.write(
@@ -108,14 +126,16 @@ Location: https://my-redirect-uri.callback?code=js89p2x1 where the authorization
                     "code": code,
                     "client_token_valability": now + timedelta(days=89),
                     "other_responses": ret + (company.other_responses or ""),
-                    "access_token":response_json.get("access_token",""),
-                    "refresh_token":response_json.get("refresh_token",""),
-                })
+                    "access_token": response_json.get("access_token", ""),
+                    "refresh_token": response_json.get("refresh_token", ""),
+                }
+            )
             return ret
         else:
-            error = f"UTC{str(now)[:19]} BAD reponse no code in reponse kw={kw}\n" 
-            company.write({"other_responses": error  + (company.other_responses or "")})
-            return f"UTC{str(now)[:19]} BAD reponse no 'code' in reponse kw={kw}\n"
+            error = f"UTC{str(now)[:19]} BAD response no code in response kw={kw}\n"
+            company.write({"other_responses": error + (company.other_responses or "")})
+            return f"UTC{str(now)[:19]} BAD response no 'code' in response kw={kw}\n"
+
 
 # post request to refresh the token must be
 # grant_type=refresh_token
--- l10n_ro_e_invoice/models/res_company.py
+++ l10n_ro_e_invoice/models/res_company.py
@@ -1,9 +1,14 @@
 # Copyright (C) 2022extERP Romania
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from odoo import _, fields, models
+import logging
+
 import requests
-import json        
+
+from odoo import fields, models
+
+_logger = logging.getLogger(__name__)
+
 
 class ResCompany(models.Model):
     _inherit = ["mail.thread", "res.company"]
@@ -11,40 +16,40 @@ class ResCompany(models.Model):
 
     anaf_callback_url = fields.Char(
         compute="_compute_anaf_callback_url",
-        help="This is the address to set in anaf_portal_url ( and will work if is https & accesible form internet)",
+        help="This is the address to set in anaf_portal_url "
+        "( and will work if is https & accessible form internet)",
     )
     client_id = fields.Char(
-        help="From anaf site the Oauth id - view the readme", tracking=1
+        help="From ANAF site the Oauth id - view the readme", tracking=1
     )
     client_secret = fields.Char(
-        help="From anaf site the Oauth id - view the readme", tracking=1
+        help="From ANAF site the Oauth id - view the readme", tracking=1
     )
     code = fields.Char(
-        help="Recived from ANAF with this you can take access toekn and refresh_token", tracking=1
+        help="Received from ANAF with this you can take access token and refresh_token",
+        tracking=1,
     )
 
-    access_token = fields.Char(
-        help="Recived from ANAF ", tracking=1,readonly=1
-    )
-    refresh_token = fields.Char(
-        help="Recived from ANAF ", tracking=1,readonly=1
-    )
+    access_token = fields.Char(help="Received from ANAF", tracking=1, readonly=1)
+    refresh_token = fields.Char(help="Received from ANAF", tracking=1, readonly=1)
 
     client_token_valability = fields.Date(
-        help="date when is going to exprire - 90 days from when was generated",
+        help="Date when is going to expire - 90 days from when was generated",
         readonly=1,
-        tracking=1,
+        tracking=1
     )
 
     response_secret = fields.Char(
         help="A generated secret to know that the response is ok", readonly=1
     )
     anaf_request_datetime = fields.Datetime(
-        help="Time when was last time pressed the Get Token From Anaf Website. It waits for anaf request for maximum 1 minute",
-        readonly=1,
+        help="Time when was last time pressed the Get Token From Anaf Website."
+        " It waits for ANAF request for maximum 1 minute",
+        readonly=1
     )
     other_responses = fields.Text(
-        help="This are request to the page /anaf_oauth that are not finished with a received token. Date is in UTC"
+        help="This are request to the page /anaf_oauth that are not finished"
+        " with a received token. Date is in UTC"
     )
 
     def _compute_anaf_callback_url(self):
@@ -61,45 +66,54 @@ class ResCompany(models.Model):
 
     def test_anaf_api(self):
         self.ensure_one()
-        param = {
-            # "client_id":f"{self.client_id}",
-            # "token":f"{self.client_received_token}",
-            # "code":f"{self.client_received_token}",
-            # "key":f"{self.client_received_token}",
-            # "access_token":f"{self.client_received_token}",
-           # "name":"xx",
-            }
+        # param = {
+        #     # "client_id":f"{self.client_id}",
+        #     # "token":f"{self.client_received_token}",
+        #     # "code":f"{self.client_received_token}",
+        #     # "key":f"{self.client_received_token}",
+        #     # "access_token":f"{self.client_received_token}",
+        #     # "name":"xx",
+        # }
         # it should work, but is giving 403 Forbidden
-        url = 'https://api.anaf.ro/TestOauth/jaxrs/hello?name=yy'
-#        url = "https://api.anaf.ro/test/FCTEL/rest/listaMesajeFactura"
-#        url = "https://api.anaf.ro/test/FCTEL/rest/descarcare" # id = index descarare factura
-#        url = "https://api.anaf.ro/test/ETRANSPORT/ws/v1/lista/{1}/{}" 
-        response = requests.get(url,
-#        response = requests.post(url,
-#                                data={"id":1},
-               # json=json.dumps(param), 
-                headers={#'Content-Type': 'application/json',
-                         'Content-Type': 'multipart/form-data',
-                         #'Accept': 'application/json',
-                         #"client_id": self.client_id,
-                         "Authorization": f"Bearer {self.access_token}"
-                         },            
-                timeout = 80,
-#                allow_redirects=True
-                )
+        url = "https://api.anaf.ro/TestOauth/jaxrs/hello?name=yy"
+        #  url = "https://api.anaf.ro/test/FCTEL/rest/listaMesajeFactura"
+        #  url = "https://api.anaf.ro/test/FCTEL/rest/descarcare"
+        #       id = index descarare factura
+        #  url = "https://api.anaf.ro/test/ETRANSPORT/ws/v1/lista/{1}/{}"
+
+        response = requests.get(
+            url,
+            #        response = requests.post(url,
+            #                                data={"id":1},
+            # json=json.dumps(param),
+            headers={  # 'Content-Type': 'application/json',
+                "Content-Type": "multipart/form-data",
+                # 'Accept': 'application/json',
+                # "client_id": self.client_id,
+                "Authorization": f"Bearer {self.access_token}",
+            },
+            timeout=80,
+            #                allow_redirects=True
+        )
         # return f"{response.content=}\n{response.headers=}"
-        anaf_hello_test = f"UTC{str(fields.datetime.now())[:19]}{response.url=}\n{response.reason=}\n{response.content=}\n{response.status_code=}\n{response.headers=}\n{response.request.headers=}\n{response.request.method=}\n"
-        print(anaf_hello_test)
-        self.write({"other_responses":anaf_hello_test + self.other_responses})
-        return 
+        anaf_hello_test = (
+            f"UTC{str(fields.datetime.now())[:19]}{response.url=}\n"
+            f"{response.reason=}\n{response.content=}\n"
+            f"{response.status_code=}\n{response.headers=}\n"
+            f"{response.request.headers=}\n{response.request.method=}\n"
+        )
+
+        _logger.info(anaf_hello_test)
+        self.write({"other_responses": anaf_hello_test + self.other_responses})
+        return
         # for a popup in future, or put in a text field?
-        return {
-                'name': 'My Window',
-                'domain': [],
-                'res_model': 'my.model',
-                'type': 'ir.actions.act_window',
-                'view_mode': 'form',
-                'view_type': 'form',
-                'context': {},
-                'target': 'new',
-            }
+        # return {
+        #     "name": "My Window",
+        #     "domain": [],
+        #     "res_model": "my.model",
+        #     "type": "ir.actions.act_window",
+        #     "view_mode": "form",
+        #     "view_type": "form",
+        #     "context": {},
+        #     "target": "new",
+        # }
