b9160d6816b97a0f81f49b5a3fcb9bc3e4f0b89a
Stana Natanaela <musatnatanaela@gmail.com>
2022-07-15 14:39:42 +0300

    improve l10n_ro_partner_unique (#244)

--- l10n_ro_partner_unique/models/res_partner.py
+++ l10n_ro_partner_unique/models/res_partner.py
@@ -10,17 +10,23 @@ class ResPartner(models.Model):
 
     @api.model
     def _get_vat_nrc_constrain_domain(self):
-        domain = [('parent_id', '=', False),
-                  ('vat', '=', self.vat),
-                  ('nrc', '=', self.nrc)]
+        domain = [
+            ("company_id", "=", self.company_id.id if self.company_id else False),
+            ("parent_id", "=", False),
+            ("vat", "=", self.vat),
+            "|",
+            ("nrc", "=", self.nrc),
+            ("nrc", "=", False),
+        ]
         return domain
 
     @api.constrains('vat', 'nrc')
     def _check_vat_nrc_unique(self):
         for record in self:
-            domain = record._get_vat_nrc_constrain_domain()
-            results = self.env['res.partner'].search(domain)
-            if len(results) > 1:
-                raise ValidationError(
-                    _("The VAT and NRC pair (%s, %s) must be unique!") %
-                    (record.vat, record.nrc))
+            if record.vat:
+                domain = record._get_vat_nrc_constrain_domain()
+                results = self.env['res.partner'].search(domain)
+                if len(results) > 1:
+                    raise ValidationError(
+                        _("The VAT and NRC pair (%s, %s) must be unique ids=%s!") %
+                        (record.vat, record.nrc, record.ids))
