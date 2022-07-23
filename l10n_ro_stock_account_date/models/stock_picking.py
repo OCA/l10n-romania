4a46164a3357f97e748c95eba0e8228a3799c888
Mihai Fekete <feketemihai@nexterp.ro>
2022-07-23 21:05:01 +0300

    [14.0] [ADD] Add modules for stock accounting date.

---
+++ l10n_ro_stock_account_date/models/stock_picking.py
@@ -0,0 +1,36 @@
+# Copyright (C) 2022 NextERP Romania SRL
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from odoo import _, fields, models
+from odoo.exceptions import ValidationError
+
+
+class StockPicking(models.Model):
+    _inherit = "stock.picking"
+
+    accounting_date = fields.Datetime(
+        "Accounting Date",
+        copy=False,
+        help="If this field is set, the svl and accounting entiries will "
+        "have this date, If not will have the today date as it should be",
+        tracking=True,
+    )
+
+    def _action_done(self):
+        """Update date_done from accounting_date field"""
+        res = super()._action_done()
+        for picking in self:
+            if picking.accounting_date:
+                if picking.accounting_date.date() > fields.date.today():
+                    raise ValidationError(
+                        _(
+                            "You can not have a Accounting date=%s for picking "
+                            "bigger than today!" % picking.accounting_date.date()
+                        )
+                    )
+                picking.write(
+                    {
+                        "date_done": picking.accounting_date,
+                    }
+                )
+        return res
