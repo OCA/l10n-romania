341a78f415114cb07943c9ec28e5e3e5f9a08a0e
Mihai Fekete <feketemihai@nexterp.ro>
2022-07-23 14:21:19 +0300

    Merge 14 in 15

--- l10n_ro_config/models/res_partner.py
+++ l10n_ro_config/models/res_partner.py
@@ -8,6 +8,22 @@ class ResPartner(models.Model):
     _inherit = "res.partner"
 
     vat_subjected = fields.Boolean("VAT Legal Statement")
+    vat_number = fields.Char(
+        "VAT number",
+        compute="_compute_vat_number",
+        store=True,
+        help="VAT number without country code.",
+    )
+    caen_code = fields.Char(default="0000")
+    l10n_ro_e_invoice = fields.Boolean(string="Ro E-Invoicing", copy=False)
+
+    @api.depends("vat")
+    def _compute_vat_number(self):
+        for partner in self:
+            if partner.vat:
+                partner.vat_number = self._split_vat(partner.vat)[1]
+            else:
+                partner.vat_number = ""
 
     def _map_vat_country_code(self, country_code):
         country_code_map = {
@@ -23,12 +39,12 @@ class ResPartner(models.Model):
         # Allowing setting the vat without country code
         vat_country = vat_number = ""
         if vat and vat.isdigit():
+            vat_number = vat
             partner = self.search([("vat", "=", vat)], limit=1)
             if partner and partner.country_id:
                 vat_country = self._map_vat_country_code(
                     partner.country_id.code.upper()
                 ).lower()
-                vat_number = vat
         else:
             vat_country, vat_number = super(ResPartner, self)._split_vat(vat)
         return vat_country, vat_number
