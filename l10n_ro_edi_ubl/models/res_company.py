6869c8ab01770e4a0a35c8d63a6f1cfc44238edd
Dorin Hongu <dhongu@gmail.com>
2022-07-28 14:55:06 +0300

    adaugare modul generea xml factura electronica

---
+++ l10n_ro_edi_ubl/models/res_company.py
@@ -0,0 +1,11 @@
+# ©  2008-2022 Dorin Hongu <dhongu(@)gmail(.)com
+# See README.rst file on addons root folder for license details
+from odoo import fields, models
+
+
+class ResCompany(models.Model):
+    _inherit = "res.company"
+
+    l10n_ro_edi_access_token = fields.Char(string="Access Token")
+    l10n_ro_edi_test_mode = fields.Boolean(string="Test mode", default=True)
+    l10n_ro_edi_manual = fields.Boolean(string="Manual submission", default=True)
