7390e51d87514da2e15e3ec06d47a38f8f2c9c4f
Dorin Hongu <dhongu@gmail.com>
2022-07-29 07:41:03 +0300

    modificari din 15

--- l10n_ro_stock_price_difference/models/account_move_line.py
+++ l10n_ro_stock_price_difference/models/account_move_line.py
@@ -16,18 +16,17 @@ class AccountMoveLine(models.Model):
     def get_stock_valuation_difference(self):
         """Se obtine diferenta dintre evaloarea stocului si valoarea din factura"""
         line = self
-
+        diff, qty_diff = 0.0, 0.0
         # Retrieve stock valuation moves.
         if not line.purchase_line_id:
-            return 0.0
+            return diff, qty_diff
 
         if line.purchase_line_id.product_id.purchase_method != "receive":
-            return 0.0
+            return diff, qty_diff
 
         valuation_stock_moves = self._get_valuation_stock_moves()
-
         if not valuation_stock_moves:
-            return 0.0
+            return diff, qty_diff
 
         valuation_total = 0
         valuation_total_qty = 0
@@ -45,7 +44,7 @@ class AccountMoveLine(models.Model):
 
         precision = line.product_uom_id.rounding or line.product_id.uom_id.rounding
         if float_is_zero(valuation_total_qty, precision_rounding=precision):
-            return 0.0
+            return diff, qty_diff
 
         lines = self.search(
             [
@@ -58,8 +57,13 @@ class AccountMoveLine(models.Model):
             inv_qty += (
                 -1 if line.move_id.move_type == "in_refund" else 1
             ) * line.quantity
-        accc_balance = sum(lines.mapped("balance")) / inv_qty * valuation_total_qty
+        if inv_qty * valuation_total_qty:
+            accc_balance = sum(lines.mapped("balance")) / inv_qty * valuation_total_qty
+        else:
+            accc_balance = 0
         diff = abs(accc_balance) - valuation_total
+        currency = line.currency_id or self.env.company.currency_id
+        diff = currency.round(diff)
         qty_diff = inv_qty - valuation_total_qty
         return diff, qty_diff
 
