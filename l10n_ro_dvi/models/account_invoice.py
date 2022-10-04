ec54ca783c73b7fa2beeee1d284fa67a86f216f3
Mihai Fekete <feketemihai@nexterp.ro>
2022-10-04 10:45:12 +0300

    [14.0] [MIG] Migrate l10n_ro_dvi

--- l10n_ro_dvi/models/account_invoice.py
+++ l10n_ro_dvi/models/account_invoice.py
@@ -1,25 +1,39 @@
 # Copyright (C) 2020 Terrabit
+# Copyright (C) 2022 NextERP Romania
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html)
 
 
-from odoo import fields, models
+from odoo import _, fields, models
+from odoo.exceptions import ValidationError
 
 
 class AccountInvoice(models.Model):
     _inherit = "account.move"
 
-    dvi_id = fields.Many2one("stock.landed.cost", string="DVI")
+    l10n_ro_dvi_ids = fields.Many2many(
+        "stock.landed.cost",
+        compute="_compute_l10n_ro_dvi_ids",
+        string="Romania - DVI's",
+        help="DVI's linked to this invoice",
+    )
 
-    def button_dvi(self):
-        if self.dvi_id:
-            # afisare DVI
-            action = self.env.ref("stock_landed_costs.action_stock_landed_cost")
-            action = action.read()[0]
-            action["views"] = [(False, "form")]
-            action["res_id"] = self.dvi_id.id
-        else:
-            # generare dvi
-            action = self.env.ref("l10n_ro_dvi.action_account_invoice_dvi")
-            action = action.read()[0]
+    def _compute_l10n_ro_dvi_ids(self):
+        for invoice in self:
+            invoice.l10n_ro_dvi_ids = [
+                (
+                    6,
+                    0,
+                    self.env["l10n.ro.account.dvi"]
+                    .search([("invoice_ids", "=", invoice.id)])
+                    .ids,
+                )
+            ]
 
+    def action_view_dvis(self):
+        self.ensure_one()
+        if not self.l10n_ro_dvi_ids:
+            raise ValidationError(_("You do not have created DVI's for this invoice"))
+        action = self.env.ref("l10n_ro_dvi.action_account_dvi")
+        action = action.sudo().read()[0]
+        action["domain"] = [("id", "in", self.l10n_ro_dvi_ids.ids)]
         return action
