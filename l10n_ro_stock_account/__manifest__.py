b81b9766b295f64aab2324b7df2e23e7de0c74d1
Mihai Fekete <feketemihai@nexterp.ro>
2022-10-12 12:17:14 +0300

    Isolate l10n_ro_stock_account

--- l10n_ro_stock_account/__manifest__.py
+++ l10n_ro_stock_account/__manifest__.py
@@ -1,7 +1,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
     "name": "Romania - Stock Accounting",
-    "version": "15.0.1.6.0",
+    "version": "15.0.1.6.1",
     "category": "Localization",
     "summary": "Romania - Stock Accounting",
     "author": "NextERP Romania,"
@@ -10,22 +10,25 @@
     "Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/l10n-romania",
     "depends": [
-        "stock_account",
+        "stock_landed_costs",
         "sale_stock",
         "purchase_stock",
         "l10n_ro_config",
         "l10n_ro_stock",
     ],
     "license": "AGPL-3",
-    "development_status": "Mature",
     "data": [
+        "views/account_account_view.xml",
         "views/product_category_view.xml",
         "views/product_template_view.xml",
+        "views/stock_landed_cost_view.xml",
         "views/stock_location_view.xml",
         "views/stock_picking_view.xml",
         "views/stock_valuation_layer_views.xml",
         "wizard/stock_picking_return_views.xml",
     ],
+    "pre_init_hook": "pre_init_hook",
     "installable": True,
+    "development_status": "Mature",
     "maintainers": ["dhongu", "feketemihai"],
 }
