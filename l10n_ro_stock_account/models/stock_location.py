aff2851ab42d22e6d8825fbf0a64bc7647387fd3
Mihai Fekete <feketemihai@nexterp.ro>
2022-10-12 12:17:14 +0300

    Isolate l10n_ro_stock_account

--- l10n_ro_stock_account/models/stock_location.py
+++ l10n_ro_stock_account/models/stock_location.py
@@ -9,19 +9,19 @@ from odoo import fields, models
 class StockLocation(models.Model):
     _inherit = "stock.location"
 
-    property_account_income_location_id = fields.Many2one(
+    l10n_ro_property_account_income_location_id = fields.Many2one(
         "account.account",
         company_dependent=True,
-        string="Income Account",
+        string="Romania - Income Account",
         domain="['&', ('deprecated', '=', False),"
         "('company_id', '=', current_company_id)]",
         help="This account will overwrite the income accounts from product "
         "or category.",
     )
-    property_account_expense_location_id = fields.Many2one(
+    l10n_ro_property_account_expense_location_id = fields.Many2one(
         "account.account",
         company_dependent=True,
-        string="Expense Account",
+        string="Romania - Expense Account",
         domain="['&', ('deprecated', '=', False),"
         "('company_id', '=', current_company_id)]",
         help="This account will overwrite the expense accounts from product "
@@ -31,9 +31,9 @@ class StockLocation(models.Model):
     # se va folosi   pentru a evalua diferit un produs care se
     # gaseste in aceasta locatie
 
-    property_stock_valuation_account_id = fields.Many2one(
+    l10n_ro_property_stock_valuation_account_id = fields.Many2one(
         "account.account",
-        string="Stock Valuation Account",
+        string="Romania - Stock Valuation Account",
         company_dependent=True,
         domain="[('company_id', '=', current_company_id),"
         "('deprecated', '=', False)]",
