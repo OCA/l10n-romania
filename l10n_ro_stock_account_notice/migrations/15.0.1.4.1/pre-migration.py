7d8da44d963bf1d40591596746d0f6e620ebae66
Mihai Fekete <feketemihai@nexterp.ro>
2022-10-15 09:18:47 +0300

    15.0 Isolate l10n_ro_stock_account_notice

---
+++ l10n_ro_stock_account_notice/migrations/15.0.1.4.1/pre-migration.py
@@ -0,0 +1,35 @@
+# Copyright (C) 2022 NextERP Romania
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+import sys
+
+
+def install(package):
+    try:
+        __import__(package)
+    except Exception:
+        import subprocess
+
+        subprocess.call([sys.executable, "-m", "pip", "install", package])
+
+
+install("openupgradelib")
+
+try:
+    from openupgradelib import openupgrade
+except ImportError:
+    openupgrade = None
+
+
+@openupgrade.migrate(use_env=True)
+def migrate(env, version):
+    openupgrade.rename_fields(
+        env,
+        [
+            (
+                "stock.picking",
+                "stock_picking",
+                "notice",
+                "l10n_ro_notice",
+            ),
+        ],
+    )
