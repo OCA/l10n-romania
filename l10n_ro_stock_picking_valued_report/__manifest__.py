4fed35d27ffa5fb811303816a53e71b6d5023be3
Mihai Fekete <feketemihai@nexterp.ro>
2022-10-15 09:38:16 +0300

    15.0 Isolate l10n_ro_stock_picking_valued_report

--- l10n_ro_stock_picking_valued_report/__manifest__.py
+++ l10n_ro_stock_picking_valued_report/__manifest__.py
@@ -2,14 +2,12 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
     "name": "Romania - Stock Picking Valued Report",
-    "version": "15.0.1.1.0",
+    "version": "15.0.1.1.1",
     "category": "Localization",
     "summary": "Romania -  Stock Picking Valued Report",
     "author": "NextERP Romania," "Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/l10n-romania",
     "depends": [
-        "sale_stock",
-        "purchase_stock",
         "l10n_ro_stock_account",
         "stock_landed_costs",
     ],
@@ -19,5 +17,6 @@
         "report/stock_picking_report_valued.xml",
     ],
     "installable": True,
+    "development_status": "Mature",
     "maintainers": ["feketemihai"],
 }
