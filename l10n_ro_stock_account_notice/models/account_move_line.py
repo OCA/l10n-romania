dad27de47b57514abd3d65f10e1469ac3bc6a773
Mihai Fekete <feketemihai@nexterp.ro>
2022-10-15 08:55:00 +0300

    15.0 Isolate l10n_ro_stock_account_notice

--- l10n_ro_stock_account_notice/models/account_move_line.py
+++ l10n_ro_stock_account_notice/models/account_move_line.py
@@ -10,19 +10,20 @@ _logger = logging.getLogger(__name__)
 
 
 class AccountMoveLine(models.Model):
-    _inherit = "account.move.line"
+    _name = "account.move.line"
+    _inherit = ["account.move.line", "l10n.ro.mixin"]
 
     def _get_computed_account(self):
-        if (
-            self.product_id.type == "product"
-            and self.move_id.company_id.anglo_saxon_accounting
-        ):
+        if self.product_id.type == "product" and self.is_l10n_ro_record:
             if self.move_id.is_purchase_document():
                 purchase = self.purchase_order_id
                 if purchase and self.product_id.purchase_method == "receive":
                     # Control bills based on received quantities
                     if any(
-                        [p.notice or p._is_dropshipped() for p in purchase.picking_ids]
+                        [
+                            p.l10n_ro_notice or p._is_dropshipped()
+                            for p in purchase.picking_ids
+                        ]
                     ):
                         self = self.with_context(valued_type="invoice_in_notice")
             if self.move_id.is_sale_document():
@@ -31,7 +32,10 @@ class AccountMoveLine(models.Model):
                     # Control bills based on received quantities
                     sale = self.sale_line_ids[0].order_id
                     if any(
-                        [p.notice and not p._is_dropshipped() for p in sale.picking_ids]
+                        [
+                            p.l10n_ro_notice and not p._is_dropshipped()
+                            for p in sale.picking_ids
+                        ]
                     ):
                         self = self.with_context(valued_type="invoice_out_notice")
         return super(AccountMoveLine, self)._get_computed_account()
