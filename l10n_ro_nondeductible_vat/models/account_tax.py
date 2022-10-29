c692b71adf4da1cefd0d72424bd59cc049a411f9
Mihai Fekete <feketemihai@nexterp.ro>
2022-10-29 08:25:22 +0300

    15.0 IMP Isolate l10n_ro_nondeductible_vat

--- l10n_ro_nondeductible_vat/models/account_tax.py
+++ l10n_ro_nondeductible_vat/models/account_tax.py
@@ -6,29 +6,39 @@ from odoo import api, fields, models
 
 
 class AccountTaxExtend(models.Model):
-    _inherit = "account.tax"
+    _name = "account.tax"
+    _inherit = ["account.tax", "l10n.ro.mixin"]
 
-    nondeductible_tax_id = fields.Many2one("account.tax", copy=False)
-    is_nondeductible = fields.Boolean(
-        compute="_compute_boolean_nondeductible", store=True
+    l10n_ro_nondeductible_tax_id = fields.Many2one(
+        "account.tax", copy=False, string="Romania - Nondeductible Tax"
+    )
+    l10n_ro_is_nondeductible = fields.Boolean(
+        string="Romania - Is Nondeductible",
+        compute="_compute_boolean_l10n_ro_nondeductible",
+        store=True,
     )
 
     @api.depends("invoice_repartition_line_ids", "refund_repartition_line_ids")
-    def _compute_boolean_nondeductible(self):
+    def _compute_boolean_l10n_ro_nondeductible(self):
         for record in self:
-            record.is_nondeductible = any(
-                record.invoice_repartition_line_ids.mapped("nondeductible")
-                + record.refund_repartition_line_ids.mapped("nondeductible")
-            )
+            if record.is_l10n_ro_record:
+                record.l10n_ro_is_nondeductible = any(
+                    record.invoice_repartition_line_ids.mapped("l10n_ro_nondeductible")
+                    + record.refund_repartition_line_ids.mapped("l10n_ro_nondeductible")
+                )
+            else:
+                record.l10n_ro_is_nondeductible = False
 
 
 class AccountTaxRepartitionLineExtend(models.Model):
-    _inherit = "account.tax.repartition.line"
+    _name = "account.tax.repartition.line"
+    _inherit = ["account.tax.repartition.line", "l10n.ro.mixin"]
 
-    nondeductible = fields.Boolean()
-    exclude_from_stock = fields.Boolean()
-    skip_cash_basis_account_switch = fields.Boolean(
+    l10n_ro_nondeductible = fields.Boolean(string="Romania - Nondeductible")
+    l10n_ro_exclude_from_stock = fields.Boolean(string="Romania - Exclude From Stock")
+    l10n_ro_skip_cash_basis_account_switch = fields.Boolean(
+        string="Romania - Skip Account Switch (Cash Basis)",
         help="If checked, then it doesn't change expense account"
         " in the tax line for invoices, and it set 44283 instead of expense"
-        " account for the journal entry created at payment reconciliation"
+        " account for the journal entry created at payment reconciliation",
     )
