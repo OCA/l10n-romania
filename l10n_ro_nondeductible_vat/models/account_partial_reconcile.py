eb47fae264effa748e3abbe43268db20299adcb5
Mihai Fekete <feketemihai@nexterp.ro>
2022-10-29 08:25:22 +0300

    15.0 IMP Isolate l10n_ro_nondeductible_vat

--- l10n_ro_nondeductible_vat/models/account_partial_reconcile.py
+++ l10n_ro_nondeductible_vat/models/account_partial_reconcile.py
@@ -2,20 +2,22 @@ from odoo import api, models
 
 
 class AccountPartialReconcile(models.Model):
-    _inherit = "account.partial.reconcile"
+    _name = "account.partial.reconcile"
+    _inherit = ["account.partial.reconcile", "l10n.ro.mixin"]
 
     @api.model
     def _prepare_cash_basis_tax_line_vals(self, tax_line, balance, amount_currency):
         vals = super()._prepare_cash_basis_tax_line_vals(
             tax_line, balance, amount_currency
         )
-        if (
-            tax_line.tax_repartition_line_id.skip_cash_basis_account_switch
-            and tax_line.company_id.account_cash_basis_base_account_id
-        ):
-            vals[
-                "account_id"
-            ] = tax_line.company_id.account_cash_basis_base_account_id.id
+        if tax_line.is_l10n_ro_record:
+            if (
+                tax_line.tax_repartition_line_id.l10n_ro_skip_cash_basis_account_switch
+                and tax_line.company_id.account_cash_basis_base_account_id
+            ):
+                vals[
+                    "account_id"
+                ] = tax_line.company_id.account_cash_basis_base_account_id.id
         return vals
 
     @api.model
@@ -23,11 +25,12 @@ class AccountPartialReconcile(models.Model):
         vals = super()._prepare_cash_basis_counterpart_tax_line_vals(
             tax_line, cb_tax_line_vals
         )
-        if (
-            tax_line.tax_repartition_line_id.skip_cash_basis_account_switch
-            and tax_line.company_id.account_cash_basis_base_account_id
-        ):
-            vals[
-                "account_id"
-            ] = tax_line.company_id.account_cash_basis_base_account_id.id
+        if tax_line.is_l10n_ro_record:
+            if (
+                tax_line.tax_repartition_line_id.l10n_ro_skip_cash_basis_account_switch
+                and tax_line.company_id.account_cash_basis_base_account_id
+            ):
+                vals[
+                    "account_id"
+                ] = tax_line.company_id.account_cash_basis_base_account_id.id
         return vals
