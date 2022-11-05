cefe51d7051ece120fa18b07cf4840646ed2e99b
Mihai Fekete <feketemihai@nexterp.ro>
2022-11-05 20:47:43 +0200

    Add bank impor tmodules, also with class for mt940

---
+++ l10n_ro_account_bank_statement_import_mt940_ing/models/account_bank_statement_import.py
@@ -0,0 +1,44 @@
+# Copyright (C) 2016 Forest and Biomass Romania
+# Copyright (C) 2022 Terrabit
+# Copyright (C) 2022 NextERP Romania
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+import logging
+from io import BytesIO
+import zipfile
+from odoo import api, models
+
+_logger = logging.getLogger(__name__)
+
+
+class AccountBankStatementImport(models.TransientModel):
+    _inherit = "account.bank.statement.import"
+
+    @api.model
+    def _parse_file(self, data_file):
+        """Parse a MT940 file.
+        For different type of MT940 statement add the type in the context.
+        and check methods return depending on get_mt940_type() """
+        try:
+            parser = self.env['account.bank.statement.import.mt940.parser']
+            _logger.debug("Try parsing with mt940 ro ing.")
+            parser = parser.with_context(type='mt940_ro_ing')
+            return parser.parse(data_file)
+        except ValueError:
+            try:
+                with zipfile.ZipFile(BytesIO(data_file)) as data:
+                    currency = None
+                    account_number = None
+                    transactions = []
+                    for member in data.namelist():
+                        currency, account_number, new = self._parse_file(
+                            data.open(member).read()
+                        )
+                        transactions.extend(new)
+                return currency, account_number, transactions
+            except (zipfile.BadZipFile, ValueError):
+                pass
+            # Not a mt940 file, returning super will call next candidate:
+            _logger.debug("Statement file was not a mt940 ro ing file.",
+                          exc_info=True)
+        return super(AccountBankStatementImport, self)._parse_file(data_file)
