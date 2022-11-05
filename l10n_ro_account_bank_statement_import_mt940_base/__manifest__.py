6328e7ff986a1685fcf0d3fde9179f36b29335a5
Mihai Fekete <feketemihai@nexterp.ro>
2022-11-05 20:47:43 +0200

    Add bank impor tmodules, also with class for mt940

---
+++ l10n_ro_account_bank_statement_import_mt940_base/__manifest__.py
@@ -0,0 +1,14 @@
+# Copyright (C) 2013-2015 Therp BV <http://therp.nl>
+
+{
+    "name": "MT940 Bank Statements Import",
+    "version": "14.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "Odoo Community Association (OCA), Therp BV",
+    "website": "https://github.com/OCA/l10n-romania",
+    "category": "Banking addons",
+    "depends": ["account_statement_import"],
+    "installable": True,
+    'development_status': 'Mature',
+    'maintainers': ['feketemihai', 'dhongu'],
+}
