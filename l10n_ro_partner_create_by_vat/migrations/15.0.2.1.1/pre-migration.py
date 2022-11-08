3edd9702c0a6012c7234646cc7210d2e8eb37700
Dorin Hongu <dhongu@gmail.com>
2022-11-08 19:48:45 +0200

    [IMP] l10n_ro_account_edi_ubl: black, isort, prettier

--- l10n_ro_partner_create_by_vat/migrations/15.0.2.1.1/pre-migration.py
+++ l10n_ro_partner_create_by_vat/migrations/15.0.2.1.1/pre-migration.py
@@ -22,35 +22,36 @@ except ImportError:
 
 @openupgrade.migrate(use_env=True)
 def migrate(env, version):
-    openupgrade.rename_tables(
-        env.cr,
-        [
-            ("res_partner_anaf_status", "l10n_ro_res_partner_anaf_status"),
-            ("res_partner_anaf_scptva", "l10n_ro_res_partner_anaf_scptva"),
-        ],
-    )
-    openupgrade.rename_fields(
-        env,
-        [
-            ("res.partner", "res_partner", "old_name", "l10n_ro_old_name"),
-            (
-                "res.partner",
-                "res_partner",
-                "active_anaf_line_ids",
-                "l10n_ro_active_anaf_line_ids",
-            ),
-            (
-                "res.partner",
-                "res_partner",
-                "l10n_ro_vat_subjected_anaf_line_ids",
-                "l10n_ro_l10n_ro_vat_subjected_anaf_line_ids",
-            ),
-        ],
-    )
-    openupgrade.rename_models(
-        env.cr,
-        [
-            ("res.partner.anaf.status", "l10n.ro.res.partner.anaf.status"),
-            ("res.partner.anaf.scptva", "l10n.ro.res.partner.anaf.scptva"),
-        ],
-    )
+    pass
+    # openupgrade.rename_tables(
+    #     env.cr,
+    #     [
+    #         ("res_partner_anaf_status", "l10n_ro_res_partner_anaf_status"),
+    #         ("res_partner_anaf_scptva", "l10n_ro_res_partner_anaf_scptva"),
+    #     ],
+    # )
+    # openupgrade.rename_fields(
+    #     env,
+    #     [
+    #         ("res.partner", "res_partner", "old_name", "l10n_ro_old_name"),
+    #         (
+    #             "res.partner",
+    #             "res_partner",
+    #             "active_anaf_line_ids",
+    #             "l10n_ro_active_anaf_line_ids",
+    #         ),
+    #         (
+    #             "res.partner",
+    #             "res_partner",
+    #             "l10n_ro_vat_subjected_anaf_line_ids",
+    #             "l10n_ro_l10n_ro_vat_subjected_anaf_line_ids",
+    #         ),
+    #     ],
+    # )
+    # openupgrade.rename_models(
+    #     env.cr,
+    #     [
+    #         ("res.partner.anaf.status", "l10n.ro.res.partner.anaf.status"),
+    #         ("res.partner.anaf.scptva", "l10n.ro.res.partner.anaf.scptva"),
+    #     ],
+    # )
