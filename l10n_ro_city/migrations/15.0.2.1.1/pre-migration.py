5c364ce3de404948c8d3593e4d7c79813cbb47d4
Dorin Hongu <dhongu@gmail.com>
2022-11-16 10:03:34 +0200

    test

--- l10n_ro_city/migrations/15.0.2.1.1/pre-migration.py
+++ l10n_ro_city/migrations/15.0.2.1.1/pre-migration.py
@@ -1,8 +1,9 @@
 # Copyright (C) 2022 NextERP Romania
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+import logging
 import sys
 
-from odoo import SUPERUSER_ID, api
+_logger = logging.getLogger(__name__)
 
 
 def install(package):
@@ -24,19 +25,24 @@ except ImportError:
 
 @openupgrade.migrate(use_env=True)
 def migrate(env, version):
-    openupgrade.rename_fields(
-        env,
-        [
-            ("res.city", "res_city", "siruta", "l10n_ro_siruta"),
-            ("res.city", "res_city", "municipality", "l10n_ro_municipality"),
-        ],
-    )
-    # Install l10n_ro_config if needed
-    with api.Environment.manage():
-        env = api.Environment(env.cr, SUPERUSER_ID, {})
-        IrModule = env["ir.module.module"]
-        IrModule.update_list()
+    pass
+    # fields_to_rename = [
+    #     ("res.city", "res_city", "siruta", "l10n_ro_siruta"),
+    #     ("res.city", "res_city", "municipality", "l10n_ro_municipality"),
+    # ]
+    #
+    # for field_to_rename in fields_to_rename:
+    #     try:
+    #         openupgrade.rename_fields(env, [field_to_rename])
+    #     except Exception as e:
+    #         _logger.error(str(e))
 
-        l10n_ro_config_module = IrModule.search([("name", "=", "l10n_ro_config")])
-        if l10n_ro_config_module.state != "installed":
-            l10n_ro_config_module.button_immediate_install()
+    # Install l10n_ro_config if needed
+    # with api.Environment.manage():
+    #     env = api.Environment(env.cr, SUPERUSER_ID, {})
+    #     IrModule = env["ir.module.module"]
+    #     IrModule.update_list()
+    #
+    #     l10n_ro_config_module = IrModule.search([("name", "=", "l10n_ro_config")])
+    #     if l10n_ro_config_module.state != "installed":
+    #         l10n_ro_config_module.button_immediate_install()
