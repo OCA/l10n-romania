c10d6fbd3ab09045d7457968aadf315fd1acbb6b
Mihai Fekete <feketemihai@nexterp.ro>
2022-11-19 15:12:25 +0200

    Update bank statement imports

--- l10n_ro_account_bank_statement_import_mt940_bcr/models/account_bank_statement_import.py
+++ l10n_ro_account_bank_statement_import_mt940_bcr/models/account_bank_statement_import.py
@@ -1,32 +1,20 @@
-# Copyright (C) 2016 Forest and Biomass Romania
 # Copyright (C) 2022 Terrabit
 # Copyright (C) 2022 NextERP Romania
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-import logging
-
 from odoo import models
-
-from .mt940 import MT940Parser as Parser
-
-_logger = logging.getLogger(__name__)
 
 
 class AccountBankStatementImport(models.TransientModel):
-    _inherit = "account.bank.statement.import"
+    _inherit = "account.statement.import"
 
     def _parse_file(self, data_file):
-        """Parse a MT940 IBAN BCR file."""
-        parser = Parser()
-
-        try:
-            _logger.debug("Try parsing with MT940 IBAN BCR.")
-            data = parser.parse(data_file)
+        parser = self.env["l10n.ro.account.bank.statement.import.mt940.parser"]
+        parser = parser.with_context(type="mt940_ro_bcr")
+        data = parser.parse(data_file)
+        if data:
             return self._post_parse_file(data)
-        except ValueError:
-            # Returning super will call next candidate:
-            _logger.debug("Statement file was not a MT940 IBAN BCR file.", exc_info=True)
-            return super(AccountBankStatementImport, self)._parse_file(data_file)
+        return super(AccountBankStatementImport, self)._parse_file(data_file)
 
     def _post_parse_file(self, data):
         currency, account_num, all_statements = data
