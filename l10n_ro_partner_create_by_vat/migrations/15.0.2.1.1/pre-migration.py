7ac8cf00fee19f545cf289def6c01094defb152b
Dorin Hongu <dhongu@gmail.com>
2022-11-24 06:04:22 +0200

    eliminare with api.Environment.manage()

--- l10n_ro_partner_create_by_vat/migrations/15.0.2.1.1/pre-migration.py
+++ l10n_ro_partner_create_by_vat/migrations/15.0.2.1.1/pre-migration.py
@@ -25,47 +25,48 @@ except ImportError:
 
 @openupgrade.migrate(use_env=True)
 def migrate(env, version):
-    pass
-    # try:
-    #     openupgrade.rename_tables(
-    #         env.cr,
-    #         [
-    #             ("res_partner_anaf_status", "l10n_ro_res_partner_anaf_status"),
-    #             ("res_partner_anaf_scptva", "l10n_ro_res_partner_anaf_scptva"),
-    #         ],
-    #     )
-    # except Exception as e:
-    #     _logger.error(str(e))
-    #
-    # fields_to_rename = [
-    #     ("res.partner", "res_partner", "old_name", "l10n_ro_old_name"),
-    #     (
-    #         "res.partner",
-    #         "res_partner",
-    #         "active_anaf_line_ids",
-    #         "l10n_ro_active_anaf_line_ids",
-    #     ),
-    #     (
-    #         "res.partner",
-    #         "res_partner",
-    #         "l10n_ro_vat_subjected_anaf_line_ids",
-    #         "l10n_ro_l10n_ro_vat_subjected_anaf_line_ids",
-    #     ),
-    # ]
-    #
-    # for field_to_rename in fields_to_rename:
-    #     try:
-    #         openupgrade.rename_fields(env, [field_to_rename])
-    #     except Exception as e:
-    #         _logger.error(str(e))
-    #         continue
-    # try:
-    #     openupgrade.rename_models(
-    #         env.cr,
-    #         [
-    #             ("res.partner.anaf.status", "l10n.ro.res.partner.anaf.status"),
-    #             ("res.partner.anaf.scptva", "l10n.ro.res.partner.anaf.scptva"),
-    #         ],
-    #     )
-    # except Exception as e:
-    #     _logger.error(str(e))
+    if openupgrade.table_exists(env.cr, "res_partner_anaf_status"):
+        try:
+            openupgrade.rename_tables(
+                env.cr,
+                [
+                    ("res_partner_anaf_status", "l10n_ro_res_partner_anaf_status"),
+                    ("res_partner_anaf_scptva", "l10n_ro_res_partner_anaf_scptva"),
+                ],
+            )
+        except Exception as e:
+            _logger.info("Error %s." % e)
+
+        try:
+            openupgrade.rename_models(
+                env.cr,
+                [
+                    ("res.partner.anaf.status", "l10n.ro.res.partner.anaf.status"),
+                    ("res.partner.anaf.scptva", "l10n.ro.res.partner.anaf.scptva"),
+                ],
+            )
+        except Exception as e:
+            _logger.info("Error %s." % e)
+
+    fields_to_rename = [
+        ("res.partner", "res_partner", "old_name", "l10n_ro_old_name"),
+        (
+            "res.partner",
+            "res_partner",
+            "active_anaf_line_ids",
+            "l10n_ro_active_anaf_line_ids",
+        ),
+        (
+            "res.partner",
+            "res_partner",
+            "l10n_ro_vat_subjected_anaf_line_ids",
+            "l10n_ro_l10n_ro_vat_subjected_anaf_line_ids",
+        ),
+    ]
+
+    for field_to_rename in fields_to_rename:
+        try:
+            openupgrade.rename_fields(env, [field_to_rename])
+        except Exception as e:
+            _logger.info("Error %s." % e)
+            continue
