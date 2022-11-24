3f05a7bf2a09c11b3304b311825f6bd6c8d454a2
Cojocaru Marcel <marcel.cojocaru@gmail.com>
2022-11-24 14:12:50 +0200

    [FIX] DeprecationWarning: Since Odoo 15.0, Environment.manage() is useless.

--- l10n_ro_partner_unique/migrations/15.0.1.1.1/pre-migration.py
+++ l10n_ro_partner_unique/migrations/15.0.1.1.1/pre-migration.py
@@ -7,11 +7,10 @@ def migrate(cr, version):
     if not version:
         return
     # Install l10n_ro_config if needed
-    with api.Environment.manage():
-        env = api.Environment(cr, SUPERUSER_ID, {})
-        IrModule = env["ir.module.module"]
-        IrModule.update_list()
+    env = api.Environment(cr, SUPERUSER_ID, {})
+    IrModule = env["ir.module.module"]
+    IrModule.update_list()
 
-        l10n_ro_config_module = IrModule.search([("name", "=", "l10n_ro_config")])
-        if l10n_ro_config_module.state != "installed":
-            l10n_ro_config_module.button_immediate_install()
+    l10n_ro_config_module = IrModule.search([("name", "=", "l10n_ro_config")])
+    if l10n_ro_config_module.state != "installed":
+        l10n_ro_config_module.button_immediate_install()
--- l10n_ro_vat_on_payment/migrations/15.0.1.3.1/pre-migration.py
+++ l10n_ro_vat_on_payment/migrations/15.0.1.3.1/pre-migration.py
@@ -60,11 +60,10 @@ def migrate(env, version):
     openupgrade.delete_records_safely_by_xml_id(env, views)
 
     # Install l10n_ro_config if needed
-    with api.Environment.manage():
-        env = api.Environment(env.cr, SUPERUSER_ID, {})
-        IrModule = env["ir.module.module"]
-        IrModule.update_list()
+    env = api.Environment(env.cr, SUPERUSER_ID, {})
+    IrModule = env["ir.module.module"]
+    IrModule.update_list()
 
-        l10n_ro_config_module = IrModule.search([("name", "=", "l10n_ro_config")])
-        if l10n_ro_config_module.state != "installed":
-            l10n_ro_config_module.button_immediate_install()
+    l10n_ro_config_module = IrModule.search([("name", "=", "l10n_ro_config")])
+    if l10n_ro_config_module.state != "installed":
+        l10n_ro_config_module.button_immediate_install()
