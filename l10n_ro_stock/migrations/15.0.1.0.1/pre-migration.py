a49d75a5df404ebc9b9cae7b1c60253b4f29c716
Dorin Hongu <dhongu@gmail.com>
2022-11-24 06:04:22 +0200

    eliminare with api.Environment.manage()

--- l10n_ro_stock/migrations/15.0.1.0.1/pre-migration.py
+++ l10n_ro_stock/migrations/15.0.1.0.1/pre-migration.py
@@ -3,6 +3,8 @@
 import logging
 import sys
 
+from odoo import SUPERUSER_ID, api
+
 _logger = logging.getLogger(__name__)
 
 
@@ -61,17 +63,17 @@ def migrate(env, version):
         try:
             openupgrade.rename_fields(env, [field_to_rename])
         except Exception as e:
-            _logger.error(str(e))
+            _logger.info("Error %s." % e)
 
     # Install l10n_ro_config if needed
-    # with api.Environment.manage():
-    #     try:
-    #         env = api.Environment(env.cr, SUPERUSER_ID, {})
-    #         IrModule = env["ir.module.module"]
-    #         IrModule.update_list()
-    #
-    #         l10n_ro_config_module = IrModule.search([("name", "=", "l10n_ro_config")])
-    #         if l10n_ro_config_module.state != "installed":
-    #             l10n_ro_config_module.button_immediate_install()
-    #     except Exception as e:
-    #         _logger.error(str(e))
+
+    try:
+        env = api.Environment(env.cr, SUPERUSER_ID, {})
+        IrModule = env["ir.module.module"]
+        IrModule.update_list()
+
+        l10n_ro_config_module = IrModule.search([("name", "=", "l10n_ro_config")])
+        if l10n_ro_config_module.state != "installed":
+            l10n_ro_config_module.button_immediate_install()
+    except Exception as e:
+        _logger.info("Error %s." % e)
