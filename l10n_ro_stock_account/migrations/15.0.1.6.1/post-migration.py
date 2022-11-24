985601059faa1e6efad1197259fe779e9e5250b7
Dorin Hongu <dhongu@gmail.com>
2022-11-24 06:04:22 +0200

    eliminare with api.Environment.manage()

--- l10n_ro_stock_account/migrations/15.0.1.6.1/post-migration.py
+++ l10n_ro_stock_account/migrations/15.0.1.6.1/post-migration.py
@@ -7,26 +7,23 @@ def migrate(cr, version):
     if not version:
         return
 
-    with api.Environment.manage():
-        env = api.Environment(cr, SUPERUSER_ID, {})
-        IrModule = env["ir.module.module"]
-        IrModule.update_list()
+    env = api.Environment(cr, SUPERUSER_ID, {})
+    IrModule = env["ir.module.module"]
+    IrModule.update_list()
 
-        ro_comps = env["res.company"].search(
-            [
-                ("l10n_ro_accounting", "=", True),
-                ("l10n_ro_stock_acc_price_diff", "=", True),
-            ]
+    ro_comps = env["res.company"].search(
+        [
+            ("l10n_ro_accounting", "=", True),
+            ("l10n_ro_stock_acc_price_diff", "=", True),
+        ]
+    )
+    if ro_comps:
+        price_diff_module = IrModule.search(
+            [("name", "=", "l10n_ro_stock_price_difference")]
         )
-        if ro_comps:
-            price_diff_module = IrModule.search(
-                [("name", "=", "l10n_ro_stock_price_difference")]
-            )
-            price_diff_module.button_install()
-            if hasattr(
-                env["account.move.line"],
-                "_l10n_ro_get_or_create_price_difference_product",
-            ):
-                env[
-                    "account.move.line"
-                ].l10n_ro_get_or_create_price_difference_product()
+        price_diff_module.button_install()
+        if hasattr(
+            env["account.move.line"],
+            "_l10n_ro_get_or_create_price_difference_product",
+        ):
+            env["account.move.line"].l10n_ro_get_or_create_price_difference_product()
