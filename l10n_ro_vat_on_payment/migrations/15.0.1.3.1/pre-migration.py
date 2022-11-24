709efc450b1ffeb0037ced2ffd40ad27ca8eb99b
Dorin Hongu <dhongu@gmail.com>
2022-11-24 06:04:22 +0200

    eliminare with api.Environment.manage()

--- l10n_ro_vat_on_payment/migrations/15.0.1.3.1/pre-migration.py
+++ l10n_ro_vat_on_payment/migrations/15.0.1.3.1/pre-migration.py
@@ -60,11 +60,11 @@ def migrate(env, version):
     openupgrade.delete_records_safely_by_xml_id(env, views)
 
     # Install l10n_ro_config if needed
-    with api.Environment.manage():
-        env = api.Environment(env.cr, SUPERUSER_ID, {})
-        IrModule = env["ir.module.module"]
-        IrModule.update_list()
 
-        l10n_ro_config_module = IrModule.search([("name", "=", "l10n_ro_config")])
-        if l10n_ro_config_module.state != "installed":
-            l10n_ro_config_module.button_immediate_install()
+    env = api.Environment(env.cr, SUPERUSER_ID, {})
+    IrModule = env["ir.module.module"]
+    IrModule.update_list()
+
+    l10n_ro_config_module = IrModule.search([("name", "=", "l10n_ro_config")])
+    if l10n_ro_config_module.state != "installed":
+        l10n_ro_config_module.button_immediate_install()
