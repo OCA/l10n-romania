e5ec7e07d7980d41d4cdc3fddf65cdd038274078
Dorin Hongu <dhongu@gmail.com>
2022-12-02 11:05:22 +0200

    fix pozitie camp

--- l10n_ro_account_edi_ubl/__manifest__.py
+++ l10n_ro_account_edi_ubl/__manifest__.py
@@ -20,7 +20,7 @@
         "views/cius_template.xml",
     ],
     "license": "AGPL-3",
-    "version": "15.0.1.3.0",
+    "version": "15.0.1.3.1",
     "author": "Terrabit," "NextERP Romania," "Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/l10n-romania",
     "installable": True,
