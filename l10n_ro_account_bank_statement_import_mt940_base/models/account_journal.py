73951e56e051200f7affbc2d621ad95819088105
Stana Natanaela <musatnatanaela@gmail.com>
2022-12-07 13:44:03 +0200

    update _statement_line_import_speeddict

--- l10n_ro_account_bank_statement_import_mt940_base/models/account_journal.py
+++ l10n_ro_account_bank_statement_import_mt940_base/models/account_journal.py
@@ -13,3 +13,24 @@ class AccountJournal(models.Model):
         )._get_bank_statements_available_import_formats()
         res.extend([("mt940_general")])
         return res
+
+    def _statement_line_import_speeddict(self):
+        """This method is designed to be inherited by reconciliation modules.
+        These modules can take advantage of this method to pre-fetch data
+        that will later be used for many statement lines (to avoid
+        searching data for each statement line).
+        The goal is to improve performances.
+        """
+        self.ensure_one()
+        super()._statement_line_import_speeddict()
+        speeddict = {"account_number": {}}
+        partner_banks = self.env["res.partner.bank"].search_read(
+            [("company_id", "in", (False, self.company_id.id))],
+            ["sanitized_acc_number", "partner_id"],
+        )
+        for partner_bank in partner_banks:
+            speeddict["account_number"][partner_bank["sanitized_acc_number"]] = {
+                "partner_id": partner_bank["partner_id"][0],
+                "partner_bank_id": partner_bank["id"],
+            }
+        return speeddict
