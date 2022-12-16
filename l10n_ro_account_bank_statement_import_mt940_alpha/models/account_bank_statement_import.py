5399a0314055daa2bcc2d1b12e30b739311e6cc6
Cojocaru Marcel <marcel.cojocaru@gmail.com>
2022-12-16 17:06:16 +0200

    fix mt940 for all

--- l10n_ro_account_bank_statement_import_mt940_alpha/models/account_bank_statement_import.py
+++ l10n_ro_account_bank_statement_import_mt940_alpha/models/account_bank_statement_import.py
@@ -7,8 +7,14 @@ from odoo import models
 class AccountBankStatementImport(models.TransientModel):
     _inherit = "account.statement.import"
 
+    def _is_alpha(self):
+        if self._context.get("journal_id"):
+            journal = self.env["account.journal"].browse(self._context["journal_id"])
+            return journal.bank_account_id.bank_bic == "BUCUROBU"
+        return self._context.get("mt940_ro_alpha")
+
     def _parse_file(self, data_file):
-        if self._context.get("type", "") == "mt940_ro_alpha":
+        if self._is_alpha():
             parser = self.env["l10n.ro.account.bank.statement.import.mt940.parser"]
             parser = parser.with_context(type="mt940_ro_alpha")
             data = parser.parse(data_file)
