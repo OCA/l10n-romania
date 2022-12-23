1dd90505bb0ba9ab3d7f1ce63b9cc15da8df6a31
Mihai Fekete <feketemihai@nexterp.ro>
2022-12-23 07:34:31 +0200

    Update init hooks

--- l10n_ro_stock_account/init_hook.py
+++ l10n_ro_stock_account/init_hook.py
@@ -47,94 +47,94 @@ def store_field_l10n_ro_vat_number(cr):
             """
         )
 
-    logger.info("Computing field l10n_ro_account_id on stock.valuation.layer")
+        logger.info("Computing field l10n_ro_account_id on stock.valuation.layer")
 
-    # do res.company.init()
+        # do res.company.init()
 
-    cr.execute(
-        """SELECT column_name
-           FROM information_schema.columns
-           WHERE table_name='res_company' AND column_name='l10n_ro_accounting'"""
-    )
+        cr.execute(
+            """SELECT column_name
+            FROM information_schema.columns
+            WHERE table_name='res_company' AND column_name='l10n_ro_accounting'"""
+        )
 
-    if not cr.fetchone():
+        if not cr.fetchone():
+            cr.execute(
+                """
+                ALTER TABLE res_company ADD COLUMN l10n_ro_accounting boolean;
+                """
+            )
+
         cr.execute(
             """
-            ALTER TABLE res_company ADD COLUMN l10n_ro_accounting boolean;
+            UPDATE res_company comp
+            SET l10n_ro_accounting = true
+            FROM res_partner rp
+            WHERE comp.partner_id = rp.id and
+                rp.country_id in (SELECT res_id FROM ir_model_data
+                                    WHERE model = 'res.country' and
+                                        name = 'ro' and module='base'
+                                );
+        """
+        )
+
+        cr.execute(
+            """
+            WITH prepared_data as (
+            SELECT
+                svl.id as svl_id,
+                COALESCE(
+                    aml_for_loop.account,
+                    replace(irp_ctg.value_reference, 'account.account,', '')::integer
+                ) as account_id
+
+                from stock_valuation_layer svl
+                left join stock_move sm on svl.stock_move_id = sm.id
+                left join product_product pp on pp.id = svl.product_id
+                left join product_template ptmpl on ptmpl.id = pp.product_tmpl_id
+
+                left join ir_property irp_ctg on
+                    irp_ctg.res_id = CONCAT('product.category,', ptmpl.categ_id)
+                    and sm.company_id = irp_ctg.company_id
+                    and irp_ctg.name = 'property_stock_valuation_account_id'
+
+                left join (
+                    select sub.move_id, sub.balance, sub.account
+                    from (
+                            select
+                                am.id as move_id,
+                                aa.code,
+                                aml.balance,
+                                COALESCE(
+                                    CASE
+                                        WHEN (aa.code like '2%' or aa.code like '3%')
+                                        THEN aml.account_id
+                                        ELSE null
+                                    END) account,
+                                row_number() over (partition by am.id order by aa.code) as rnum
+                                from account_move am
+                                left join account_move_line aml on aml.move_id = am.id
+                                left join account_account aa on aa.id = aml.account_id
+                                group by am.id, aa.code, aml.account_id, aml.balance
+                                order by am.id, aa.code
+                    ) as sub where sub.account is not null and sub.rnum = 1
+                    group by sub.move_id, sub.account, sub.balance
+
+                ) as aml_for_loop on
+                    aml_for_loop.move_id = svl.account_move_id
+                    and round(aml_for_loop.balance - svl.value, 2) = 0
+
+                left join res_company comp on sm.company_id = comp.id
+
+                where comp.l10n_ro_accounting = true
+            order by svl.id
+            )
+
+            UPDATE stock_valuation_layer svl
+            SET l10n_ro_account_id = prepared_data.account_id
+            FROM prepared_data
+            WHERE svl.id = prepared_data.svl_id;
             """
         )
-
-    cr.execute(
-        """
-        UPDATE res_company comp
-        SET l10n_ro_accounting = true
-        FROM res_partner rp
-        WHERE comp.partner_id = rp.id and
-              rp.country_id in (SELECT res_id FROM ir_model_data
-                                WHERE model = 'res.country' and
-                                      name = 'ro' and module='base'
-                               );
-    """
-    )
-
-    cr.execute(
-        """
-        WITH prepared_data as (
-        SELECT
-            svl.id as svl_id,
-            COALESCE(
-                aml_for_loop.account,
-                replace(irp_ctg.value_reference, 'account.account,', '')::integer
-            ) as account_id
-
-            from stock_valuation_layer svl
-            left join stock_move sm on svl.stock_move_id = sm.id
-            left join product_product pp on pp.id = svl.product_id
-            left join product_template ptmpl on ptmpl.id = pp.product_tmpl_id
-
-            left join ir_property irp_ctg on
-                irp_ctg.res_id = CONCAT('product.category,', ptmpl.categ_id)
-                and sm.company_id = irp_ctg.company_id
-                and irp_ctg.name = 'property_stock_valuation_account_id'
-
-            left join (
-                select sub.move_id, sub.balance, sub.account
-                  from (
-                        select
-                            am.id as move_id,
-                            aa.code,
-                            aml.balance,
-                            COALESCE(
-                                CASE
-                                    WHEN (aa.code like '2%' or aa.code like '3%')
-                                    THEN aml.account_id
-                                    ELSE null
-                                END) account,
-                            row_number() over (partition by am.id order by aa.code) as rnum
-                            from account_move am
-                            left join account_move_line aml on aml.move_id = am.id
-                            left join account_account aa on aa.id = aml.account_id
-                            group by am.id, aa.code, aml.account_id, aml.balance
-                            order by am.id, aa.code
-                 ) as sub where sub.account is not null and sub.rnum = 1
-                 group by sub.move_id, sub.account, sub.balance
-
-            ) as aml_for_loop on
-                aml_for_loop.move_id = svl.account_move_id
-                and round(aml_for_loop.balance - svl.value, 2) = 0
-
-            left join res_company comp on sm.company_id = comp.id
-
-            where comp.l10n_ro_accounting = true
-        order by svl.id
-        )
-
-        UPDATE stock_valuation_layer svl
-        SET l10n_ro_account_id = prepared_data.account_id
-        FROM prepared_data
-        WHERE svl.id = prepared_data.svl_id;
-        """
-    )
 
     # initializare valued_type
 
@@ -240,75 +240,94 @@ def store_field_l10n_ro_vat_number(cr):
 def store_svl_lot_and_locations(cr):
     # initializare svl.lot_ids
     cr.execute(
-        """CREATE TABLE stock_production_lot_stock_valuation_layer_rel
-        (stock_valuation_layer_id INTEGER, stock_production_lot_id INTEGER)""",
+        """SELECT column_name
+           FROM information_schema.columns
+           WHERE table_name='stock_production_lot_stock_valuation_layer_rel'"""
     )
-    cr.execute(
-        """
-        INSERT INTO stock_production_lot_stock_valuation_layer_rel (
-            stock_valuation_layer_id, stock_production_lot_id)
-        SELECT svl.id, sml.lot_id
-        FROM stock_valuation_layer svl
-        LEFT JOIN stock_move sm ON svl.stock_move_id = sm.id
-        LEFT JOIN stock_move_line sml ON sml.move_id = sm.id
-        WHERE sml.lot_id != null
-        """,
-    )
+    if not cr.fetchone():
+        cr.execute(
+            """CREATE TABLE stock_production_lot_stock_valuation_layer_rel
+            (stock_valuation_layer_id INTEGER, stock_production_lot_id INTEGER)""",
+        )
+        cr.execute(
+            """
+            INSERT INTO stock_production_lot_stock_valuation_layer_rel (
+                stock_valuation_layer_id, stock_production_lot_id)
+            SELECT svl.id, sml.lot_id
+            FROM stock_valuation_layer svl
+            LEFT JOIN stock_move sm ON svl.stock_move_id = sm.id
+            LEFT JOIN stock_move_line sml ON sml.move_id = sm.id
+            WHERE sml.lot_id != null
+            """,
+        )
 
     # initializare svl.l10n_ro_stock_move_line_id
     cr.execute(
-        """
-        ALTER TABLE stock_valuation_layer
-        ADD COLUMN l10n_ro_stock_move_line_id integer""",
+        """SELECT column_name
+           FROM information_schema.columns
+           WHERE table_name='stock_valuation_layer' AND
+           column_name='l10n_ro_stock_move_line_id'"""
     )
-    cr.execute(
-        """
-        UPDATE stock_valuation_layer svl
-        SET l10n_ro_stock_move_line_id = sub.sml_id
-        FROM (
-            SELECT svl.id as svl_id, min(sml.id) as sml_id
-            FROM stock_valuation_layer svl
-            LEFT JOIN stock_move sm on sm.id = svl.stock_move_id
-            LEFT JOIN stock_move_line sml on sml.move_id = sm.id
-            GROUP BY svl.id
-        ) as sub
-        WHERE sub.svl_id = svl.id
-        """,
-    )
+    if not cr.fetchone():
+        cr.execute(
+            """
+            ALTER TABLE stock_valuation_layer
+            ADD COLUMN l10n_ro_stock_move_line_id integer""",
+        )
+        cr.execute(
+            """
+            UPDATE stock_valuation_layer svl
+            SET l10n_ro_stock_move_line_id = sub.sml_id
+            FROM (
+                SELECT svl.id as svl_id, min(sml.id) as sml_id
+                FROM stock_valuation_layer svl
+                LEFT JOIN stock_move sm on sm.id = svl.stock_move_id
+                LEFT JOIN stock_move_line sml on sml.move_id = sm.id
+                GROUP BY svl.id
+            ) as sub
+            WHERE sub.svl_id = svl.id
+            """,
+        )
 
     # initializare svl.l10n_ro_location_id, svl.l10n_ro_location_dest_id
     cr.execute(
-        """
-            ALTER TABLE stock_valuation_layer
-            ADD COLUMN l10n_ro_location_id integer""",
+        """SELECT column_name
+           FROM information_schema.columns
+           WHERE table_name='stock_valuation_layer' AND column_name='l10n_ro_location_id'"""
     )
-    cr.execute(
-        """
-            ALTER TABLE stock_valuation_layer
-            ADD COLUMN l10n_ro_location_dest_id integer""",
-    )
-    cr.execute(
-        """
-        WITH sub as (
-            SELECT
-                svl.id as svl_id,
-                CASE WHEN count(sml.id) = 1
-                    THEN min(sml.location_id)
-                    ELSE min(sm.location_id)
-                END as location_id,
-                CASE WHEN count(sml.id) = 1
-                    THEN min(sml.location_dest_id)
-                    ELSE min(sm.location_dest_id)
-                END as location_dest_id
-            FROM stock_valuation_layer svl
-            LEFT JOIN stock_move sm on sm.id = svl.stock_move_id
-            LEFT JOIN stock_move_line sml on sml.move_id = sm.id
-            GROUP BY svl.id
+    if not cr.fetchone():
+        cr.execute(
+            """
+                ALTER TABLE stock_valuation_layer
+                ADD COLUMN l10n_ro_location_id integer""",
         )
+        cr.execute(
+            """
+                ALTER TABLE stock_valuation_layer
+                ADD COLUMN l10n_ro_location_dest_id integer""",
+        )
+        cr.execute(
+            """
+            WITH sub as (
+                SELECT
+                    svl.id as svl_id,
+                    CASE WHEN count(sml.id) = 1
+                        THEN min(sml.location_id)
+                        ELSE min(sm.location_id)
+                    END as location_id,
+                    CASE WHEN count(sml.id) = 1
+                        THEN min(sml.location_dest_id)
+                        ELSE min(sm.location_dest_id)
+                    END as location_dest_id
+                FROM stock_valuation_layer svl
+                LEFT JOIN stock_move sm on sm.id = svl.stock_move_id
+                LEFT JOIN stock_move_line sml on sml.move_id = sm.id
+                GROUP BY svl.id
+            )
 
-        UPDATE stock_valuation_layer svl SET
-            l10n_ro_location_id = sub.location_id,
-            l10n_ro_location_dest_id = sub.location_dest_id
-        FROM sub
-        WHERE sub.svl_id = svl.id""",
-    )
+            UPDATE stock_valuation_layer svl SET
+                l10n_ro_location_id = sub.location_id,
+                l10n_ro_location_dest_id = sub.location_dest_id
+            FROM sub
+            WHERE sub.svl_id = svl.id""",
+        )
