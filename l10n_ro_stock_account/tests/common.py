6f52bd3ea7a08b625cb2ca69f993764e3291f92a
Cojocaru Marcel <marcel.cojocaru@gmail.com>
2023-01-03 22:44:11 +0200

    [14.0][FIX] fix tracking in average

--- l10n_ro_stock_account/tests/common.py
+++ l10n_ro_stock_account/tests/common.py
@@ -549,3 +549,32 @@ class TestStockCommon(ValuationReconciliationTestCommon):
             ],
             order="date, id",
         )
+
+    def create_lc(self, picking, lc_p1, lc_p2):
+        default_vals = self.env["stock.landed.cost"].default_get(
+            list(self.env["stock.landed.cost"].fields_get())
+        )
+        default_vals.update(
+            {
+                "picking_ids": [picking.id],
+                "account_journal_id": self.company_data["default_journal_misc"],
+                "cost_lines": [(0, 0, {"product_id": self.product_1.id})],
+                "valuation_adjustment_lines": [],
+            }
+        )
+        cost_lines_values = {
+            "name": ["equal split"],
+            "split_method": ["equal"],
+            "price_unit": [lc_p1 + lc_p2],
+        }
+        stock_landed_cost_1 = self.env["stock.landed.cost"].new(default_vals)
+        for index, cost_line in enumerate(stock_landed_cost_1.cost_lines):
+            cost_line.onchange_product_id()
+            cost_line.name = cost_lines_values["name"][index]
+            cost_line.split_method = cost_lines_values["split_method"][index]
+            cost_line.price_unit = cost_lines_values["price_unit"][index]
+        vals = stock_landed_cost_1._convert_to_write(stock_landed_cost_1._cache)
+        stock_landed_cost_1 = self.env["stock.landed.cost"].create(vals)
+
+        stock_landed_cost_1.compute_landed_cost()
+        stock_landed_cost_1.button_validate()
