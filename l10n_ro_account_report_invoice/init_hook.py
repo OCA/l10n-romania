cbdf06d87ec07a06a9a0998a13e0e91440bd47bd
Mihai Fekete <feketemihai@nexterp.ro>
2023-03-27 12:22:53 +0300

    Move code from 14

--- l10n_ro_account_report_invoice/init_hook.py
+++ l10n_ro_account_report_invoice/init_hook.py
@@ -37,59 +37,60 @@ def store_field_l10n_ro_currency_rate(cr):
             """
         )
 
-    logger.info("Computing field l10n_ro_currency_rate on account.move")
-    cr.execute(
-        """
-        select id from res_country where code = 'RO';
-        """
-    )
-    ro_country = cr.fetchone()
-    if ro_country:
+        logger.info("Computing field l10n_ro_currency_rate on account.move")
         cr.execute(
             """
-            SELECT res_company.id
-            FROM res_company
-            LEFT JOIN res_partner partner ON partner.id = res_company.partner_id
-            WHERE partner.country_id = %s;
+            select id from res_country where code = 'RO';
             """
-            % ro_country
         )
-        ro_companies = cr.fetchall()
-        if ro_companies:
-            ro_companies = [x[0] for x in ro_companies]
+        ro_country = cr.fetchone()
+        if ro_country:
+            cr.execute(
+                """
+                SELECT res_company.id
+                FROM res_company
+                LEFT JOIN res_partner partner ON partner.id = res_company.partner_id
+                WHERE partner.country_id = %s;
+                """
+                % ro_country
+            )
+            ro_companies = cr.fetchall()
+            if ro_companies:
+                ro_companies = [x[0] for x in ro_companies]
+                cr.execute(
+                    r"""
+                    WITH currency_rate
+                    (currency_id, company_id, rate, date_start, date_end) AS (
+                        SELECT
+                            r.currency_id,
+                            COALESCE(r.company_id, c.id) as company_id,
+                            r.rate,
+                            r.name AS date_start,
+                            (SELECT name FROM res_currency_rate r2
+                            WHERE r2.name > r.name AND
+                                r2.currency_id = r.currency_id AND
+                                (r2.company_id is null or r2.company_id = c.id)
+                            ORDER BY r2.name ASC
+                            LIMIT 1) AS date_end
+                        FROM res_currency_rate r
+                        JOIN res_company c ON (r.company_id is null or r.company_id = c.id)
+                    )
+                    UPDATE account_move acc_move
+                    SET l10n_ro_currency_rate = round(1 / coalesce(cr.rate, 1),4)::numeric
+                    FROM currency_rate cr
+                    WHERE acc_move.company_id in %(ids)s AND
+                        (cr.currency_id = acc_move.currency_id AND
+                        cr.date_start <= COALESCE(acc_move.invoice_date, acc_move.date) AND
+                        (cr.date_end IS NULL OR
+                            cr.date_end > COALESCE(acc_move.invoice_date, acc_move.date)) AND
+                        (acc_move.company_id = cr.company_id OR cr.company_id is Null));
+                    """,
+                    {"ids": tuple(ro_companies)},
+                )
             cr.execute(
                 r"""
-                WITH currency_rate (currency_id, company_id, rate, date_start, date_end) AS (
-                    SELECT
-                        r.currency_id,
-                        COALESCE(r.company_id, c.id) as company_id,
-                        r.rate,
-                        r.name AS date_start,
-                        (SELECT name FROM res_currency_rate r2
-                         WHERE r2.name > r.name AND
-                               r2.currency_id = r.currency_id AND
-                               (r2.company_id is null or r2.company_id = c.id)
-                         ORDER BY r2.name ASC
-                         LIMIT 1) AS date_end
-                    FROM res_currency_rate r
-                    JOIN res_company c ON (r.company_id is null or r.company_id = c.id)
-                )
                 UPDATE account_move acc_move
-                SET l10n_ro_currency_rate = round(1 / coalesce(cr.rate, 1),4)::numeric
-                FROM currency_rate cr
-                WHERE acc_move.company_id in %(ids)s AND
-                      (cr.currency_id = acc_move.currency_id AND
-                       cr.date_start <= COALESCE(acc_move.invoice_date, acc_move.date) AND
-                       (cr.date_end IS NULL OR
-                        cr.date_end > COALESCE(acc_move.invoice_date, acc_move.date)) AND
-                       (acc_move.company_id = cr.company_id OR cr.company_id is Null));
-                """,
-                {"ids": tuple(ro_companies)},
+                SET l10n_ro_currency_rate = 1
+                WHERE acc_move.l10n_ro_currency_rate is Null;
+                """
             )
-        cr.execute(
-            r"""
-            UPDATE account_move acc_move
-            SET l10n_ro_currency_rate = 1
-            WHERE acc_move.l10n_ro_currency_rate is Null;
-            """
-        )
