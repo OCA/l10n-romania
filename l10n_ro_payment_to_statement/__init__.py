db207b1b389303964abd151fa32efb77c508c7ba
Mihai Fekete <feketemihai@nexterp.ro>
2023-03-28 16:58:50 +0300

    Adapt code from 14

--- l10n_ro_payment_to_statement/__init__.py
+++ l10n_ro_payment_to_statement/__init__.py
@@ -4,12 +4,11 @@ from odoo import SUPERUSER_ID, api
 
 
 def pre_init_hook(cr, registry):
-    with api.Environment.manage():
-        env = api.Environment(cr, SUPERUSER_ID, {})
-        ro_comps = env["res.company"].search([("l10n_ro_accounting", "=", True)])
-        if ro_comps:
-            cash_journals = env["account.journal"].search(
-                [("type", "=", "cash"), ("company_id", "in", ro_comps.ids)]
-            )
-            for journal in cash_journals:
-                journal.l10n_ro_update_cash_vals()
+    env = api.Environment(cr, SUPERUSER_ID, {})
+    ro_comps = env["res.company"].search([("l10n_ro_accounting", "=", True)])
+    if ro_comps:
+        cash_journals = env["account.journal"].search(
+            [("type", "=", "cash"), ("company_id", "in", ro_comps.ids)]
+        )
+        for journal in cash_journals:
+            journal.l10n_ro_update_cash_vals()
