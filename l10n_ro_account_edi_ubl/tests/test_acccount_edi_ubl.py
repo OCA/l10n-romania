2090e228ea31a14a08cf0b1c8fcb0109ffcf128c
Stana Natanaela <musatnatanaela@gmail.com>
2023-03-29 18:11:27 +0300

    update account_edi_ubl tests

--- l10n_ro_account_edi_ubl/tests/test_acccount_edi_ubl.py
+++ l10n_ro_account_edi_ubl/tests/test_acccount_edi_ubl.py
@@ -21,18 +21,35 @@ class TestAccountEdiUbl(AccountEdiTestCommon):
         cls.country_state = cls.env["res.country.state"].search(
             [("name", "=", "Timiș")]
         )
-        cls.env.company.write(
-            {
-                "vat": "RO30834857",
-                "name": "FOREST AND BIOMASS ROMANIA S.A.",
-                "country_id": cls.env.ref("base.ro").id,
-                "currency_id": cls.currency.id,
-                "street": "Ferma 5-6",
-                "city": "Giulvaz",
-                "state_id": cls.country_state.id,
-                "zip": "300011",
-            }
+        l10n_ro_address_extend = cls.env["ir.module.module"].search(
+            [("name", "=", "l10n_ro_address_extended")]
         )
+        if l10n_ro_address_extend.state != "installed":
+            cls.env.company.write(
+                {
+                    "vat": "RO30834857",
+                    "name": "FOREST AND BIOMASS ROMANIA S.A.",
+                    "country_id": cls.env.ref("base.ro").id,
+                    "currency_id": cls.currency.id,
+                    "street": "Ferma 5-6",
+                    "city": "Giulvaz",
+                    "state_id": cls.country_state.id,
+                    "zip": "300011",
+                }
+            )
+        else:
+            cls.env.company.write(
+                {
+                    "vat": "RO30834857",
+                    "name": "FOREST AND BIOMASS ROMANIA S.A.",
+                    "country_id": cls.env.ref("base.ro").id,
+                    "currency_id": cls.currency.id,
+                    "street_name": "Ferma 5-6",
+                    "city": "Giulvaz",
+                    "state_id": cls.country_state.id,
+                    "zip": "300011",
+                }
+            )
 
         cls.partner = cls.env["res.partner"].create(
             {
