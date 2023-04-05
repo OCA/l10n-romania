cd2d33af8099a0da6d0bba6963efb78dd400aba2
Stana Natanaela <musatnatanaela@gmail.com>
2023-04-05 18:58:28 +0300

    [MIG] l10n_ro_account_bank_statement_import_mt940_alpha: Migration to 15.0

--- l10n_ro_account_bank_statement_import_mt940_alpha/__manifest__.py
+++ l10n_ro_account_bank_statement_import_mt940_alpha/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     "name": "MT940 Alpha Format Bank Statements Import",
-    "version": "14.0.1.2.0",
+    "version": "15.0.1.0.0",
     "license": "AGPL-3",
     "author": "NextERP Romania SRL," "Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/l10n-romania",
