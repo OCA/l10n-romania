e927c7223fee4c2f45391202cff08746f4ee7ebc
Mihai Fekete <feketemihai@nexterp.ro>
2023-04-06 08:48:56 +0300

    Increase versions

--- l10n_ro_account_period_close/__manifest__.py
+++ l10n_ro_account_period_close/__manifest__.py
@@ -12,7 +12,7 @@
         "security/ir.model.access.csv",
     ],
     "license": "AGPL-3",
-    "version": "15.0.3.2.0",
+    "version": "15.0.3.2.1",
     "author": "NextERP Romania,"
     "Forest and Biomass Romania,"
     "Odoo Community Association (OCA)",
