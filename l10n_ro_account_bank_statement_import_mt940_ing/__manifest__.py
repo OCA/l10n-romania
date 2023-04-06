5975ad35b0c7848926fc2c33e4c5cd67b9bb4a4c
Stana Natanaela <musatnatanaela@gmail.com>
2023-04-06 09:48:28 +0300

    [MIG] l10n_ro_account_bank_statement_import_mt940_ing: Migration to 15.0

--- l10n_ro_account_bank_statement_import_mt940_ing/__manifest__.py
+++ l10n_ro_account_bank_statement_import_mt940_ing/__manifest__.py
@@ -5,7 +5,7 @@
 
 {
     "name": "MT940 ING Format Bank Statements Import",
-    "version": "14.0.1.4.0",
+    "version": "15.0.1.0.0",
     "license": "AGPL-3",
     "author": "Terrabit,"
     "NextERP Romania SRL,"
