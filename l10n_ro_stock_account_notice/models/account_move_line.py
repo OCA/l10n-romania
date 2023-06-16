37338d44a740354fd177e8bd97ccac1a84aa651a
Adrian Vacaru <adrian.vacaru@dakai.ro>
2023-06-16 14:31:13 +0300

    Final notice

--- l10n_ro_stock_account_notice/models/account_move_line.py
+++ l10n_ro_stock_account_notice/models/account_move_line.py
@@ -13,6 +13,48 @@ class AccountMoveLine(models.Model):
     _name = "account.move.line"
     _inherit = ["account.move.line", "l10n.ro.mixin"]
 
+    def _compute_account_id(self):
+        l10n_ro_lines_for_notice = self.filtered(lambda x: x.product_id.type == "product" and x.is_l10n_ro_record)
+        if l10n_ro_lines_for_notice:
+            move_id = self.mapped("move_id")
+            #Presupunem ca avem doar o factura de evaluat.
+            #Nu este compatibil cu factura ce stige mai multe achizitii.
+            if move_id.is_purchase_document():
+                purchase = l10n_ro_lines_for_notice.mapped("purchase_order_id")
+                if purchase and any(
+                    [
+                            p.purchase_method == "receive"
+                            for p in self.mapped("product_id")
+                        ]
+                    ):
+                    # Control bills based on received quantities
+                    if any(
+                        [
+                            p.l10n_ro_notice or p._is_dropshipped()
+                            for p in purchase.picking_ids
+                        ]
+                    ):
+                        self = self.with_context(valued_type="invoice_in_notice")
+            if move_id.is_sale_document():
+                sale_lines = l10n_ro_lines_for_notice.mapped("sale_line_ids")
+                if sale_lines and any(
+                    [
+                        p.invoice_policy == "delivery"
+                        for p in self.mapped("product_id")
+                        ]
+                    ):
+                    # Control bills based on received quantities
+                    sale = sale_lines.mapped("order_id")
+                    if any(
+                        [
+                            p.l10n_ro_notice and not p._is_dropshipped()
+                            for p in sale.mapped("picking_ids")
+                        ]
+                    ):
+                        self = self.with_context(valued_type="invoice_out_notice")
+        return super(AccountMoveLine, self)._compute_account_id()
+
+    #TODO: de sters, metoda depreciata.
     def _get_computed_account(self):
         if self.product_id.type == "product" and self.is_l10n_ro_record:
             if self.move_id.is_purchase_document():
--- l10n_ro_stock_account_notice/tests/test_purchase.py
+++ l10n_ro_stock_account_notice/tests/test_purchase.py
@@ -31,30 +31,3 @@ class TestStockPurchase(TestStockCommon):
         # soldul lui 408 trebuie sa fie zero
         self.check_account_valuation(0, 0, self.stock_picking_payable_account_id)
         
-
-    # def check_account_valuation(self, val_p1, val_p2, account=None):
-    #     val_p1 = round(val_p1, 2)
-    #     val_p2 = round(val_p2, 2)
-    #     if not account:
-    #         account = self.account_valuation
-    #
-    #     domain = [
-    #         ("product_id", "in", [self.product_1.id, self.product_2.id]),
-    #         ("account_id", "=", account.id),
-    #         ("parent_state", "=", "posted"),
-    #     ]
-    #     account_valuations = self.env["account.move.line"].read_group(
-    #         domain, ["debit:sum", "credit:sum", "quantity:sum"], ["product_id"]
-    #     )
-    #     d = self.env["account.move.line"].search(domain)
-    #     _logger.error(f"------------------{domain, account_valuations, val_p1, val_p2, account}--------------------------")
-    #     for valuation in account_valuations:
-    #         val = round(valuation["debit"] - valuation["credit"], 2)
-    #         _logger.error(f"i8 {val, valuation['product_id'][0], self.product_1.id}")
-    #         if valuation["product_id"][0] == self.product_1.id:
-    #             _logger.debug("Check account P1 {} = {}".format(val, val_p1))
-    #             self.assertAlmostEqual(val, val_p1)
-    #         _logger.error(f"i9 {val, valuation['product_id'][0] == self.product_2.id}")
-    #         if valuation["product_id"][0] == self.product_2.id:
-    #             _logger.debug("Check account P2 {} = {}".format(val, val_p2))
-    #             self.assertAlmostEqual(val, val_p2)
