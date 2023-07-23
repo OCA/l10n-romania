1b6c9eb0b374a6e92d6e473971dd154cea5e9792
Dorin Hongu <dhongu@gmail.com>
2023-07-23 15:27:01 +0300

    mig l10n_ro_stock_price_difference

--- l10n_ro_stock_price_difference/models/account_move_line.py
+++ l10n_ro_stock_price_difference/models/account_move_line.py
@@ -51,7 +51,6 @@ class AccountMoveLine(models.Model):
         if float_is_zero(valuation_total_qty, precision_rounding=precision):
             return diff, qty_diff
 
-
         lines = self.search(
             [
                 ("purchase_line_id", "=", line.purchase_line_id.id),
