3e2c04effe669618eef8d7ed059d255cfaa7fa34
Dorin Hongu <dhongu@gmail.com>
2023-07-23 10:41:12 +0300

    mig l10n_ro_stock_report

--- l10n_ro_stock_report/__manifest__.py
+++ l10n_ro_stock_report/__manifest__.py
@@ -2,7 +2,7 @@
 {
     "name": "Romania - Stock Report (Fisa Magazie)",
     "license": "AGPL-3",
-    "version": "15.0.6.9.0",
+    "version": "16.0.6.9.0",
     "author": "Terrabit,NextERP Romania,Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/l10n-romania",
     "category": "Warehouse",
