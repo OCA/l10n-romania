1ab0b48b8960a37bde1ac07486fc48dd20977b64
Dorin Hongu <dhongu@gmail.com>
2023-07-23 13:41:30 +0300

    mig l10n_ro_stock_account_notice

--- l10n_ro_stock_account/tests/common2.py
+++ l10n_ro_stock_account/tests/common2.py
@@ -83,7 +83,7 @@ class TestStockCommon2(TestStockCommon):
 
             self.picking.button_validate()
             self.picking._action_done()
-            _logger.info("Receptie facuta")
+            _logger.debug("Receptie facuta")
 
         self.po = po
         return po
