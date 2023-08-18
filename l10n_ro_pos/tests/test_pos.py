1156b7a882bd968ba317543a8c7a8f6dd0c480f0
Dorin Hongu <dhongu@gmail.com>
2023-08-18 04:45:54 +0300

    test

--- l10n_ro_pos/tests/test_pos.py
+++ l10n_ro_pos/tests/test_pos.py
@@ -1,18 +1,21 @@
-import odoo
+from odoo.tests import tagged
 
-from odoo.addons.point_of_sale.tests.common import TestPoSCommon
+from odoo.addons.account.tests.common import AccountTestInvoicingCommon
 
 
-@odoo.tests.tagged("post_install", "-at_install")
-class TestReportPoSOrder(TestPoSCommon):
-    def setUp(self):
-        super(TestReportPoSOrder, self).setUp()
-        self.config = self.basic_config
+@tagged("post_install", "-at_install")
+class TestReportPoSOrder(AccountTestInvoicingCommon):
+    @classmethod
+    def setUpClass(cls):
+        ro_template_ref = "l10n_ro.ro_chart_template"
+        super(TestReportPoSOrder, cls).setUpClass(chart_template_ref=ro_template_ref)
+        cls.env.company.anglo_saxon_accounting = True
+        cls.env.company.l10n_ro_accounting = True
 
     def test_wizard_report(self):
         wizard = self.env["pos.details.wizard"].create({})
         wizard.generate_report()
 
     def test_report_invoice(self):
-        report_invoice = self.env["report.point_of_sale.report_invoice"].create({})
+        report_invoice = self.env["report.point_of_sale.report_invoice"].sudo()
         report_invoice._get_report_values([], {})
