902af2556de2ff10ac13bc444ee5411ad6347c73
Adrian Vacaru <adrian.vacaru@dakai.ro>
2023-08-25 13:59:57 +0300

    pre-commit

--- l10n_ro_stock_account/models/product_product.py
+++ l10n_ro_stock_account/models/product_product.py
@@ -454,7 +454,8 @@ class ProductProduct(models.Model):
                     )
                 ]
             for vals in svsl_vals:
-                vals.pop("l10n_ro_tracking")
+                if 'l10n_ro_tracking' in vals:
+                    vals.pop("l10n_ro_tracking")
                 vals["description"] = description + vals.pop("rounding_adjustment", "")
                 vals["company_id"] = self.env.company.id
             empty_stock_svl_list.extend(svsl_vals)
