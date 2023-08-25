b42b0f37fcf5a069e5c406f2e5c15a3effda12bd
Adrian Vacaru <adrian.vacaru@dakai.ro>
2023-08-25 14:08:39 +0300

    Add Stock Account Tracing

---
+++ l10n_ro_stock_account_tracing/__manifest__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+{
+  'name':'Romania stock account tracing',
+  'description': "Accounting trace stock move to SVL, stock move to AML",
+  'version':'14.0.0.1',
+  'author':'Dakai SOFT',
+  'website': 'https://dakai.ro',
+  'author': 'Dakai SOFT SRL',
+  'maintainers': ["adrian-dks"],
+  'license': 'OPL-1',
+  'installable': True,
+  'data': [
+    'views/stock_valuation_layer.xml',
+    ],
+  'category': 'Accounting',
+  'depends': ['l10n_ro_stock_account'],
+}
