cf17f0b85b4e79b75b3a962663cda27d8d827429
Adrian Vacaru <adrian.vacaru@dakai.ro>
2023-09-06 21:07:12 +0300

    add gestiune si categorie contabila

--- l10n_ro_stock_account/models/stock_location.py
+++ l10n_ro_stock_account/models/stock_location.py
@@ -38,3 +38,52 @@ class StockLocation(models.Model):
         domain="[('company_id', '=', current_company_id),"
         "('deprecated', '=', False)]",
     )
+    
+    l10n_ro_accounting_location = fields.Boolean(
+        string="Romania - Is Accounting Location",
+        help="Only for Romania, this define a category "
+        "as accounting location",
+        )
+    l10n_ro_accounting_location_id = fields.Many2one(
+        string="Romania - Parent Accounting Location",
+        help="Only for Romania, link a location "
+        "on accounting location",
+        )
+    
+    
+    def _l10n_ro_prepare_copy_values(self, values):
+        for value in values:
+            if 'l10n_ro_accounting_category_id' in value:
+                value.update(
+                    self._l10n_ro_prepare_copy_value(value)
+                    )
+        return values
+        
+    def _l10n_ro_prepare_copy_value(self, value):
+        accouning_category = self.browse(value.get('l10n_ro_accounting_location_id'))
+        return accouning_category._l10n_ro_copy_value(value)
+    
+    def _l10n_ro_copy_value(self, value):
+        value.update({
+            'l10n_ro_property_account_income_location_id': self.l10n_ro_property_account_income_location_id.id,
+            'l10n_ro_property_account_expense_location_id': self.l10n_ro_property_account_expense_location_id.id,
+            'l10n_ro_property_stock_valuation_account_id': self.l10n_ro_property_stock_valuation_account_id.id,
+            })
+        return value
+    
+    @api.multi
+    def write(self, value):
+        if 'l10n_ro_accounting_location_id' in value:
+            value = self._l10n_ro_prepare_copy_value(value)
+        return super(StockLocation, self).write(value)
+    
+    @api.model
+    def create(self, values):
+        values = self._l10n_ro_prepare_copy_values(values)
+        return super(StockLocation, self).create(values)
+    
+    @api.multi
+    def l10n_ro_fixStockLocation_accounting(self):
+        for s in self:
+            value = s._l10n_ro_copy_value({})
+            self.search([('l10n_ro_accounting_location_id','=',s.id)]).write(value)
