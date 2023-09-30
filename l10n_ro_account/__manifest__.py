9fd8720dd4703c5e07799efc56af27065106d556
Dorin Hongu <dhongu@gmail.com>
2023-09-30 09:35:03 +0300

    migrare si adaugare test script

--- l10n_ro_account/__manifest__.py
+++ l10n_ro_account/__manifest__.py
@@ -4,7 +4,7 @@
 
 {
     "name": "Romania - Account",
-    "version": "15.0.1.1.0",
+    "version": "16.0.1.1.0",
     "summary": "Romania - Account",
     "license": "AGPL-3",
     "author": "Terrabit,Odoo Community Association (OCA)",
