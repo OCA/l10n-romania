fbb60dd7d02e1e39efced94820eea84a056a615f
vasi26ro <43593951+vasi26ro@users.noreply.github.com>
2023-10-13 08:31:18 +0300

    [MIG] currency_rate_update_RO_BNR: Migration to 16.0

---
+++ currency_rate_update_RO_BNR/__manifest__.py
@@ -0,0 +1,21 @@
+{
+    "name": "Currency Rate Update - BNR",
+    "category": "Financial Management/Configuration",
+    "summary": "Currency Rate Update National Bank of Romania service",
+    "version": "16.0.0",
+    "depends": [
+        "base",
+        "currency_rate_update"
+    ],
+    "data": [
+        "views/res_currency_rate.xml"
+    ],
+    "author": "SM Freelancer,"
+    "NextERP Romania,"
+    "Forest and Biomass Romania,"
+    "Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/l10n-romania",
+    "installable": True,
+    "development_status": "Beta",
+    "maintainers": ["feketemihai"],
+}
