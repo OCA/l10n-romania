7c0dffe6aec1728c8bf50a3876e6639386d74234
vasi26ro <43593951+vasi26ro@users.noreply.github.com>
2023-10-13 08:35:43 +0300

    Bumped version

--- currency_rate_update_RO_BNR/__manifest__.py
+++ currency_rate_update_RO_BNR/__manifest__.py
@@ -2,7 +2,7 @@
     "name": "Currency Rate Update - BNR",
     "category": "Financial Management/Configuration",
     "summary": "Currency Rate Update National Bank of Romania service",
-    "version": "16.0.0",
+    "version": "16.0.1.0.0",
     "depends": [
         "base",
         "currency_rate_update"
