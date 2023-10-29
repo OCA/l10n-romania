490ece3792652e4cde1186ffc3af41c3f920d2e9
Mihai Fekete <feketemihai@nexterp.ro>
2023-10-29 09:01:33 +0200

    Update from 14

--- l10n_ro_stock_picking_valued_report/tests/test_consum.py
+++ l10n_ro_stock_picking_valued_report/tests/test_consum.py
@@ -12,32 +12,6 @@ _logger = logging.getLogger(__name__)
 
 @tagged("post_install", "-at_install")
 class TestStockConsumn(TestStockPickingValued):
-    def set_stock(self, product, qty):
-        stock_quant = self.env["stock.quant"].search(
-            [
-                ("product_id", "=", product.id),
-                ("location_id", "=", self.location_warehouse.id),
-            ]
-        )
-        if not stock_quant:
-            stock_quant_obj = self.env["stock.quant"].with_context(inventory_mode=True)
-            inventory = stock_quant_obj.create(
-                {
-                    "location_id": self.location_warehouse.id,
-                    "product_id": product.id,
-                    "inventory_quantity": qty,
-                }
-            )
-            inventory._apply_inventory()
-        else:
-            stock_quant = stock_quant.with_context(inventory_mode=True)
-            stock_quant.write(
-                {
-                    "inventory_quantity": qty,
-                }
-            )
-            stock_quant._apply_inventory()
-
     def test_transfer(self):
         self.set_stock(self.product_mp, 1000)
         location_id = self.picking_type_transfer.default_location_src_id
