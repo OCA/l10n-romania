901cd7962c4b4e5e97103fdcd0bc604d67dc9450
Dorin Hongu <dhongu@gmail.com>
2023-11-01 07:37:55 +0200

    adaugare modul de limitare incasari cash

---
+++ l10n_ro_account/__manifest__.py
@@ -0,0 +1,16 @@
+# ©  2020 Terrabit
+# See README.rst file on addons root folder for license details
+
+
+{
+    "name": "Romania - Account",
+    "version": "14.0.1.1.1",
+    "summary": "Romania - Account",
+    "license": "AGPL-3",
+    "author": "Terrabit,Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/l10n-romania",
+    "category": "Localization",
+    "depends": ["account", "l10n_ro", "l10n_ro_config"],
+    "data": [],
+    "maintainers": ["dhongu"],
+}
