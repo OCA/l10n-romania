9b28ed3691f7f11abbce7729f09d89203e823096
Dorin Hongu <dhongu@gmail.com>
2023-11-12 07:03:48 +0200

    test anaf_sync

---
+++ l10n_ro_account_anaf_sync/tests/test_controller.py
@@ -0,0 +1,36 @@
+# ©  2015-2021 Deltatech
+#              Dorin Hongu <dhongu(@)gmail(.)com
+# See README.rst file on addons root folder for license details
+
+
+from odoo import fields
+from odoo.tests.common import HttpCase, tagged
+
+
+@tagged("-at_install", "post_install")
+class TestAnafSyncControllers(HttpCase):
+    def setUp(self):
+        super(TestAnafSyncControllers, self).setUp()
+        self.test_company = self.env["res.company"].create({"name": "Test Sync"})
+        self.sync = self.env["l10n.ro.account.anaf.sync"].create(
+            {
+                "company_id": self.test_company.id,
+                "client_id": "123",
+                "client_secret": "123",
+                "access_token": "123",
+            }
+        )
+
+    def test_redirect_anaf(self):
+        self.authenticate("demo", "demo")
+        response = self.url_open(
+            "/l10n_ro_account_anaf_sync/redirect_anaf/%s" % self.sync.id
+        )
+        self.assertTrue(response.status_code)
+
+    def test_anaf_oauth(self):
+        self.sync.write({"last_request_datetime": fields.Datetime.now()})
+        response = self.url_open(
+            "/l10n_ro_account_anaf_sync/anaf_oauth", data={"code": "123"}
+        )
+        self.assertEqual(response.status_code, 200)
