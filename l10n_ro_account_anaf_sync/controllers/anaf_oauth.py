511187ad23a0c65ebb08d9dbae59dca82affc92b
Dorin Hongu <dhongu@gmail.com>
2023-11-12 14:01:23 +0200

    fix

--- l10n_ro_account_anaf_sync/controllers/anaf_oauth.py
+++ l10n_ro_account_anaf_sync/controllers/anaf_oauth.py
@@ -95,20 +95,10 @@ class AccountANAFSyncWeb(http.Controller):
         user = request.env["res.users"].browse(uid)
         now = datetime.now()
         ANAF_Configs = request.env["l10n.ro.account.anaf.sync"].sudo()
-        anaf_config = ANAF_Configs.search(
-            [
-                ("client_id", "!=", ""),
-                ("last_request_datetime", ">", now - timedelta(seconds=90)),
-            ]
-        )
+        anaf_config = ANAF_Configs.search([("company_id", "=", user.company_id.id)])
         message = ""
-        if len(anaf_config) > 1:
-            message = _(
-                "More than one ANAF config requested authentication in the last minutes."
-                "Please request them in order, waiting for 2 minutes between requests."
-            )
-        elif not anaf_config:
-            message = _("The response was done too late.\nResponse was: kw=%s") % kw
+        if not anaf_config:
+            message = _("No ANAF config was found for this company.")
 
         if message:
             anaf_config.message_post(body=message)
