abee12520523039cf0bf83b902874c35da71cc72
Dorin Hongu <dhongu@gmail.com>
2023-11-12 12:01:43 +0200

    eFactura

--- l10n_ro_account_edi_ubl/models/res_config_settings.py
+++ l10n_ro_account_edi_ubl/models/res_config_settings.py
@@ -11,5 +11,12 @@ class ResConfigSettings(models.TransientModel):
     l10n_ro_edi_manual = fields.Boolean(
         related="company_id.l10n_ro_edi_manual",
         readonly=False,
+        help="If checked, the invoice will be submitted manually to the ANAF eInvoice.",
         string="E-Invoice Manual submission",
+    )
+
+    l10n_ro_edi_residence = fields.Integer(
+        related="company_id.l10n_ro_edi_residence",
+        readonly=False,
+        string="Residence",
     )
