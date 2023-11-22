73671620403b5a038108c225d751daa5baeb9ee4
Stana Natanaela <musatnatanaela@gmail.com>
2023-11-22 11:17:10 +0200

    [14.0] rulare metoda check_vat_on_payment la create si write pe partener

--- l10n_ro_vat_on_payment/models/res_partner.py
+++ l10n_ro_vat_on_payment/models/res_partner.py
@@ -120,7 +120,7 @@ class ResPartner(models.Model):
     def create(self, vals_list):
         partner = super().create(vals_list)
         partner.filtered(
-            lambda p: p.country_code == "RO" and p.vat
+            lambda p: p.country_id.code == "RO" and p.vat
         ).check_vat_on_payment()
         return partner
 
@@ -128,6 +128,6 @@ class ResPartner(models.Model):
         res = super().write(vals)
         if "vat" in vals:
             self.filtered(
-                lambda partner: partner.country_code == "RO"
+                lambda partner: partner.country_id.code == "RO"
             ).check_vat_on_payment()
         return res
