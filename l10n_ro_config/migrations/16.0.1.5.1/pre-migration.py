2b761c7b7c47b1ad0664c31724b1550bacc75cf9
Dorin Hongu <dhongu@gmail.com>
2023-11-27 08:57:03 +0200

    fix

--- l10n_ro_config/migrations/16.0.1.5.1/pre-migration.py
+++ l10n_ro_config/migrations/16.0.1.5.1/pre-migration.py
@@ -22,14 +22,19 @@ except ImportError:
 
 @openupgrade.migrate(use_env=True)
 def migrate(env, version):
-    openupgrade.rename_fields(
-        env,
-        [
-            (
-                "res.company",
-                "res_company",
-                "l10n_ro_property_customs_commision_product_id",
-                "l10n_ro_property_customs_commission_product_id",
-            ),
-        ],
-    )
+    if not openupgrade.column_exists(
+        env.cr, "res_company", "l10n_ro_property_customs_commission_product_id"
+    ) and openupgrade.column_exists(
+        env.cr, "res_company", "l10n_ro_property_customs_commision_product_id"
+    ):
+        openupgrade.rename_fields(
+            env,
+            [
+                (
+                    "res.company",
+                    "res_company",
+                    "l10n_ro_property_customs_commision_product_id",
+                    "l10n_ro_property_customs_commission_product_id",
+                ),
+            ],
+        )
