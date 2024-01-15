4c9c204abfcf7e7600a4c3260008ec2654525996
Adrian Vacaru <adrian.vacaru@dakai.ro>
2024-01-15 19:03:09 +0200

    Fix dates on trigger

--- l10n_ro_account_edi_ubl/models/account_move.py
+++ l10n_ro_account_edi_ubl/models/account_move.py
@@ -79,7 +79,7 @@ class AccountMove(models.Model):
             )
         if existing_edi_document:
             residence = min(posted.mapped("company_id").l10n_ro_edi_residence)
-            trigger_date = fields.Datetime.now() + timedelta(days=residence))
+            trigger_date = fields.Datetime.now() + timedelta(days=residence)
             self.env.ref("account_edi.ir_cron_edi_network")._trigger(at=trigger_date)
         return posted
 
