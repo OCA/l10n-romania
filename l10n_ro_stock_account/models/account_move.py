dfd864d43b43490dbf368938c8b0d3ad0b13db38
Sima Elisabeta <sima.elisabeta@gmail.com>
2024-03-05 12:18:32 +0200

    FIX anglo_saxon_move_lines

--- l10n_ro_stock_account/models/account_move.py
+++ l10n_ro_stock_account/models/account_move.py
@@ -43,9 +43,10 @@ class AccountMove(models.Model):
 
     def _stock_account_prepare_anglo_saxon_out_lines_vals(self):
         # nu se mai face descarcarea de gestiune la facturare
+        import ipdb; ipdb.set_trace()
         invoices = self
         for move in self:
-            if move.is_l10n_ro_record:
+            if move.company_id._check_is_l10n_ro_record():
                 invoices -= move
         return super(
             AccountMove, invoices
