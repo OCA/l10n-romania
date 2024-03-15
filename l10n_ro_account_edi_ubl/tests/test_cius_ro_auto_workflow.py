9a95f11473a44fc163c88fe273cf7a24da659740
Mihai Fekete <feketemihai@gmail.com>
2024-03-15 13:07:23 +0200

    Update from JWT

--- l10n_ro_account_edi_ubl/tests/test_cius_ro_auto_workflow.py
+++ l10n_ro_account_edi_ubl/tests/test_cius_ro_auto_workflow.py
@@ -43,7 +43,7 @@ class TestCiusRoAutoWorkflow(CiusRoTestSetup):
     # Test case for the cron job that processes documents in step 1 of the CIUS workflow.
     @freezegun.freeze_time("2022-09-04")
     def test_process_documents_web_services_step1_cron(self):
-        anaf_config = self.env.company.l10n_ro_account_anaf_sync_id
+        anaf_config = self.env.company._l10n_ro_get_anaf_sync(scope="e-factura")
         anaf_config.access_token = "test"
         self.invoice.action_post()
 
@@ -176,7 +176,7 @@ class TestCiusRoAutoWorkflow(CiusRoTestSetup):
 
     def test_l10n_ro_get_anaf_efactura_messages(self):
         self.env.company.vat = "RO23685159"
-        anaf_config = self.env.company.l10n_ro_account_anaf_sync_id
+        anaf_config = self.env.company._l10n_ro_get_anaf_sync(scope="e-factura")
         anaf_config.access_token = "test"
         msg_dict = {
             "mesaje": [
@@ -221,7 +221,7 @@ class TestCiusRoAutoWorkflow(CiusRoTestSetup):
             )
 
     def test_l10n_ro_create_anaf_efactura(self):
-        anaf_config = self.env.company.l10n_ro_account_anaf_sync_id
+        anaf_config = self.env.company._l10n_ro_get_anaf_sync(scope="e-factura")
         anaf_config.access_token = "test"
         self.env.company.l10n_ro_download_einvoices = True
         self.env.company.partner_id.write(
