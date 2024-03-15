d7e681b002b76da600e75deda8cce55fec63b926
Dorin Hongu <dhongu@gmail.com>
2024-03-15 17:12:52 +0200

    conversie select

---
+++ l10n_ro_etransport/models/l10n_ro_e_transport.py
@@ -0,0 +1,20 @@
+# Copyright (C) 2024 Deltatech
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from odoo import fields, models
+
+
+class ETransportAim(models.Model):
+    _name = "l10n.ro.e.transport.aim"
+    _description = "E Transport Aim"
+
+    name = fields.Char(required=True)
+    code = fields.Char(required=True)
+
+
+class ETransportCustoms(models.Model):
+    _name = "l10n.ro.e.transport.customs"
+    _description = "E Transport Customs"
+
+    name = fields.Char(required=True)
+    code = fields.Char(required=True)
