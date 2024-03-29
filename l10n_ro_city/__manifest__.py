5798888f86a2ef6181617c8566217fd48cf86df8
Dorin Hongu <dhongu@gmail.com>
2024-03-29 08:23:35 +0200

    update

--- l10n_ro_city/__manifest__.py
+++ l10n_ro_city/__manifest__.py
@@ -4,6 +4,7 @@
 {
     "name": "Romania - City",
     "summary": "Romania - City",
+    "countries": ["ro"],
     "license": "AGPL-3",
     "version": "17.0.1.0.0",
     "author": "Terrabit,Odoo Community Association (OCA)",
--- l10n_ro_city/models/res_city.py
+++ l10n_ro_city/models/res_city.py
@@ -25,11 +25,7 @@ class CountryCity(models.Model):
                     result.append(
                         (
                             record.id,
-                            "{} ({}) ({})".format(
-                                record.name,
-                                record.l10n_ro_municipality,
-                                record.state_id.code,
-                            ),
+                            f"{record.name} ({record.l10n_ro_municipality}) ({record.state_id.code})",
                         )
                     )
                 else:
