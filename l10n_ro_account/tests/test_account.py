6f1a8a0dd2f63b20bba2c5049a7989ac3aaf1700
Dorin Hongu <dhongu@gmail.com>
2024-03-29 21:40:29 +0200

    [IMP] l10n_ro_account: pre-commit stuff

--- l10n_ro_account/tests/test_account.py
+++ l10n_ro_account/tests/test_account.py
@@ -7,7 +7,7 @@ from odoo.tests.common import TransactionCase
 
 class TestAccount(TransactionCase):
     def setUp(self):
-        super(TestAccount, self).setUp()
+        super().setUp()
         self.env.company.l10n_ro_accounting = True
 
         self.account = self.env["account.account"].create(
