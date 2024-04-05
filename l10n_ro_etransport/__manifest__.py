ab44c1a3f479489e7b9da922cb2a0b74a064e346
Dorin Hongu <dhongu@gmail.com>
2024-04-05 13:53:50 +0300

    fix e-transport

--- l10n_ro_etransport/__manifest__.py
+++ l10n_ro_etransport/__manifest__.py
@@ -22,7 +22,7 @@
         "data/l10n.ro.e.transport.operation.csv",
     ],
     "license": "AGPL-3",
-    "version": "16.0.0.4.0",
+    "version": "16.0.0.4.1",
     "author": "Terrabit," "Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/l10n-romania",
     "installable": True,
