4357bdb95081e898d646924b26e2a01197a4c4c1
Dorin Hongu <dhongu@gmail.com>
2024-04-22 08:46:20 +0300

    spargere modul stock account

---
+++ l10n_ro_stock_account_tracking/readme/CONTRIBUTORS.rst
@@ -0,0 +1,12 @@
+* `NextERP Romania <https://www.nexterp.ro>`_:
+
+  * Fekete Mihai <feketemihai@nexterp.ro>
+  * Alexandru Teodor <teodoralexandru@nexterp.ro>
+
+* `Terrabit <https://www.terrabit.ro>`_:
+
+  * Dorin Hongu <dhongu@gmail.com>
+
+
+
+Do not contact contributors directly about support or help with technical issues.
---
+++ l10n_ro_stock_account_tracking/tests/__init__.py
@@ -0,0 +1,13 @@
+from . import common
+from . import common2
+
+from . import test_retur
+from . import test_purchase
+from . import test_sale
+from . import test_inventory
+
+from . import test_consum
+from . import test_reception_return_not_fifo
+from . import test_svl_vacuum_location
+from . import test_sale_return_not_fifo
+from . import test_stock_quant_value
