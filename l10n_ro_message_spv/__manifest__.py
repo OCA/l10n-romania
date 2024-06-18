4cb7317ba96f5ed31a017b8a3b757e05a92754f6
Dorin Hongu <dhongu@gmail.com>
2024-06-18 08:09:04 +0300

    migrare message spv

--- l10n_ro_message_spv/__manifest__.py
+++ l10n_ro_message_spv/__manifest__.py
@@ -18,7 +18,7 @@
         ],
     },
     "license": "AGPL-3",
-    "version": "15.0.1.1.0",
+    "version": "16.0.1.1.0",
     "author": "Terrabit," "NextERP Romania," "Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/l10n-romania",
     "installable": True,
