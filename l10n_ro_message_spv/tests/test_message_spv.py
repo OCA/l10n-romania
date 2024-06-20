e28fbae3754a4183b00b3c58b9d667eaacd80679
Dorin Hongu <dhongu@gmail.com>
2024-06-20 14:07:17 +0300

    traducere

---
+++ l10n_ro_message_spv/tests/test_message_spv.py
@@ -0,0 +1,31 @@
+# Copyright (C) 2020 Terrabit
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html)
+
+
+from odoo.tests.common import TransactionCase
+
+
+class TestMessageSPV(TransactionCase):
+    # test de creare mesaje preluate de la SPV
+
+    def setUp(self):
+        # initializare date
+
+        #
+        return super().setUp()
+
+    def test_download_messages(self):
+        # test de descarcare a mesajelor de la SPV
+        pass
+
+    def test_render_pdf(self):
+        # test de randare a mesajelor in format PDF
+        pass
+
+    def test_get_embedded_pdf(self):
+        # test de preluare a PDF-ului din mesaj
+        pass
+
+    def test_create_invoice_from_message(self):
+        # test de creare a unei facturi din mesaj
+        pass
