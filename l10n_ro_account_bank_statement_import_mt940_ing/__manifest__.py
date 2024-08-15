2ebf38d6630d2d34d5311e513f8fa3a0e3dfcc5e
Dorin Hongu <dhongu@gmail.com>
2024-08-15 09:37:04 +0300

    migrare l10n_ro_account_bank_statement_import_mt940_ing

--- l10n_ro_account_bank_statement_import_mt940_ing/__manifest__.py
+++ l10n_ro_account_bank_statement_import_mt940_ing/__manifest__.py
@@ -5,7 +5,7 @@
 
 {
     "name": "MT940 ING Format Bank Statements Import",
-    "version": "16.0.1.4.0",
+    "version": "17.0.0.0.0",
     "license": "AGPL-3",
     "author": "Terrabit,"
     "NextERP Romania SRL,"
