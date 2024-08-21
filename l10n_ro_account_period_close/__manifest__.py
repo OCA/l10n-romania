6473de40a1c7e1907361dc95863cab4196b7b555
Stana Natanaela <musatnatanaela@gmail.com>
2024-08-21 15:06:03 +0300

    [MIG] l10n_ro_account_period_close: Migration to 17.0

--- l10n_ro_account_period_close/__manifest__.py
+++ l10n_ro_account_period_close/__manifest__.py
@@ -12,7 +12,7 @@
         "security/ir.model.access.csv",
     ],
     "license": "AGPL-3",
-    "version": "16.0.3.7.0",
+    "version": "17.0.0.0.0",
     "author": "NextERP Romania,"
     "Forest and Biomass Romania,"
     "Odoo Community Association (OCA)",
