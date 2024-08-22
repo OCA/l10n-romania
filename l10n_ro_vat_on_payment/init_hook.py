fffe3d299afe6e7b3d99a8e32e73bb06fed7e256
Stana Natanaela <musatnatanaela@gmail.com>
2024-08-22 17:05:36 +0300

    [MIG] l10n_ro_vat_on_payment: Migration to 17.0

--- l10n_ro_vat_on_payment/init_hook.py
+++ l10n_ro_vat_on_payment/init_hook.py
@@ -6,12 +6,12 @@ import os
 import shutil
 
 from odoo import tools
-from odoo.modules.module import get_module_resource
+from odoo.tools.misc import file_path
 
 
 def pre_init_hook(cr):
     data_dir = tools.config["data_dir"]
     istoric_file = os.path.join(data_dir, "istoric.txt")
 
-    test_file = get_module_resource("l10n_ro_vat_on_payment", "tests", "istoric.txt")
+    test_file = file_path("l10n_ro_vat_on_payment", "tests", "istoric.txt")
     shutil.copyfile(test_file, istoric_file)
