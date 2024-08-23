521d6efb6748440ac697beb0aee5a5fd12abeb93
Stana Natanaela <musatnatanaela@gmail.com>
2024-08-23 16:47:49 +0300

    [MIG] l10n_ro_stock_account_landed_cost: Migration to 17.0

--- l10n_ro_stock_account_landed_cost/__manifest__.py
+++ l10n_ro_stock_account_landed_cost/__manifest__.py
@@ -1,7 +1,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
     "name": "Romania - Stock Accounting Landed Cost",
-    "version": "16.0.1.26.0",
+    "version": "17.0.1.0.0",
     "category": "Localization",
     "summary": "Romania - Stock Accounting Landed Cost",
     "author": "NextERP Romania,"
