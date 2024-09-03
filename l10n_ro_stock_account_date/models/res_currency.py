ea18c5c1245bb9ae380cacc790930f6bef37ed09
Mihai Fekete <feketemihai@gmail.com>
2024-09-03 17:49:16 +0300

    Update pre-commit

--- l10n_ro_stock_account_date/models/res_currency.py
+++ l10n_ro_stock_account_date/models/res_currency.py
@@ -8,6 +8,7 @@ from odoo import models
 class Currency(models.Model):
     _inherit = "res.currency"
 
+    # pylint: disable=W0622
     def _convert(self, from_amount, to_currency, company=None, date=None, round=True):
         force_period_date = self.env.context.get("force_period_date", False)
         if force_period_date:
