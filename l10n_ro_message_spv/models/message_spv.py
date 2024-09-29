83f138e1d2f200a448b7b87696395eaa4521a585
Dorin Hongu <dhongu@gmail.com>
2024-09-29 09:57:30 +0300

    get partner

--- l10n_ro_message_spv/models/message_spv.py
+++ l10n_ro_message_spv/models/message_spv.py
@@ -235,6 +235,7 @@ class MessageSPV(models.Model):
         return info_msg
 
     def get_invoice_from_move(self):
+        self.get_partner()
         messages_without_invoice = self.filtered(lambda m: not m.invoice_id)
         message_ids = messages_without_invoice.mapped("name")
         request_ids = messages_without_invoice.mapped("request_id")
@@ -274,6 +275,7 @@ class MessageSPV(models.Model):
         self.get_data_from_invoice()
 
     def get_data_from_invoice(self):
+        self.get_partner()
         for message in self:
             if not message.invoice_id:
                 continue
@@ -288,7 +290,8 @@ class MessageSPV(models.Model):
 
             message.write(
                 {
-                    "partner_id": message.invoice_id.commercial_partner_id.id,
+                    "partner_id": message.invoice_id.commercial_partner_id.id
+                    or message.partner_id.id,
                     "invoice_amount": invoice_amount,
                     "state": state,
                 }
@@ -300,11 +303,12 @@ class MessageSPV(models.Model):
                 attachments += message.attachment_xml_id
                 attachments += message.attachment_anaf_pdf_id
                 attachments += message.attachment_embedded_pdf_id
-                attachments.write(
+                attachments.sudo().write(
                     {"res_id": message.invoice_id.id, "res_model": "account.move"}
                 )
 
     def create_invoice(self):
+        self.get_partner()
         for message in self.filtered(lambda m: not m.invoice_id):
             if not message.message_type == "in_invoice":
                 continue
@@ -461,3 +465,18 @@ class MessageSPV(models.Model):
             "url": f"/web/content/{attachment_field_id}?download=true",
             "target": "self",
         }
+
+    def get_partner(self):
+        for message in self.filtered(lambda m: not m.partner_id):
+            if message.cif:
+                domain = [("vat", "like", message.cif), ("is_company", "=", True)]
+                partner = self.env["res.partner"].search(domain, limit=1)
+                if not partner:
+                    partner = self.env["res.partner"].create(
+                        {
+                            "name": message.cif,
+                            "vat": message.cif,
+                            "is_company": True,
+                        }
+                    )
+                message.write({"partner_id": partner.id})
