4210ae342530e2054fbea0e2d6c35b3f31f520bd
Marius <marius.nick@gmail.com>
2024-10-07 09:33:51 +0300

    [16.0] [IMP] Add option to print prices in valued report

--- l10n_ro_config/models/res_company.py
+++ l10n_ro_config/models/res_company.py
@@ -141,6 +141,13 @@ class ResCompany(models.Model):
         "tax repartition lines marked as not deductible. If the line account does not have "
         "a non deductible account set, this account will be used.",
     )
+    
+    l10n_ro_print_delivery_price = fields.Boolean(string= "Print Delivery Slip Prices and Totals" ,
+            help= "For delivery order to customer or intern transfer order, this variable sets whether if the unit price and total are printed on the delivery slip"
+            "Default is enabled, the system will automatically print these values  on the delivery note.", 
+            default=True
+            )
+    
 
     def _check_is_l10n_ro_record(self, company=False):
         if not company:
