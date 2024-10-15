f415aa39d1740d9852dfb035bc47e3137176d8a3
Dorin Hongu <dhongu@gmail.com>
2024-10-15 19:56:54 +0300

    migrare city

--- l10n_ro_city/tests/test_ro_city.py
+++ l10n_ro_city/tests/test_ro_city.py
@@ -2,7 +2,8 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html)
 
 
-from odoo.tests.common import Form, TransactionCase
+from odoo.tests import Form
+from odoo.tests.common import TransactionCase
 
 
 class TestRoCity(TransactionCase):
