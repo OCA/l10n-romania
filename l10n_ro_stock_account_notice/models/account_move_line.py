dc8f928489d82452aec896e7ff2103880a496643
Dorin Hongu <dhongu@gmail.com>
2024-10-26 06:12:00 +0300

    fix receptie in progres

--- l10n_ro_stock_account_notice/models/account_move_line.py
+++ l10n_ro_stock_account_notice/models/account_move_line.py
@@ -17,7 +17,8 @@ class AccountMoveLine(models.Model):
         l10n_ro_lines_for_notice = self.filtered(
             lambda x: x.product_id.type == "product" and x.is_l10n_ro_record
         )
-        remaining = self.with_context(valued_type=self.move_id.move_type)
+        valued_type = self.env.context.get("valued_type", self.move_id.move_type)
+        remaining = self.with_context(valued_type=valued_type)
         invoice_in_notice_lines = self.env["account.move.line"].with_context(
             valued_type="invoice_in_notice"
         )
