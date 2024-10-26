89640f04e8ddf3b43aad65abef5b581d606b058d
Dorin Hongu <dhongu@gmail.com>
2024-10-26 07:23:09 +0300

    desactivarea  metode _stock_account_prepare_anglo_saxon_in_lines_vals se va face in l10n_ro_stock_price_difference

--- l10n_ro_stock_account_notice/models/account_move.py
+++ l10n_ro_stock_account_notice/models/account_move.py
@@ -61,10 +61,11 @@ class AccountMove(models.Model):
                 account = lines_diff_acc[0]
         return account
 
-    def _stock_account_prepare_anglo_saxon_in_lines_vals(self):
-        l10n_ro_records = self.filtered("is_l10n_ro_record")
-        if l10n_ro_records == self:
-            return []
-        return super(
-            AccountMove, self - l10n_ro_records
-        )._stock_account_prepare_anglo_saxon_in_lines_vals()
+    # desactivarea acestie metode se va face in l10n_ro_stock_price_difference
+    # def _stock_account_prepare_anglo_saxon_in_lines_vals(self):
+    #     l10n_ro_records = self.filtered("is_l10n_ro_record")
+    #     if l10n_ro_records == self:
+    #         return []
+    #     return super(
+    #         AccountMove, self - l10n_ro_records
+    #     )._stock_account_prepare_anglo_saxon_in_lines_vals()
