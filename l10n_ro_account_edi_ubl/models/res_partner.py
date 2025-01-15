03ffd23e9e1397af8cf21df03e8e7e08bd508977
Sima Elisabeta <elisabeta.sima@nexterp.ro>
2025-01-15 13:39:59 +0200

    Add pre-migr to set edi_ubl_no_send_cnp as true on individuals

--- l10n_ro_account_edi_ubl/models/res_partner.py
+++ l10n_ro_account_edi_ubl/models/res_partner.py
@@ -1,7 +1,7 @@
 # Copyright (C) 2024 NextERP Romania
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from odoo import fields, models
+from odoo import api, fields, models
 
 
 class ResPartner(models.Model):
@@ -11,3 +11,15 @@ class ResPartner(models.Model):
         "Romania - No send CNP UBL",
         help="Check this if the partner should not receive UBL invoices on their CNP.",
     )
+
+    @api.model_create_multi
+    def create(self, vals_list):
+        res = super().create(vals_list)
+        for record in res:
+            if (
+                record.country_id == self.env.ref("base.ro")
+                and not record.is_company
+                and not record.parent_id
+            ):
+                record.l10n_ro_edi_ubl_no_send_cnp = True
+        return res
