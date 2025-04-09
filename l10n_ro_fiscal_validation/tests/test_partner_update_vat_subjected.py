5a0fd7b65a3609ac7e839239acba70750bd2f7a0
Marius <marius.nick@gmail.com>
2025-04-09 12:43:23 +0300

    [18.0][MIG] l10n_ro_fiscal_validation to 18.0

--- l10n_ro_fiscal_validation/tests/test_partner_update_vat_subjected.py
+++ l10n_ro_fiscal_validation/tests/test_partner_update_vat_subjected.py
@@ -2,12 +2,11 @@
 # Copyright (C) 2020 NextERP Romania
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-import io
+import codecs
+import csv
 import os
 
-from odoo import tools
 from odoo.tests.common import TransactionCase
-from odoo.tools import pycompat
 
 
 class TestPartnerUpdateVatSubjectedBase(TransactionCase):
@@ -22,20 +21,24 @@ class TestPartnerUpdateVatSubjectedBase(TransactionCase):
         data_dir = os.path.join(
             os.path.dirname(os.path.dirname(os.path.abspath(__file__))), "examples/"
         )
-        context = {"tracking_disable": True, "no_vat_validation": True}
-        fdata = tools.file_open(data_dir + "res.partner.csv")
-        csvdata = pycompat.csv_reader(
-            io.BytesIO(bytes(fdata.read(), "utf-8")), quotechar='"', delimiter=","
-        )
+        context = {
+            "tracking_disable": True,
+            "no_vat_validation": True,
+        }
+
+        f = open(os.path.join(data_dir, "res.partner.csv"), "rb")
+
+        csvdata = csv.DictReader(codecs.iterdecode(f, "utf-8"))
+
         lines = [line for line in csvdata if any(line)]
         cls.env.user.company_id.write({"vat_check_vies": False})
         for line in lines:
             cls.partner_model.with_context(**context).create(
                 {
-                    "id": line[0],
-                    "name": line[1],
-                    "vat": line[2],
-                    "is_company": line[3],
+                    "id": line["id"],
+                    "name": line["name"],
+                    "vat": line["vat"],
+                    "is_company": line["is_company"],
                     "country_id": cls.env.ref("base.ro").id,
                 }
             )
@@ -45,4 +48,5 @@ class TestUpdatePartner(TestPartnerUpdateVatSubjectedBase):
     def test_vat_subjected_cron(self):
         """Check methods vat from ANAF."""
         # Test cron update vat subjected from ANAF
-        self.partner_model._update_l10n_ro_vat_subjected_all()
+        context = {"test_data": True}
+        self.partner_model.with_context(**context)._update_l10n_ro_vat_subjected_all()
