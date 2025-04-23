229340b5feb76cf23a9823ae510fa106675f246a
Stana Natanaela <musatnatanaela@gmail.com>
2025-04-23 10:07:42 +0300

    [18.0]adaugare parametru l10n_ro_download_message_days pentru metoda refresh

--- l10n_ro_message_spv/models/message_spv.py
+++ l10n_ro_message_spv/models/message_spv.py
@@ -604,7 +604,15 @@ class MessageSPV(models.Model):
                 message.write({"partner_id": partner.id})
 
     def refresh(self):
-        self.env.company._l10n_ro_download_message_spv(no_days=1)
+        l10n_ro_download_message_days = int(
+            self.env["ir.config_parameter"]
+            .sudo()
+            .get_param("l10n_ro_download_message_days")
+            or 1
+        )
+        self.env.company._l10n_ro_download_message_spv(
+            no_days=l10n_ro_download_message_days
+        )
 
     def show_invoice(self):
         invoices = self.mapped("invoice_id")
