f5947d71251cd490b713151dbd28539be72df4ad
Dorin Hongu <dhongu@gmail.com>
2025-05-28 08:49:21 +0300

    Adaugă opțiunea de a dezactiva descărcarea fișierului ZIP
    Actualizează metoda `_l10n_ro_download_message_spv` pentru a permite controlul descărcării fișierelor ZIP printr-un flag `download_zip`. Acest lucru oferă flexibilitate pentru teste și alte utilizări specifice.

--- l10n_ro_message_spv/tests/test_message_spv.py
+++ l10n_ro_message_spv/tests/test_message_spv.py
@@ -39,7 +39,7 @@ class TestMessageSPV(TestMessageSPV):
             "odoo.addons.l10n_ro_efactura.models.ciusro_document.make_efactura_request",
             return_value=anaf_messages,
         ):
-            self.env.company.l10n_ro_download_message_spv()
+            self.env.company._l10n_ro_download_message_spv(download_zip=False)
 
     def test_download_from_spv(self):
         # test descarcare zip from SPV
