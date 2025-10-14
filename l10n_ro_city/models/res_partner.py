c89eaed0d7de370ef2db81370dc7579dbe20c4c9
Dorin Hongu <dhongu@gmail.com>
2025-10-14 18:08:51 +0300

    Corectează verificarea contextului pentru `skip_ro_vat_change`.
    Adaugă paranteze suplimentare pentru o mai bună lizibilitate în verificarea condițiilor din contextul `self.env.context`. Elimină, de asemenea, un rând gol redundant din cod pentru o mai bună organizare.

--- l10n_ro_city/models/res_partner.py
+++ l10n_ro_city/models/res_partner.py
@@ -47,7 +47,10 @@ class Partner(models.Model):
                     "06": "l10n_ro_city.RO_179196",  # Sector 6
                 }
                 city = self.env.ref(mapping[self.zip[:2]])
-                if self.state_id != state_b and 'skip_ro_vat_change' not in self.env.context:
+                if (
+                    self.state_id != state_b
+                    and "skip_ro_vat_change" not in self.env.context
+                ):
                     raise UserError(
                         _(
                             f"The city {city.name} doesn't match the"
@@ -65,7 +68,6 @@ class Partner(models.Model):
                 self.city = city.name
                 self.city_id = city
                 self.state = city.state_id
-
 
     @api.onchange("city_id")
     def _onchange_city_id(self):
