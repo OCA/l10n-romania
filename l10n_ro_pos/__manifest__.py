351436cb47fd2239c05f49e4922c4ae087e5762e
Dorin Hongu <dhongu@gmail.com>
2025-12-03 10:29:39 +0200

    Corectează logica și elimină raportul nereferit.
    Înlocuiește accesul direct la chei cu metoda `get` pentru a evita erorile în caz de lipsă a valorilor și setează valorile implicite. Comentează definiția raportului `report_saledetails.xml` din manifest pentru a preveni referințele nerelevante.

--- l10n_ro_pos/__manifest__.py
+++ l10n_ro_pos/__manifest__.py
@@ -3,7 +3,7 @@
 # See README.rst file on addons root folder for license details
 {
     "name": "Romania - Point of Sale",
-    "version": "18.0.1.3.0",
+    "version": "18.0.1.3.1",
     "category": "Localization",
     "countries": ["ro"],
     "license": "AGPL-3",
@@ -11,7 +11,7 @@
     "website": "https://github.com/OCA/l10n-romania",
     "depends": ["point_of_sale", "l10n_ro_stock", "l10n_ro_config"],
     "data": [
-        "views/report_saledetails.xml",
+        # "views/report_saledetails.xml",
     ],
     "maintainers": ["dhongu"],
     "assets": {
