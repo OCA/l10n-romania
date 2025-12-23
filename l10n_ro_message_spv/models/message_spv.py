afb7ac847356c78d098c21dc2e909b73dadc5868
Mihai Fekete <feketemihai@nexterp.ro>
2025-12-23 20:26:50 +0200

    Internal transfer comment template use warehouses addresses

--- l10n_ro_message_spv/models/message_spv.py
+++ l10n_ro_message_spv/models/message_spv.py
@@ -432,9 +432,9 @@ class MessageSPV(models.Model):
         if b"<CreditNote" in xml:
             val1 = "FCN"
 
-        url = f"https://webservicesp.anaf.ro/prod/FCTEL/rest/transformare/{val1}"
+        url = f"https://webservicesp.anaf.ro/prod/FCTEL/rest/transformare/{val1}" # pylint: disable=E231
         if no_validate:
-            url = f"https://webservicesp.anaf.ro/prod/FCTEL/rest/transformare/{val1}/DA"
+            url = f"https://webservicesp.anaf.ro/prod/FCTEL/rest/transformare/{val1}/DA" # pylint: disable=E231
 
         res = requests.post(url, data=xml, headers=headers, timeout=25)
         if "The requested URL was rejected" in res.text:
