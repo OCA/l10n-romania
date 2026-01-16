c54c6d3fe0fe32b25714d226cd4c8f3c1ff34cfc
Debora7 <burnichinaomi@gmail.com>
2026-01-16 14:46:26 +0200

    update pre migration file .

--- l10n_ro_partner_create_by_vat/migrations/19.0.0.3.0/pre-migration.py
+++ l10n_ro_partner_create_by_vat/migrations/19.0.0.3.0/pre-migration.py
@@ -8,31 +8,19 @@ def migrate(cr, version):
     model_name = 'l10n.ro.res.partner.anaf.status'
     table_name = 'l10n_ro_res_partner_anaf_status'
     field_name = 'date'
-
-    # Drop ref for XML
+    
     openupgrade.logged_query(cr, """
         UPDATE ir_ui_view 
-        SET arch_db = regexp_replace(arch_db::text, '<field[^>]*name=[''"]' || %s || '[''"][^>]*/>', '', 'g')::jsonb
-        WHERE model = %s AND arch_db::text LIKE %s
-    """, (field_name, model_name, f'%{field_name}%'))
+        SET arch_db = regexp_replace(
+            arch_db, 
+            '<field[^>]*name=["\']' || %s || '["\'][^>]*/>', 
+            '', 
+            'g'
+        )
+        WHERE arch_db LIKE %s
+    """, (field_name, f'%name="{field_name}"%'))
 
-    # Drop field from db Odoo
-    openupgrade.logged_query(cr, """
-        DELETE FROM ir_model_fields 
-        WHERE name = %s AND model = %s
-    """, (field_name, model_name))
-
-    # Case rename column
-    # Folosim utilitarul openupgrade pentru a verifica și redenumi coloana safe
     if openupgrade.column_exists(cr, table_name, field_name):
         openupgrade.rename_columns(cr, {
-            table_name: [(field_name, 'date_old')]
+            table_name: [(field_name, openupgrade.get_legacy_name(field_name))]
         })
-
-    # Clear ref for all External ID
-    openupgrade.logged_query(cr, """
-        DELETE FROM ir_model_data 
-        WHERE model = 'ir.model.fields' 
-          AND name = %s 
-          AND module = 'l10n_ro_anaf_status'
-    """, (f'field_{table_name}_{field_name}',))
