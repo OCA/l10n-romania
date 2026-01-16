4636060f06677376215bbeaf0b7a6acc7377c7de
Debora7 <burnichinaomi@gmail.com>
2026-01-16 13:10:33 +0200

    upup

--- l10n_ro_partner_create_by_vat/migrations/19.0.0.3.0/pre-migration.py
+++ l10n_ro_partner_create_by_vat/migrations/19.0.0.3.0/pre-migration.py
@@ -12,8 +12,8 @@ def migrate(cr, version):
     # Drop ref for XML
     openupgrade.logged_query(cr, """
         UPDATE ir_ui_view 
-        SET arch_db = regexp_replace(arch_db, '<field[^>]*name=[''"]' || %s || '[''"][^>]*/>', '', 'g')
-        WHERE model = %s AND arch_db LIKE %s
+        SET arch_db = regexp_replace(arch_db::text, '<field[^>]*name=[''"]' || %s || '[''"][^>]*/>', '', 'g')::jsonb
+        WHERE model = %s AND arch_db::text LIKE %s
     """, (field_name, model_name, f'%{field_name}%'))
 
     # Drop field from db Odoo
