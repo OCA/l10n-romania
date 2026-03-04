e9b3bdd0a43aa620209aef4e7de3207b4784d173
Debora7 <burnichinaomi@gmail.com>
2026-03-04 15:53:32 +0200

    Removed from report lines same account moves.

--- l10n_ro_stock_report/report/stock_report.py
+++ l10n_ro_stock_report/report/stock_report.py
@@ -437,6 +437,7 @@ class StorageSheet(models.TransientModel):
             ( %(all_products)s OR sm.product_id in %(product)s ) AND
             sm.date >= %(datetime_from)s AND sm.date <= %(datetime_to)s AND
             sm.location_dest_id in %(locations)s AND
+            sm.l10n_ro_account_id != sm.l10n_ro_second_account_id AND
             sm.l10n_ro_account_id IS NOT NULL
 
         GROUP BY sm.product_id, sm.date, sm.reference,
@@ -488,6 +489,7 @@ class StorageSheet(models.TransientModel):
             ( %(all_products)s OR sm.product_id in %(product)s ) AND
             sm.date >= %(datetime_from)s AND sm.date <= %(datetime_to)s AND
             sm.location_id in %(locations)s AND
+            sm.l10n_ro_account_id != sm.l10n_ro_second_account_id AND
             sm.l10n_ro_second_account_id IS NOT NULL
 
         GROUP BY sm.product_id, sm.date, sm.reference,
