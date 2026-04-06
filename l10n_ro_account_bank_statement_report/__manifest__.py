a049fbdc6c54e3cc4d6a71afd2918e29cc895d07
Mihai Fekete <feketemihai@nexterp.ro>
2026-04-06 15:52:57 +0300

    [IMP] l10n_ro_account_bank_statement_report: pre-commit auto fixes

--- l10n_ro_account_bank_statement_report/__manifest__.py
+++ l10n_ro_account_bank_statement_report/__manifest__.py
@@ -5,7 +5,7 @@
     "name": "Romania - Bank Statement Report",
     "summary": "Romania - Bank Statement Report",
     "version": "18.0.0.1.0",
-    "author": "NextERP Romania," "Odoo Community Association (OCA)",
+    "author": "NextERP Romania,Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/l10n-romania",
     "category": "Accounting",
     "countries": ["ro"],
